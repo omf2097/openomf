6480a1bbc4afb148154715354fb77a193b9e9bb3
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-23 23:37:15 +0200

    Start work on BK file parser

--- include/bk.h
+++ include/bk.h
@@ -2,10 +2,25 @@
 #define _BK_H
 
 typedef struct bk_file_t {
-
+    uint32_t file_id;
+    uint8_t unknown_a;
+    uint16_t img_w;
+    uint16_t img_h;
 } bk_file;
 
-bk_file* bk_parse(const char* data, int len);
-void bk_destroy(bk_file* bkfile);
+/**
+  * Load .BK file. Returns 0 on error.
+  */
+bk_file* bk_load(const char *filename);
+
+/**
+  * Saves BK file structure. Returns 1 on success, 0 on error.
+  */
+int bk_save(const char* filename, bk_file *data);
+
+/**
+  * Destroys bk_file structure from memory.
+  */
+void bk_destroy(bk_file *data);
 
 #endif // _BK_H
--- src/bk.c
+++ src/bk.c
@@ -3,10 +3,49 @@
 #include "internal/writer.h"
 #include "internal/animation.h"
 
-bk_file* bk_parse(const char* data, int len) {
-    return 0;
+bk_file* bk_load(const char *filename) {
+    // Initialize reader
+    sd_reader *r = sd_reader_open(filename);
+    if(!r) {
+        return 0;
+    }
+
+    // Allocate structure
+    bk_file *data = malloc(sizeof(bk_file));
+
+    // Header
+    data->file_id = sd_read_udword(r);
+    data->unknown_a = sd_read_ubyte(r);
+    data->img_w = sd_read_uword(r);
+    data->img_h = sd_read_uword(r);
+
+    // Read animations
+    int8_t animno = 0;
+    int8_t last_animno = 0;
+    while(true) {
+        sd_skip(r, 4);
+        animno = sd_read_ubyte(r);
+        if(animno >= 50 || !sd_reader_ok()) {
+            last_animno = animno;
+            break;
+        }
+
+        // TODO: Parse animation header here
+    }
+
+    // TODO: Read VGA Image
+    // TODO: Read palettes
+    // TODO: Read footer
+
+    // Close & return
+    sd_reader_close(r);
+    return data;
 }
 
-void bk_destroy(bk_file* bkfile) {
+int bk_save(const char* filename, bk_file *data) {
 
+}
+
+void bk_destroy(bk_file *data) {
+    free(data);
 }
