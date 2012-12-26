328da39243b71c76e645317f209bf37d0e65e1ce
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-26 21:41:43 +0200

    VGA Image decoding (not tested)

--- src/rgba_image.c
+++ src/rgba_image.c
@@ -1,11 +1,13 @@
 #include "rgba_image.h"
 #include <stdlib.h>
+#include <string.h>
 
 rgba_image* sd_create_rgba_image(unsigned int w, unsigned int h) {
     rgba_image *img = (rgba_image*)malloc(sizeof(rgba_image));
     img->data = (char*)malloc(w * h * 4);
     img->w = w;
     img->h = h;
+    memset(img->data, w*h*4, 0);
     return img;
 }
 
--- src/vga_image.c
+++ src/vga_image.c
@@ -1,5 +1,7 @@
 #include "vga_image.h"
 #include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
 
 vga_image* sd_vga_image_create(unsigned int w, unsigned int h) {
     vga_image *img = (vga_image*)malloc(sizeof(vga_image));
@@ -7,6 +9,7 @@ vga_image* sd_vga_image_create(unsigned int w, unsigned int h) {
     img->h = h;
     img->len = w*h;
     img->data = (char*)malloc(w*h);
+    memset(img->data, w*h, 0);
     return img;
 }
 
@@ -22,5 +25,22 @@ vga_image* sd_vga_image_encode(rgba_image *img, palette *pal, int remapping) {
 
 rgba_image* sd_vga_image_decode(vga_image *img, palette *pal, int remapping) {
     rgba_image *rgba = sd_create_rgba_image(img->w, img->h);
+    int pos = 0;
+    for(int y = img->h - 1; y >= 0; y--) {
+        for(int x = 0; x < img->w; x++) {
+            uint8_t b = img->data[y * img->w + x];
+            pos = ((y * img->w) + x) * 4;
+            if(remapping > -1) {
+                rgba->data[pos+0] = (uint8_t)pal->data[(uint8_t)pal->remaps[remapping][b]][0];
+                rgba->data[pos+1] = (uint8_t)pal->data[(uint8_t)pal->remaps[remapping][b]][1];
+                rgba->data[pos+2] = (uint8_t)pal->data[(uint8_t)pal->remaps[remapping][b]][2];
+            } else {
+                rgba->data[pos+0] = (uint8_t)pal->data[b][0];
+                rgba->data[pos+1] = (uint8_t)pal->data[b][1];
+                rgba->data[pos+2] = (uint8_t)pal->data[b][2];
+            }
+            rgba->data[pos+3] = 0;
+        }
+    }
     return rgba;
 }
