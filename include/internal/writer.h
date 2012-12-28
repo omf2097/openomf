8abc09a37a6d239eaa55aa3f8c1d5b979756987d
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-28 19:40:24 +0200

    Writer

--- include/internal/writer.h
+++ include/internal/writer.h
@@ -15,4 +15,21 @@ sd_writer* sd_writer_open(const char *file);
   */
 void sd_writer_close(sd_writer *writer);
 
+/**
+  * Returns the position of the file pointer
+  */
+long sd_writer_pos(sd_writer *writer);
+
+/**
+  * Write a buffer to file.
+  */
+int sd_write_buf(sd_writer *writer, char *buf, int len);
+
+void sd_write_ubyte(sd_writer *writer, uint8_t data);
+void sd_write_uword(sd_writer *writer, uint16_t data);
+void sd_write_udword(sd_writer *writer, uint32_t data);
+void sd_write_byte(sd_writer *writer, int8_t data);
+void sd_write_word(sd_writer *writer, int16_t data);
+void sd_write_dword(sd_writer *writer, int32_t data);
+
 #endif // _WRITER_H
--- src/internal/writer.c
+++ src/internal/writer.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct sd_writer_t {
     FILE *handle;
@@ -24,3 +25,40 @@ void sd_writer_close(sd_writer *writer) {
     fclose(writer->handle);
     free(writer);
 }
+
+long sd_writer_pos(sd_writer *writer) {
+    return ftell(writer->handle);
+}
+
+int sd_write_buf(sd_writer *writer, char *buf, int len) {
+    if(fwrite(buf, 1, len, writer->handle) != len) {
+        strcpy(writer->err, "Error: Wrote less than requested amount of bytes.");
+        return 0;
+    }
+    return 1;
+}
+
+void sd_write_ubyte(sd_writer *writer, uint8_t data) {
+    sd_write_buf(writer, (char*)&data, 1);
+}
+
+void sd_write_uword(sd_writer *writer, uint16_t data) {
+    sd_write_buf(writer, (char*)&data, 2);
+}
+
+void sd_write_udword(sd_writer *writer, uint32_t data) {
+    sd_write_buf(writer, (char*)&data, 4);
+}
+
+void sd_write_byte(sd_writer *writer, int8_t data) {
+    sd_write_buf(writer, (char*)&data, 1);
+}
+
+void sd_write_word(sd_writer *writer, int16_t data) {
+    sd_write_buf(writer, (char*)&data, 2);
+}
+
+void sd_write_dword(sd_writer *writer, int32_t data) {
+    sd_write_buf(writer, (char*)&data, 4);
+}
+
