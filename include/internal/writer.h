cbd09a6af53847d8d9f5e9e63353b71554c8a5bb
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-28 14:13:37 +0200

    Start a filewriter

--- include/internal/writer.h
+++ include/internal/writer.h
@@ -1,6 +1,18 @@
 #ifndef _WRITER_H
 #define _WRITER_H
 
+#include <stdint.h>
 
+typedef struct sd_writer_t sd_writer;
+
+/**
+  * Open file for writing. If file exists, it will be overwritten.
+  */
+sd_writer* sd_writer_open(const char *file);
+
+/**
+  * Close file.
+  */
+void sd_writer_close(sd_writer *writer);
 
 #endif // _WRITER_H
--- src/internal/writer.c
+++ src/internal/writer.c
@@ -1,2 +1,26 @@
 #include "internal/writer.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct sd_writer_t {
+    FILE *handle;
+    char err[64];
+} sd_writer;
+
+sd_writer* sd_writer_open(const char *file) {
+    sd_writer *writer = malloc(sizeof(sd_writer));
+
+    writer->handle = fopen(file, "w");
+    if(!writer->handle) {
+        free(writer);
+        return 0;
+    }
+
+    return writer;
+}
+
+void sd_writer_close(sd_writer *writer) {
+    fclose(writer->handle);
+    free(writer);
+}
