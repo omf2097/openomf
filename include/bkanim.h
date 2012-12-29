67379d96ba5e454a7c84a8e6a3fd10f9b760a054
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-29 23:15:00 +0200

    Add missing files

---
+++ include/bkanim.h
@@ -0,0 +1,27 @@
+#ifndef _BK_ANIMS
+#define _BK_ANIMS
+
+#include "animation.h"
+
+typedef struct sd_writer_t sd_writer;
+typedef struct sd_reader_t sd_reader;
+
+typedef struct sd_bk_anim_t {
+    uint8_t null;
+    uint8_t unknown_a;
+    uint8_t unknown_b;
+    uint8_t unknown_c;
+    uint16_t unknown_d;
+    uint8_t unknown_e;
+    uint16_t unknown_size;
+    char* unknown_data;
+
+    sd_animation *animation;
+} sd_bk_anim;
+
+sd_bk_anim* sd_bk_anim_create();
+void sd_bk_anim_delete(sd_bk_anim *bka);
+int sd_bk_anim_load(sd_reader *reader, sd_bk_anim *bka);
+void sd_bk_anim_save(sd_writer *writer, sd_bk_anim *bka);
+
+#endif // _BK_ANIMS
---
+++ src/bkanim.c
@@ -0,0 +1,54 @@
+#include "bkanim.h"
+#include "internal/reader.h"
+#include "internal/writer.h"
+#include <stdlib.h>
+
+sd_bk_anim* sd_bk_anim_create() {
+    sd_bk_anim *bka = (sd_bk_anim*)malloc(sizeof(sd_bk_anim));
+    bka->unknown_data = NULL;
+    bka->animation = NULL;
+    return bka;
+}
+
+void sd_bk_anim_delete(sd_bk_anim *bka) {
+    if(bka->unknown_data)
+        free(bka->unknown_data);
+    if(bka->animation)
+        sd_animation_delete(bka->animation);
+    free(bka);
+}
+
+int sd_bk_anim_load(sd_reader *r, sd_bk_anim *bka) {
+    // BK Specific animation header
+    bka->null = sd_read_ubyte(r);
+    bka->unknown_a = sd_read_ubyte(r);
+    bka->unknown_b = sd_read_ubyte(r);
+    bka->unknown_c = sd_read_ubyte(r);
+    bka->unknown_d = sd_read_uword(r);
+    bka->unknown_e = sd_read_ubyte(r);
+    bka->unknown_size = sd_read_uword(r);
+    bka->unknown_data = (char*)malloc(bka->unknown_size);
+    sd_read_buf(r, bka->unknown_data, bka->unknown_size);
+
+    // Initialize animation
+    bka->animation = sd_animation_create();
+    sd_animation_load(r, bka->animation);
+
+    // Return success if reader is still ok
+    return sd_reader_ok(r);
+}
+
+void sd_bk_anim_save(sd_writer *writer, sd_bk_anim *bka) {
+    // Write BK specific header
+    sd_write_ubyte(writer, bka->null);
+    sd_write_ubyte(writer, bka->unknown_a);
+    sd_write_ubyte(writer, bka->unknown_b);
+    sd_write_ubyte(writer, bka->unknown_c);
+    sd_write_uword(writer, bka->unknown_d);
+    sd_write_ubyte(writer, bka->unknown_e);
+    sd_write_uword(writer, bka->unknown_size);
+    sd_write_buf(writer, bka->unknown_data, bka->unknown_size);
+
+    // Write animation
+    sd_animation_save(writer, bka->animation);
+}
