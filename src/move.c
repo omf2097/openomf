09f9e96c59ff27ee1f9ef3c0be4bc45fd997a5f1
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-29 23:19:19 +0200

    Writer for sd_move

--- src/move.c
+++ src/move.c
@@ -2,6 +2,7 @@
 #include "internal/reader.h"
 #include "internal/writer.h"
 #include <stdlib.h>
+#include <string.h>
 
 sd_move* sd_move_create() {
     sd_move *move = (sd_move*)malloc(sizeof(sd_move));
@@ -35,5 +36,14 @@ int sd_move_load(sd_reader *r, sd_move *move) {
 }
 
 void sd_move_save(sd_writer *writer, sd_move *move) {
+    // Save animation
+    sd_animation_save(writer, &move->animation);
 
+    // Save move footer
+    sd_write_buf(writer, move->unknown, 21);
+    sd_write_buf(writer, move->move_string, 21);
+    uint16_t fs_size = strlen(move->footer_string);
+    sd_write_uword(writer, fs_size);
+    sd_write_buf(writer, move->footer_string, fs_size);
+    sd_write_ubyte(writer, 0);
 }
