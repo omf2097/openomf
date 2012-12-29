0f9a47610ada756816870fc4cbcb2a9c712ef0f6
Tuomas Virtanen <katajakasa@gmail.com>
2012-12-29 23:58:52 +0200

    Sound file parser start

---
+++ include/sounds.h
@@ -0,0 +1,14 @@
+#ifndef _SOUNDS_H
+#define _SOUNDS_H
+
+typedef struct sd_writer_t sd_writer;
+typedef struct sd_reader_t sd_reader;
+
+typedef struct sd_sound_t {
+    unsigned int len;
+    char *data;
+} sd_sound;
+
+
+
+#endif // _SOUNDS_H
---
+++ src/sounds.c
@@ -0,0 +1,5 @@
+#include "sounds.h"
+#include "internal/reader.h"
+#include "internal/writer.h"
+
+
