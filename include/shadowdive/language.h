efd2a3315c360e97a007735d5c0f117dcc06bda3
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-07 00:29:39 +0200

    Stubs for languagefileparser

--- include/shadowdive/language.h
+++ include/shadowdive/language.h
@@ -3,13 +3,18 @@
 
 typedef struct sd_lang_string_t {
     unsigned int _offset;
-    char description[33];
-    char string[129];
+    char *description;
+    char *string;
 } sd_lang_string;
 
 typedef struct sd_language_t {
     unsigned int string_count;
-    sd_lang_string *strings;
+    sd_lang_string **strings;
 } sd_language;
+
+sd_language* sd_language_create();
+void sd_language_delete(sd_language *language);
+int sd_language_load(sd_language *language, const char *filename);
+int sd_language_save(sd_language *language, const char *filename);
 
 #endif // _SD_LANGUAGE_H
--- src/language.c
+++ src/language.c
@@ -1,2 +1,23 @@
 #include "shadowdive/language.h"
+#include <stdlib.h>
 
+sd_language* sd_language_create() {
+    sd_language *language = (sd_language*)malloc(sizeof(sd_language));
+    language->string_count = 0;
+    language->strings = 0;
+    return language;
+}
+
+void sd_language_delete(sd_language *language) {
+    if(language) {
+        free(language);
+    }
+}
+
+int sd_language_load(sd_language *language, const char *filename) {
+    return 0;
+}
+
+int sd_language_save(sd_language *language, const char *filename) {
+    return 0;
+}
