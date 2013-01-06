72f222c28a2b3cf602571ff5fa3726fc79e17a7a
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-06 22:36:15 +0200

    Add test file

--- include/shadowdive/language.h
+++ include/shadowdive/language.h
@@ -1,5 +1,15 @@
 #ifndef _SD_LANGUAGE_H
 #define _SD_LANGUAGE_H
 
+typedef struct sd_lang_string_t {
+    unsigned int _offset;
+    char description[33];
+    char string[129];
+} sd_lang_string;
+
+typedef struct sd_language_t {
+    unsigned int string_count;
+    sd_lang_string *strings;
+} sd_language;
 
 #endif // _SD_LANGUAGE_H
