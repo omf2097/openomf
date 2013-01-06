afc93a45810253b0ba0a4f59f2b3128f3f657ced
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-06 16:26:38 +0200

    Unify small and big fonts. Wastes a few bytes, but easier to use.

--- include/shadowdive/fonts.h
+++ include/shadowdive/fonts.h
@@ -5,31 +5,18 @@ typedef struct sd_rgba_image_t sd_rgba_image;
 
 #include <stdint.h>
 
-typedef struct sd_char_big_t {
+typedef struct sd_char_t {
     char data[8];
-} sd_char_big;
+} sd_char;
 
-typedef struct sd_char_small_t {
-    char data[6];
-} sd_char_small;
+typedef struct sd_font_t {
+    unsigned int h;
+    sd_char chars[224];
+} sd_font;
 
-typedef struct sd_font_big_t {
-    sd_char_big chars[224];
-} sd_font_big;
-
-typedef struct sd_font_small_t {
-    sd_char_small chars[224];
-} sd_font_small;
-
-sd_font_small* sd_font_small_create();
-sd_font_big* sd_font_big_create();
-void sd_font_small_delete(sd_font_small *font);
-void sd_font_big_delete(sd_font_big *font);
-
-int sd_font_small_load(sd_font_small *font, const char *file);
-int sd_font_big_load(sd_font_big *font, const char *file);
-
-sd_rgba_image* sd_font_big_decode(sd_char_big *ch, uint32_t color);
-sd_rgba_image* sd_font_small_decode(sd_char_small *ch, uint32_t color);
+sd_font* sd_font_create();
+void sd_font_delete(sd_font *font);
+int sd_font_load(sd_font *font, const char *file, unsigned int font_h);
+sd_rgba_image* sd_font_decode(sd_font *font, uint8_t ch, uint8_t r, uint8_t g, uint8_t b);
 
 #endif // _FONTS_H
--- src/fonts.c
+++ src/fonts.c
@@ -6,30 +6,23 @@
 
 #include <stdio.h>
 
-sd_font_small* sd_font_small_create() {
-    return (sd_font_small*)malloc(sizeof(sd_font_small));
+sd_font* sd_font_create() {
+    return (sd_font*)malloc(sizeof(sd_font));
 }
 
-sd_font_big* sd_font_big_create() {
-    return (sd_font_big*)malloc(sizeof(sd_font_big));
-}
-
-void sd_font_small_delete(sd_font_small *font) {
+void sd_font_delete(sd_font *font) {
     if(font) { free(font); }
 }
 
-void sd_font_big_delete(sd_font_big *font) {
-    if(font) { free(font); }
-}
-
-int sd_font_big_load(sd_font_big *font, const char *file) {
+int sd_font_load(sd_font *font, const char *file, unsigned int font_h) {
     sd_reader *r = sd_reader_open(file);
     if(!r) {
         return SD_FILE_OPEN_ERROR;
     }
     
+    font->h = font_h;
     for(int i = 0; i < 224; i++) {
-        sd_read_buf(r, font->chars[i].data, 8);
+        sd_read_buf(r, font->chars[i].data, font_h);
         if(!sd_reader_ok(r)) {
             sd_reader_close(r);
             return SD_FILE_PARSE_ERROR;
@@ -40,32 +33,23 @@ int sd_font_big_load(sd_font_big *font, const char *file) {
     return SD_SUCCESS;
 }
 
-int sd_font_small_load(sd_font_small *font, const char *file) {
-    sd_reader *r = sd_reader_open(file);
-    if(!r) {
-        return SD_FILE_OPEN_ERROR;
-    }
-    
-    for(int i = 0; i < 224; i++) {
-        sd_read_buf(r, font->chars[i].data, 6);
-        if(!sd_reader_ok(r)) {
-            sd_reader_close(r);
-            return SD_FILE_PARSE_ERROR;
+sd_rgba_image* sd_font_decode(sd_font *font, uint8_t ch, uint8_t r, uint8_t g, uint8_t b) {
+    sd_rgba_image *o = sd_rgba_image_create(8, font->h);
+    int t = 0;
+    for(int i = 0; i < font->h; i++) {
+        for(int k = 7; k >= 0; k--) {
+            if(font->chars[ch].data[i] & (1 << k)) {
+                o->data[t++] = r;
+                o->data[t++] = g;
+                o->data[t++] = b;
+                o->data[t++] = 255;
+            } else {
+                o->data[t++] = 0;
+                o->data[t++] = 0;
+                o->data[t++] = 0;
+                o->data[t++] = 0;
+            }
         }
     }
-    
-    sd_reader_close(r);
-    return SD_SUCCESS;
-}
-
-sd_rgba_image* sd_font_big_decode(sd_char_big *ch, uint32_t color) {
-    sd_rgba_image *o = sd_rgba_image_create(8, 6);
-
-    return o;
-}
-
-sd_rgba_image* sd_font_small_decode(sd_char_small *ch, uint32_t color) {
-    sd_rgba_image *o = sd_rgba_image_create(8, 8);
-    
     return o;
 }
