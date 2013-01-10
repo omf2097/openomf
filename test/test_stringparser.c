d4fb25c6afea056568fa80a510575a10b719bea6
hunter <animehunter@gmail.com>
2013-01-11 10:12:38 +1100

    added an example to test callbacks in stringparser

--- test/test_stringparser.c
+++ test/test_stringparser.c
@@ -2,6 +2,57 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct tag_value_t {
+    int value;
+    int has_value;
+} tag_value;
+
+typedef struct anim_states_t {
+    tag_value blendstart; //bs
+    tag_value blendfinish; //bf
+    tag_value blendadditive; //br
+
+} anim_states;
+
+void anim_states_reset(anim_states *st) {
+    st->blendstart.has_value = 0;
+    st->blendfinish.has_value = 0;
+    st->blendadditive.has_value = 0;
+}
+
+void set_variable(sd_stringparser_cb_param *param) {
+    tag_value *v = param->userdata;
+    v->has_value = 1;
+    v->value = param->tag_value;
+}
+
+void test_state_variables(const char *anim_str) {
+    sd_stringparser *parser = sd_stringparser_create();
+    char err_msg[256];
+
+    int err = sd_stringparser_set_string(parser, anim_str);
+    if(err == 0) 
+    {
+        anim_states st;
+        sd_stringparser_set_cb(parser, "bs", set_variable, &st.blendstart);
+        sd_stringparser_set_cb(parser, "bf", set_variable, &st.blendfinish);
+        sd_stringparser_set_cb(parser, "br", set_variable, &st.blendadditive);
+
+        for(unsigned int tick=0;tick<1000;++tick) {
+            anim_states_reset(&st);
+            sd_stringparser_run(parser, tick);
+            if(st.blendadditive.has_value) printf("Tick %d: blend additive %d\n", tick, st.blendadditive.value);
+            if(st.blendstart.has_value) printf("Tick %d: blend start %d\n", tick, st.blendstart.value);
+            if(st.blendfinish.has_value) printf("Tick %d: blend finish %d\n", tick, st.blendfinish.value);
+        }
+    } else {
+        sd_get_error(err_msg, err);
+        printf("Animation string parser error: %s (%s)\n", err_msg, anim_str);
+    }
+
+    sd_stringparser_delete(parser);
+}
+
 void test_anim_string(sd_stringparser *parser, const char *str) {
     // execute anim and print result
     unsigned int ticks = 0;
@@ -37,7 +88,13 @@ int main(int argc, char **argv) {
 
     if (argc <= 1) {
         printf("Usage %s <AF filename>  <BK filename>\n", argv[0]);
+        printf("Usage %s --testvar\n", argv[0]);
         return 1;
+    }
+
+    if(strcmp(argv[1], "--testvar") == 0) {
+        test_state_variables("brA20-bs200B200-bf200C200");
+        return 0;
     }
 
     sd_stringparser *parser = sd_stringparser_create();
