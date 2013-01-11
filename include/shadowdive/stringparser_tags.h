fa08d01ee16eac20cec48947d01ab3dfa894e769
hunter <animehunter@gmail.com>
2013-01-12 09:10:59 +1100

    added some more tag defines

--- include/shadowdive/stringparser_tags.h
+++ include/shadowdive/stringparser_tags.h
@@ -4,5 +4,26 @@
 /* Tags for callbacks */
 #define SD_CB_MUSIC "smo"
 #define SD_CB_SOUND "s"
+#define SD_CB_JUMP_TICK "d"
+#define SD_CB_BLEND_START "bs"
+#define SD_CB_BLEND_FINISH "bf"
+#define SD_CB_BLEND_ADDITIVE "br"
+#define SD_CB_CREATE_ANIMATION "m"
+#define SD_CB_DESTROY_ANIMATION "md"
+#define SD_CB_ANIMATION_X_POSITION "mx"
+#define SD_CB_ANIMATION_Y_POSITION "my"
+#define SD_CB_APPLY_DAMAGE "cp"
+#define SD_CB_FLIP_SPRITE_VERTICALLY "f"
+#define SD_CB_FLIP_SPRITE_HORIZONTALLY "r"
+
+#define SD_CB_DECREMENT_X "x-"
+#define SD_CB_INCREMENT_X "x+"
+#define SD_CB_INTERPOLATE_X "x="
+#define SD_CB_SET_X "x"
+
+#define SD_CB_DECREMENT_Y "y-"
+#define SD_CB_INCREMENT_Y "y+"
+#define SD_CB_INTERPOLATE_Y "y="
+#define SD_CB_SET_Y "y"
 
 #endif
