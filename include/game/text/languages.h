6495409f3a87814429a1e9720056587b09e181f8
Tuomas Virtanen <katajakasa@gmail.com>
2013-01-20 09:11:18 +0200

    Some comment changes, function stubs

--- include/game/text/languages.h
+++ include/game/text/languages.h
@@ -7,4 +7,19 @@
 * texture with the help of text.h ?
 */
 
+typedef struct lang_t lang;
+
+struct lang_t {
+    // Something here ...
+};
+
+void lang_create(lang *lang);
+void lang_free(lang *lang);
+
+// Return 0 on success
+int lang_load(lang *lang, const char *filename);
+
+// Maybe something like this ?
+const char* lang_get(lang *lang, unsigned int textid);
+
 #endif // _LANGUAGES_H
--- include/game/text/text.h
+++ include/game/text/text.h
@@ -3,6 +3,9 @@
 
 /*
 * This file should load fonts and be able to render given text on a certain size texture.
+* We might also want some cache for textures created here (for speed).
 */
+
+
 
 #endif // _TEXT_H
