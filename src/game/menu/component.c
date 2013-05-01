8bf109520fb0cdb88bcca07a01c649beaa690f75
Tuomas Virtanen <katajakasa@gmail.com>
2013-05-01 08:11:36 +0300

    Add missing file

---
+++ src/game/menu/component.c
@@ -0,0 +1,27 @@
+#include "game/menu/component.h"
+#include <stdlib.h>
+#include "utils/log.h"
+
+void component_create(component *c) {
+    c->x = 0;
+    c->y = 0;
+    c->w = 0;
+    c->h = 0;
+    c->selected = 0;
+    c->obj = NULL;
+    c->render = NULL;
+    c->event = NULL;
+    c->layout = component_layout;
+    c->tick = NULL;
+}
+
+void component_free(component *c) {
+
+}
+
+void component_layout(component *c, int x, int y, int w, int h) {
+    c->x = x;
+    c->y = y;
+    c->w = w;
+    c->h = h;
+}
