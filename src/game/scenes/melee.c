92fa551acd020761b37b8e5e9e25a076ee3a4915
Andrew Thompson <andrew@hijacked.us>
2013-05-08 00:28:00 -0400

    Add background highlight of selected HAR to melee screen

--- src/game/scenes/melee.c
+++ src/game/scenes/melee.c
@@ -16,18 +16,17 @@ int column_a, column_b; // 0-4
 
 struct players_t {
     sd_sprite *sprite;
-    /*texture *tex;*/
 };
 
 struct players_t players[10];
 struct players_t players_big[10];
-struct players_t hars[10];
 
 int player_id_a, player_id_b;
 
 texture feh;
 texture bleh;
 texture select_hilight;
+texture harportraits[10];
 unsigned int ticks, hartick;
 unsigned int pulsedir;
 
@@ -38,6 +37,31 @@ void melee_switch_animation(scene *scene, animationplayer *harplayer, int id) {
     animationplayer_free(harplayer);
     animationplayer_create(harplayer, id, array_get(&scene->animations, id));
     animationplayer_run(harplayer);
+}
+
+// extract part of a sprite as a new sprite
+// we need this because the HAR portraits are one single sprite, unlike the player portraits
+// so we need to chunk them up into individual sprites and strip out the black background
+sd_rgba_image* sub_sprite(sd_sprite *sprite, sd_palette *pal, int x, int y, int w, int h) {
+    sd_rgba_image *img = 0;
+    sd_rgba_image *out = sd_rgba_image_create(w, h);
+    img = sd_sprite_image_decode(sprite->img, pal, -1);
+    for(int i = y; i < y+h; i++) {
+        for(int j = x; j < x+w; j++) {
+            int offset = (i*sprite->img->w*4)+(j*4);
+            int local_offset = ((i-y)*w*4)+((j-x)*4);
+            out->data[local_offset]   = (char)img->data[offset];
+            out->data[local_offset+1] = (char)img->data[offset+1];
+            out->data[local_offset+2] = (char)img->data[offset+2];
+            if (!out->data[local_offset] && !out->data[local_offset+1] && !out->data[local_offset+2]) {
+                // all three colors are black, set the pixel to be transparent!
+                out->data[local_offset+3] = 0;
+            } else {
+                out->data[local_offset+3] = (char)img->data[offset+3];
+            }
+        }
+    }
+    return out;
 }
 
 // Init menus
@@ -55,6 +79,12 @@ int melee_init(scene *scene) {
         players[i].sprite = scene->bk->anims[3]->animation->sprites[i];
         DEBUG("found sprite %d x %d at %d, %d", players[i].sprite->img->w, players[i].sprite->img->h, players[i].sprite->pos_x, players[i].sprite->pos_y);
         players_big[i].sprite = scene->bk->anims[4]->animation->sprites[i];
+
+        int row = i / 5;
+        int col = i % 5;
+        sd_rgba_image * out = sub_sprite(scene->bk->anims[1]->animation->sprites[0], scene->bk->palettes[0], (62*col), (42*row), 51, 36);
+        texture_create(&harportraits[i], out->data, 51, 36);
+        sd_rgba_image_delete(out);
     }
     menu_background_create(&feh, 90, 61);
     menu_background_create(&bleh, 160, 43);
@@ -102,6 +132,12 @@ int melee_event(scene *scene, SDL_Event *event) {
         switch (event->key.keysym.sym) {
             case SDLK_ESCAPE:
                 if (selection == 1) {
+                    // restorethe player selection
+                    column_a = player_id_a % 5;
+                    row_a = player_id_a / 5;
+                    column_b = player_id_b % 5;
+                    row_b = player_id_b / 5;
+
                     selection = 0;
                 } else {
                     scene->next_id = SCENE_MENU;
@@ -189,22 +225,26 @@ int melee_event(scene *scene, SDL_Event *event) {
     return 0;
 }
 
-void melee_render(scene *scene) {
-    animation *ani;
-    int current_a = 5*row_a + column_a;
-    int current_b = 5*row_b + column_b;
-
-    if (selection == 0) {
-        video_render_sprite_flip(&feh, 70, 0, BLEND_ALPHA, FLIP_NONE);
-        video_render_sprite_flip(&bleh, 0, 62, BLEND_ALPHA, FLIP_NONE);
-    }
-    if (scene->player2.selectable && current_a == current_b) {
+void render_highlights(scene *scene) {
+    if (scene->player2.selectable && row_a == row_b && column_a == column_b) {
         video_render_char(&select_hilight, 11 + (62*column_a), 115 + (42*row_a), 250 - ticks , 0, 250 - ticks);
     } else {
         if (scene->player2.selectable) {
             video_render_char(&select_hilight, 11 + (62*column_b), 115 + (42*row_b), 0 , 0, 250 - ticks);
         }
         video_render_char(&select_hilight, 11 + (62*column_a), 115 + (42*row_a), 250 - ticks, 0, 0);
+    }
+}
+
+void melee_render(scene *scene) {
+    animation *ani;
+    int current_a = 5*row_a + column_a;
+    int current_b = 5*row_b + column_b;
+
+
+    if (selection == 0) {
+        video_render_sprite_flip(&feh, 70, 0, BLEND_ALPHA, FLIP_NONE);
+        video_render_sprite_flip(&bleh, 0, 62, BLEND_ALPHA, FLIP_NONE);
     }
     ani = array_get(&scene->animations, 5);
     if (scene->player2.selectable) {
@@ -213,32 +253,47 @@ void melee_render(scene *scene) {
         video_render_sprite_flip(array_get(&ani->sprites, 1), 162, 0, BLEND_ALPHA, FLIP_NONE);
     }
     if (selection == 0) {
+        render_highlights(scene);
         for(int i = 0; i < 10; i++) {
             ani = array_get(&scene->animations, 3);
 
             video_render_sprite_flip(array_get(&ani->sprites, i), players[i].sprite->pos_x, players[i].sprite->pos_y, BLEND_ALPHA, FLIP_NONE);
 
             if (i == current_a) {
+                // render the big portrait
                 ani = array_get(&scene->animations, 4);
                 video_render_sprite_flip(array_get(&ani->sprites, i), players_big[i].sprite->pos_x, players_big[i].sprite->pos_y, BLEND_ALPHA, FLIP_NONE);
             }
 
             if (scene->player2.selectable && i == current_b) {
+                // render the big portrait
                 ani = array_get(&scene->animations, 4);
                 video_render_sprite_flip(array_get(&ani->sprites, i), 320-(players_big[i].sprite->img->w + players_big[i].sprite->pos_x), players_big[i].sprite->pos_y, BLEND_ALPHA, FLIP_HORIZONTAL);
             }
         }
     } else {
+        // render the stupid unselected HAR portraits before anything
+        // so we can render anything else on top of them
         sd_sprite *sprite = scene->bk->anims[1]->animation->sprites[0];
         ani = array_get(&scene->animations, 1);
         video_render_sprite_flip(array_get(&ani->sprites, 0), sprite->pos_x, sprite->pos_y, BLEND_ALPHA, FLIP_NONE);
+
+        render_highlights(scene);
+
+        // currently selected player
         ani = array_get(&scene->animations, 4);
         video_render_sprite_flip(array_get(&ani->sprites, player_id_a), players_big[player_id_a].sprite->pos_x, players_big[player_id_a].sprite->pos_y, BLEND_ALPHA, FLIP_NONE);
 
+        //currently selected HAR
+        video_render_sprite_flip(&harportraits[current_a], 11 + (62*column_a), 115 + (42*row_a), BLEND_ALPHA, FLIP_NONE);
         animationplayer_render(&harplayer_a);
         if (scene->player2.selectable) {
+
+            // currently selected player
             video_render_sprite_flip(array_get(&ani->sprites, player_id_b), 320-(players_big[player_id_b].sprite->img->w + players_big[player_id_b].sprite->pos_x),
                     players_big[player_id_b].sprite->pos_y, BLEND_ALPHA, FLIP_HORIZONTAL);
+            // currently selected HAR
+            video_render_sprite_flip(&harportraits[current_b], 11 + (62*column_b), 115 + (42*row_b), BLEND_ALPHA, FLIP_NONE);
             animationplayer_render(&harplayer_b);
         }
     }
