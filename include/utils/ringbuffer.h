c3f7ffa898682d650a51a32e5293fedb763f9242
Tuomas Virtanen <katajakasa@gmail.com>
2013-07-31 23:36:24 +0300

    Make ringbuffer api similar to other data containers in utils

--- include/utils/ringbuffer.h
+++ include/utils/ringbuffer.h
@@ -15,13 +15,74 @@ typedef struct {
     char* data;
 } ringbuffer;
 
-ringbuffer* rb_create(int size);
-void rb_delete(ringbuffer* rb);
+/**
+  * Creates a new ringbuffer with the given size.
+  * @param size Size for the new ringbuffer
+  * @return Ringbuffer handle
+  */
+void rb_create(ringbuffer *rb, int size);
+
+/**
+  * Deletes the given ringbuffer.
+  * @param rb Ringbuffer to be deleted.
+  */
+void rb_free(ringbuffer *rb);
+
+/**
+  * Writes to the given ringbuffer. If given length is larger than the amount
+  * the ringbuffer can fit, only the data that fits will be written.
+  * @param rb Ringbuffer to write to.
+  * @param data Data to write
+  * @param len Data length
+  * @return Amount of data that was actually written.
+  */
 int rb_write(ringbuffer *rb, char* data, int len);
+
+/**
+  * Reads data from ringbuffer. If ringbuffer has less data than was requested,
+  * only the available data will be read.
+  * @param rb Ringbuffer to read from.
+  * @param data Buffer to read into.
+  * @param len How much data do we want
+  * @return Amount of data that was actually read.
+  */
 int rb_read(ringbuffer *rb, char* data, int len);
+
+/**
+  * Peeks into the given ringbuffer. Technically same as rb_read, but does not advance
+  * the internal position pointer. In other words, you may peek as many times as you wish,
+  * and will always get the same data.
+  * @param rb Ringbuffer to peek into.
+  * @param data Buffer to read into
+  * @param len How much data do we need
+  * @return Amount of data actually read
+  */
 int rb_peek(ringbuffer *rb, char* data, int len);
+
+/**
+  * Advances the internal position counter by given amount. Note that counter can only be
+  * advanced by the amount of unreadable data in ringbuffer.
+  * @param rb Ringbuffer to handle
+  * @param len How much should the position counter be increased
+  * @return How much the position counter was actually increased.
+  */
 int rb_advance(ringbuffer *rb, int len);
+
+/**
+  * Returns the current length of the Ringbuffer. In other words, how much data
+  * the ringbuffer contains
+  * @param rb Ringbuffer to handle
+  * @return Data in ringbuffer (in bytes).
+  */
 int rb_length(ringbuffer *rb);
+
+
+/**
+  * Returns the size of the ringbuffer. In other words, the maximum amount of data
+  * the ringbuffer can hold.
+  * @param rb Ringbuffer to handle
+  * @return Size of the ringbuffer
+  */
 int rb_size(ringbuffer *rb);
 
 #endif // _RINGBUFFER_H
--- src/utils/ringbuffer.c
+++ src/utils/ringbuffer.c
@@ -6,45 +6,23 @@
 */
 
 #include "utils/ringbuffer.h"
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-/**
-  * Creates a new ringbuffer with the given size.
-  * @param size Size for the new ringbuffer
-  * @return Ringbuffer handle
-  */
-ringbuffer* rb_create(int size) {
-    ringbuffer *rb;
-    rb = malloc(sizeof(ringbuffer));
+void rb_create(ringbuffer* rb, int size) {
     rb->size = size;
     rb->len = 0;
     rb->wpos = 0;
     rb->rpos = 0;
     rb->data = malloc(size);
-    return rb;
 }
 
-/**
-  * Deletes the given ringbuffer.
-  * @param rb Ringbuffer to be deleted.
-  */
-void rb_delete(ringbuffer* rb) {
+void rb_free(ringbuffer* rb) {
     if(rb == 0) return;
     free(rb->data);
-    free(rb);
 }
 
-/**
-  * Writes to the given ringbuffer. If given length is larger than the amount
-  * the ringbuffer can fit, only the data that fits will be written.
-  * @param rb Ringbuffer to write to.
-  * @param data Data to write
-  * @param len Data length
-  * @return Amount of data that was actually written.
-  */
 int rb_write(ringbuffer *rb, char* data, int len) {
     int k;
     len = (len > (rb->size - rb->len)) ? (rb->size - rb->len) : len;
@@ -66,14 +44,6 @@ int rb_write(ringbuffer *rb, char* data, int len) {
     return 0;
 }
 
-/**
-  * Reads data from ringbuffer. If ringbuffer has less data than was requested,
-  * only the available data will be read.
-  * @param rb Ringbuffer to read from.
-  * @param data Buffer to read into.
-  * @param len How much data do we want
-  * @return Amount of data that was actually read.
-  */
 int rb_read(ringbuffer *rb, char* data, int len) {
     int k;
     len = (len > rb->len) ? rb->len : len;
@@ -95,15 +65,6 @@ int rb_read(ringbuffer *rb, char* data, int len) {
     return 0;
 }
 
-/**
-  * Peeks into the given ringbuffer. Technically same as rb_read, but does not advance
-  * the internal position pointer. In other words, you may peek as many times as you wish,
-  * and will always get the same data.
-  * @param rb Ringbuffer to peek into.
-  * @param data Buffer to read into
-  * @param len How much data do we need
-  * @return Amount of data actually read
-  */
 int rb_peek(ringbuffer *rb, char* data, int len) {
     int k;
     len = (len > rb->len) ? rb->len : len;
@@ -120,13 +81,6 @@ int rb_peek(ringbuffer *rb, char* data, int len) {
     return 0;
 }
 
-/**
-  * Advances the internal position counter by given amount. Note that counter can only be
-  * advanced by the amount of unreadable data in ringbuffer.
-  * @param rb Ringbuffer to handle
-  * @param len How much should the position counter be increased
-  * @return How much the position counter was actually increased.
-  */
 int rb_advance(ringbuffer *rb, int len) {
     len = (len > rb->len) ? rb->len : len;
     if(rb->len > 0) {
@@ -140,22 +94,10 @@ int rb_advance(ringbuffer *rb, int len) {
     return 0;
 }
 
-/**
-  * Returns the current length of the Ringbuffer. In other words, how much data
-  * the ringbuffer contains
-  * @param rb Ringbuffer to handle
-  * @return Data in ringbuffer (in bytes).
-  */
 int rb_length(ringbuffer *rb) {
     return rb->len;
 }
 
-/**
-  * Returns the size of the ringbuffer. In other words, the maximum amount of data
-  * the ringbuffer can hold.
-  * @param rb Ringbuffer to handle
-  * @return Size of the ringbuffer
-  */
 int rb_size(ringbuffer *rb) {
     return rb->size;
 }
