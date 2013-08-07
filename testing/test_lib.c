f6f4b0badfee9f7c6d354d549f658823ee78c0ee
Tuomas Virtanen <katajakasa@gmail.com>
2013-08-07 05:33:07 +0300

    Small changes to tests

--- testing/test_lib.c
+++ testing/test_lib.c
@@ -2,18 +2,27 @@
 #include <CUnit/Basic.h>
 #include <shadowdive/shadowdive.h>
 
-sd_bk_file *file;
+sd_bk_file *bk_file;
+sd_af_file *af_file;
 
 void test_sd_bk_create(void) {
-    file = sd_bk_create();
-    CU_ASSERT(file != NULL);
+    bk_file = sd_bk_create();
+    CU_ASSERT_PTR_NOT_NULL_FATAL(bk_file);
 }
 
 void test_sd_bk_delete(void) {
-    if(file != NULL) {
-        sd_bk_delete(file);
-        CU_ASSERT(file == NULL);
-    }
+    sd_bk_delete(bk_file);
+    //CU_ASSERT_PTR_NULL(bk_file);
+}
+
+void test_sd_af_create(void) {
+    af_file = sd_af_create();
+    CU_ASSERT_PTR_NOT_NULL_FATAL(af_file);
+}
+
+void test_sd_af_delete(void) {
+    sd_af_delete(af_file);
+    //CU_ASSERT_PTR_NULL(af_file);
 }
 
 int main(int argc, char **argv) {
@@ -32,6 +41,8 @@ int main(int argc, char **argv) {
     // Add tests
     if(CU_add_test(suite, "test of sd_bk_create", test_sd_bk_create) == NULL) { goto end; }
     if(CU_add_test(suite, "test of sd_bk_delete", test_sd_bk_delete) == NULL) { goto end; }
+    if(CU_add_test(suite, "test of sd_af_create", test_sd_af_create) == NULL) { goto end; }
+    if(CU_add_test(suite, "test of sd_af_delete", test_sd_af_delete) == NULL) { goto end; }
 
     // Run tests
     CU_basic_set_mode(CU_BRM_VERBOSE);
