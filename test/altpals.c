98dcd7bdc576bcf38bbbe692cd35048914277c1a
Andrew Thompson <andrew@hijacked.us>
2013-09-24 20:24:54 -0400

    Add simple altpals exporter to gimp palettes

---
+++ test/altpals.c
@@ -0,0 +1,30 @@
+#include <shadowdive/shadowdive.h>
+#include <shadowdive/internal/reader.h>
+#include <shadowdive/internal/writer.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+
+int main(int argc, char **argv) {
+    char buf[256];
+
+    if (argc != 2) {
+        printf("Usage %s <input>\n", argv[0]);
+        return 1;
+    }
+ 
+    sd_reader *r = sd_reader_open(argv[1]);
+    if(!r) {
+        return SD_FILE_OPEN_ERROR;
+    }
+
+    sd_palette **palettes = malloc(11 * sizeof(sd_palette*));
+    for(uint8_t i = 0; i < 11; i++) {
+        palettes[i] = (sd_palette*)malloc(sizeof(sd_palette));
+        sd_palette_load(r, palettes[i]);
+        sprintf(buf, "altpal%d.pal", i);
+        sd_palette_to_gimp_palette(buf, palettes[i]);
+    }
+    return 0;
+}
