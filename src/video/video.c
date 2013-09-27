54c1e1ebc3a37e2c9f539b1b326689df251bdb53
Tuomas Virtanen <katajakasa@gmail.com>
2013-09-27 04:50:14 +0300

    Oops, don't enable 3.0 context quite yet :P

--- src/video/video.c
+++ src/video/video.c
@@ -37,8 +37,8 @@ int video_init(int window_w, int window_h, int fullscreen, int vsync) {
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,   24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);                                               
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);   
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);                                               
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);   
 
     // Open window
     window = SDL_CreateWindow(
