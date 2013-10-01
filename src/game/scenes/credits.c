c17de07098399bdb4e4941c97cf7032cb71887db
Tuomas Virtanen <katajakasa@gmail.com>
2013-10-01 21:56:14 +0300

    Timeouts for intro and credits

--- src/game/scenes/credits.c
+++ src/game/scenes/credits.c
@@ -1,6 +1,12 @@
+#include <stdlib.h>
+
 #include "game/scenes/credits.h"
 #include "resources/ids.h"
 #include "game/game_state.h"
+
+typedef struct credits_local_t {
+    int ticks;
+} credits_local;
 
 int credits_event(scene *scene, SDL_Event *e) {
     switch(e->type) {
@@ -14,7 +20,24 @@ int credits_event(scene *scene, SDL_Event *e) {
     return 1;
 }
 
+void credits_tick(scene *scene) {
+    credits_local *local = scene_get_userdata(scene);
+    local->ticks++;
+    if(local->ticks > 4500) {
+        game_state_set_next(SCENE_NONE);
+    }
+}
+
+void credits_free(scene *scene) {
+    free(scene_get_userdata(scene));
+}
+
 int credits_create(scene *scene) {
+    credits_local *local = malloc(sizeof(credits_local));
+    local->ticks = 0;
+    scene_set_userdata(scene, local);
+    scene_set_tick_cb(scene, credits_tick);
+    scene_set_free_cb(scene, credits_free);
     scene_set_event_cb(scene, credits_event);
     return 0;
 }
--- src/game/scenes/intro.c
+++ src/game/scenes/intro.c
@@ -1,8 +1,14 @@
+#include <SDL2/SDL.h>
+#include <stdlib.h>
+
 #include "game/scenes/intro.h"
 #include "resources/ids.h"
 #include "game/game_state.h"
 #include "utils/log.h"
-#include <SDL2/SDL.h>
+
+typedef struct intro_local_t {
+    int ticks;
+} intro_local;
 
 int intro_event(scene *scene, SDL_Event *e) {
     switch(e->type) {
@@ -24,8 +30,25 @@ int intro_startup(scene *scene, int id) {
     return 0;
 }
 
+void intro_tick(scene *scene) {
+    intro_local *local = scene_get_userdata(scene);
+    local->ticks++;
+    if(local->ticks > 2500) {
+        game_state_set_next(SCENE_MENU);
+    }
+}
+
+void intro_free(scene *scene) {
+    free(scene_get_userdata(scene));
+}
+
 int intro_create(scene *scene) {
+    intro_local *local = malloc(sizeof(intro_local));
+    local->ticks = 0;
+    scene_set_userdata(scene, local);
+    scene_set_tick_cb(scene, intro_tick);
     scene_set_event_cb(scene, intro_event);
+    scene_set_free_cb(scene, intro_free);
     scene_set_startup_cb(scene, intro_startup);
     return 0;
 }
