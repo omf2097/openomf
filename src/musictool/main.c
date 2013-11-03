5699a185c135820fb360589d80bc9bf2d6d2baa6
Tuomas Virtanen <katajakasa@gmail.com>
2013-11-03 18:20:00 +0200

    Change musictool to use SDL2 instead of libao for playback.

--- src/musictool/main.c
+++ src/musictool/main.c
@@ -6,23 +6,49 @@
 
 #include <argtable2.h>
 #include <dumb/dumb.h>
-#include <ao/ao.h>
+#include <SDL2/SDL.h>
 #include <stdint.h>
 #include <string.h>
 
 #define DELTA (65536.0f / 44100)
 
+typedef struct _streamer {
+    DUH_SIGRENDERER *renderer;
+    int pos;
+    int size;
+    int ended;
+} Streamer;
+
+void stream(void* userdata, Uint8* stream, int len) {
+    Streamer *s = (Streamer*)userdata;
+    int before = s->pos;
+    duh_render(s->renderer, 16, 0, 1.0f, DELTA, len/4, stream);
+    s->pos = duh_sigrenderer_get_position(s->renderer);
+    if(s->pos >= s->size || s->pos < before) {
+        s->ended = 1;
+    }
+}
+
 int main(int argc, char *argv[]) {
+    DUH *src_data;
+    DUH_SIGRENDERER *src_renderer;
+    SDL_AudioSpec want, have;
+    SDL_AudioDeviceID dev;
+    Streamer streamer;
+
+    // Init libs
     dumb_register_stdfiles();
+    if(SDL_Init(SDL_INIT_AUDIO) != 0) {
+        fprintf(stderr, "Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
     // commandline argument parser options
     struct arg_lit *help = arg_lit0("h", "help", "print this help and exit");
     struct arg_lit *vers = arg_lit0("v", "version", "print version information and exit");
-    struct arg_lit *loop = arg_lit0("l", "loop", "Loop playback");
     struct arg_file *file = arg_file1("f", "file", "<file>", "File to play");
-    struct arg_file *export = arg_file0("e", "export", "<file>", "WAV file to export to");
     struct arg_end *end = arg_end(20);
-    void* argtable[] = {help,vers,loop,file,export,end};
+    void* argtable[] = {help,vers,file,end};
     const char* progname = "musictool";
     
     // Make sure everything got allocated
@@ -59,93 +85,56 @@ int main(int argc, char *argv[]) {
         goto exit_0;
     }
     
-    // Some libdumb related stuff
-    DUH *data;
-    DUH_SIGRENDERER *renderer;
-    
     // Load PSM file
-    data = dumb_load_psm(file->filename[0], 0);
-    if(!data) {
+    src_data = dumb_load_psm(file->filename[0], 0);
+    if(!src_data) {
         printf("Unable to load file!\n");
         goto exit_0;
     }
-    renderer = duh_start_sigrenderer(data, 0, 2, 0);
+    src_renderer = duh_start_sigrenderer(src_data, 0, 2, 0);
     printf("File loaded!\n");
     
-    // Initialize libao
-    ao_initialize();
+    // Streamer
+    streamer.renderer = src_renderer;
+    streamer.size = duh_get_length(src_data);
+    streamer.pos = 0;
+    streamer.ended = 0;
     
-    // Some required vars ...
-    int driver;
-    ao_device* output;
-    ao_sample_format format;
-        
-    // Set format
-    memset(&format, 0, sizeof(format));
-    format.bits = 16;
-    format.rate = 44100;
-    format.channels = 2;
-    format.byte_format = AO_FMT_LITTLE;
-    format.matrix = NULL;
-    
-    // Export vs. play
-    if(export->count > 0) {
-        printf("Attempting to export to '%s'.\n", export->filename[0]);
+    // SDL2
+    SDL_zero(want);
+    want.freq = 44100;
+    want.format = AUDIO_S16;
+    want.channels = 2;
+    want.samples = 4096;
+    want.callback = stream;
+    want.userdata = &streamer;
 
-        // Open output file
-        driver = ao_driver_id("wav");
-        output = ao_open_file(driver, export->filename[0], 0, &format, NULL);
-        if(output == NULL) {
-            printf("Could not open export file.");
+    dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0);
+    if(dev == 0) {
+        printf("Failed to open audio dev: %s\n", SDL_GetError());
+        goto exit_0;
+    } else {
+        if(have.format != want.format) {
+            printf("Could not get correct playback format.\n");
             goto exit_1;
         }
         
-        // Save to file
-        printf("Converting ... ");
-        long pos = 0;
-        long len = duh_get_length(data);
-        char buf[4096];
-        while(pos < len) {
-            duh_render(renderer, 16, 0, 1.0f, DELTA, 1024, buf);
-            if(ao_play(output, buf, 4096) == 0) {
-                printf(" Error while doing ao_play!\n");
-                goto exit_2;
-            }
-            pos = duh_sigrenderer_get_position(renderer);
+        printf("Starting playback ...\n");
+        SDL_PauseAudioDevice(dev, 0);
+        while(!streamer.ended) {
+            SDL_Delay(5000);
+            float seek = ((float)streamer.pos) / ((float)streamer.size) * 100.0f;
+            printf("%d%% played.\n", (int)seek);
         }
-        printf(" done.\n");
-    } else {
-        printf("Attempting to play file ...\n");
-    
-        // Open live output dev
-        driver = ao_default_driver_id();
-        output = ao_open_live(driver, &format, NULL);
-        if(output == NULL) {
-            printf("Unable to open output device!\n");
-            goto exit_1;
-        }
-    
-        // Play
-        printf("Playing ...");
-        long pos = 0;
-        long len = duh_get_length(data);
-        char buf[4096];
-        while(pos < len || loop->count > 0) {
-            duh_render(renderer, 16, 0, 1.0f, DELTA, 1024, buf);
-            ao_play(output, buf, 4096);
-            pos = duh_sigrenderer_get_position(renderer);
-        }
-        printf(" done.\n");
+        printf("All done.\n");
     }
-    
-    printf("Finished!\n");
-exit_2:
-    ao_close(output);
+
 exit_1:
-    ao_shutdown();
-    duh_end_sigrenderer(renderer);
-    unload_duh(data);
+    SDL_CloseAudioDevice(dev);
+    duh_end_sigrenderer(src_renderer);
+    unload_duh(src_data);
 exit_0:
     arg_freetable(argtable, sizeof(argtable)/sizeof(argtable[0]));
+    SDL_Quit();
     return 0;
 }
