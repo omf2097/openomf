9c0a2f00ec83264534269aab1b97dc7f78bd2a4d
Tuomas Virtanen <katajakasa@gmail.com>
2013-11-23 03:40:59 +0200

    Add some more test function stubs

--- testing/test_hashmap.c
+++ testing/test_hashmap.c
@@ -6,9 +6,28 @@ void test_hashmap_create(void) {
     return;
 }
 
+void test_hashmap_free(void) {
+    return;
+}
+
+void test_hashmap_delete(void) {
+    return;
+}
+
+void test_hashmap_insert(void) {
+    return;
+}
+
+void test_hashmap_get(void) {
+    return;
+}
+
+void test_hashmap_iterator(void) {
+    return;
+}
+
 int main(int argc, char **argv) {
     CU_pSuite suite = NULL;
-
     if(CU_initialize_registry() != CUE_SUCCESS) {
         return CU_get_error();
     }
@@ -20,8 +39,13 @@ int main(int argc, char **argv) {
     }
     
     // Add tests
-    if(CU_add_test(suite, "test of hashmap_create", test_hashmap_create) == NULL) { goto end; }
-
+    if(CU_add_test(suite, "Test for hashmap create", test_hashmap_create) == NULL) { goto end; }
+    if(CU_add_test(suite, "Test for hashmap delete operation", test_hashmap_delete) == NULL) { goto end; }
+    if(CU_add_test(suite, "Test for hashmap insert operation", test_hashmap_insert) == NULL) { goto end; }
+    if(CU_add_test(suite, "Test for hashmap get operation", test_hashmap_get) == NULL) { goto end; }
+    if(CU_add_test(suite, "Test for hashmap iterator", test_hashmap_iterator) == NULL) { goto end; }
+    if(CU_add_test(suite, "Test for hashmap free operation", test_hashmap_free) == NULL) { goto end; }
+    
     // Run tests
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
