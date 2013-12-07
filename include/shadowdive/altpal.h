a7fd20a14bbf71ee248c21b1ed320815671cd21b
Tuomas Virtanen <katajakasa@gmail.com>
2013-12-07 06:43:41 +0200

    Add altpals writer

--- include/shadowdive/altpal.h
+++ include/shadowdive/altpal.h
@@ -8,6 +8,7 @@ typedef struct sd_altpal_file_t {
 
 sd_altpal_file* sd_altpal_create();
 int sd_altpals_load(sd_altpal_file *ap, const char *filename);
+int sd_altpals_save(sd_altpal_file *ap, const char *filename);
 void sd_altpal_delete(sd_altpal_file *ap);
 
 #endif // _SD_ALTPAL_H
--- src/altpal.c
+++ src/altpal.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "shadowdive/internal/reader.h"
+#include "shadowdive/internal/writer.h"
 #include "shadowdive/error.h"
 #include "shadowdive/palette.h"
 #include "shadowdive/altpal.h"
-#include <stdlib.h>
-#include <string.h>
+
 
 sd_altpal_file* sd_altpal_create() {
     sd_altpal_file *ap = (sd_altpal_file*)malloc(sizeof(sd_altpal_file));
@@ -23,6 +26,21 @@ int sd_altpals_load(sd_altpal_file *ap, const char *filename) {
 
     // Close & return
     sd_reader_close(r);
+    return SD_SUCCESS;
+}
+
+int sd_altpals_save(sd_altpal_file *ap, const char *filename) {
+    sd_writer *w = sd_writer_open(filename);
+    if(!w) {
+        return SD_FILE_OPEN_ERROR;
+    }
+
+    for(uint8_t i = 0; i < 11; i++) {
+        sd_palette_save(w, &ap->palettes[i]);
+    }
+
+    // All done.
+    sd_writer_close(w);
     return SD_SUCCESS;
 }
 
