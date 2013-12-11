8e43bf77f1d03350ee5d780d8601442a0b8d99ab
Tuomas Virtanen <katajakasa@gmail.com>
2013-12-11 23:13:15 +0200

    Add some coordinate bounds checking to image.c

--- include/video/image.h
+++ include/video/image.h
@@ -14,15 +14,15 @@ int image_create_from_sd(image *img, sd_rgba_image *sdimg);
 void image_free(image *img);
 
 void image_clear(image *img, color c);
-void image_line(image *img, unsigned int x0, unsigned int y0, unsigned int x1, unsigned int y1, color c);
-void image_set_pixel(image *img, unsigned int x, unsigned int y, color c);
-void image_rect(image *img, unsigned int x, unsigned int y, unsigned int w, unsigned int h, color c);
+void image_line(image *img, int x0, int y0, int x1, int y1, color c);
+void image_set_pixel(image *img, int x, int y, color c);
+void image_rect(image *img, int x, int y, int w, int h, color c);
 void image_rect_bevel(image *img, 
-                      unsigned int x, unsigned int y, 
-                      unsigned int w, unsigned int h, 
+                      int x, int y, 
+                      int w, int h, 
                       color ctop, color cright, 
                       color cbottom, color left);
-void image_filled_rect(image *img, unsigned int x, unsigned int y, unsigned int w, unsigned int h, color c);
+void image_filled_rect(image *img, int x, int y, int w, int h, color c);
 void image_write_tga(image *img, const char *filename);
 
 #endif // _IMAGE_H
--- src/video/image.c
+++ src/video/image.c
@@ -6,6 +6,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define CHECK_COORD_BOUNDS(n, x) n = (n >= x ? (x-1) : (n < 0 ? 0 : n))
+
 typedef struct __attribute__ ((__packed__)) tga_header_t {
     uint8_t id;
     uint8_t colormap;
@@ -44,7 +46,11 @@ void image_clear(image *img, color c) {
 }
 
 // Bresenham
-void image_line(image *img, unsigned int x0, unsigned int y0, unsigned int x1, unsigned int y1, color c) {
+void image_line(image *img, int x0, int y0, int x1, int y1, color c) {
+    CHECK_COORD_BOUNDS(x0, img->w);
+    CHECK_COORD_BOUNDS(x1, img->w);
+    CHECK_COORD_BOUNDS(y0, img->h);
+    CHECK_COORD_BOUNDS(y1, img->h);
     int dx = abs(x1 - x0);
     int sx = (x0 < x1) ? 1 : -1;
     int dy = abs(y1 - y0);
@@ -61,15 +67,16 @@ void image_line(image *img, unsigned int x0, unsigned int y0, unsigned int x1, u
     }
 }
 
-void image_set_pixel(image *img, unsigned int x, unsigned int y, color c) {
-    if(x >= img->w || y >= img->h) return;
+void image_set_pixel(image *img, int x, int y, color c) {
+    CHECK_COORD_BOUNDS(x, img->w);
+    CHECK_COORD_BOUNDS(y, img->h);
     img->data[(y * img->w + x) * 4 + 0] = c.r;
     img->data[(y * img->w + x) * 4 + 1] = c.g;
     img->data[(y * img->w + x) * 4 + 2] = c.b;
     img->data[(y * img->w + x) * 4 + 3] = c.a;
 }
 
-void image_rect(image *img, unsigned int x, unsigned int y, unsigned int w, unsigned int h, color c) {
+void image_rect(image *img, int x, int y, int w, int h, color c) {
     image_line(img, x, y, x+w, y, c);
     image_line(img, x, y+h, x+w, y+h, c);
     image_line(img, x+w, y, x+w, y+h, c);
@@ -77,8 +84,8 @@ void image_rect(image *img, unsigned int x, unsigned int y, unsigned int w, unsi
 }
 
 void image_rect_bevel(image *img, 
-                      unsigned int x, unsigned int y, 
-                      unsigned int w, unsigned int h, 
+                      int x, int y, 
+                      int w, int h, 
                       color ctop, color cright, 
                       color cbottom, color cleft) {
     image_line(img, x, y, x+w, y, ctop);
@@ -87,7 +94,7 @@ void image_rect_bevel(image *img,
     image_line(img, x, y, x, y+h, cleft);
 }
 
-void image_filled_rect(image *img, unsigned int x, unsigned int y, unsigned int w, unsigned int h, color c) {
+void image_filled_rect(image *img, int x, int y, int w, int h, color c) {
     for(int my = y; my < y+h; my++) {
         for(int mx = x; mx < x+w; mx++) {
             image_set_pixel(img, mx, my, c);
