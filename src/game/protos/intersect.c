0b304230081eee7162862a5aacc9dc3c83ab5f69
Andrew Thompson <andrew@hijacked.us>
2013-12-11 12:45:58 -0500

    Improve collision detection

--- src/game/protos/intersect.c
+++ src/game/protos/intersect.c
@@ -47,12 +47,14 @@ int intersect_sprite_hitpoint(object *obj, object *target, int level, vec2i *poi
     vec2i size_a = object_get_size(obj);
     vec2i size_b = object_get_size(target);
 
-    if(object_get_direction(obj) == OBJECT_FACE_LEFT) {
-        pos_a.x = pos_a.x - obj->cur_sprite->pos.x - size_a.x;
+    if (object_get_direction(obj) == OBJECT_FACE_LEFT) {
+        pos_a.x = object_get_pos(obj).x + ((obj->cur_sprite->pos.x * -1) - size_a.x);
     }
-    if(object_get_direction(target) == OBJECT_FACE_LEFT) {
-        pos_b.x = pos_b.x - target->cur_sprite->pos.x - size_b.x;
+
+    if (object_get_direction(target) == OBJECT_FACE_LEFT) {
+        pos_b.x = object_get_pos(target).x + ((target->cur_sprite->pos.x * -1) - size_b.x);
     }
+
 
     // Iterate through hitpoints
     vec2i hcoords[level];
@@ -66,27 +68,44 @@ int intersect_sprite_hitpoint(object *obj, object *target, int level, vec2i *poi
 
         // Convert coords to target sprite local space
         int t = (object_get_direction(obj) == OBJECT_FACE_RIGHT)
-                ? (pos_a.x + cc->pos.x)
-                : (pos_a.x + (size_a.x - cc->pos.x));
+            ? (pos_a.x + cc->pos.x - obj->cur_sprite->pos.x)
+            : (pos_a.x + (size_a.x - cc->pos.x) + obj->cur_sprite->pos.x);
+
+        // convert global coordinates to local coordinates by compensating for the other player's position
         int xcoord = t - pos_b.x;
+        // Also note that the hit pixel position during jumps is innacurate because hacks
         int ycoord = (pos_a.y + size_a.y + cc->pos.y) - pos_b.y;
 
 // TODO: Use correct coordinates
 #ifdef DEBUGMODE
         image_set_pixel(
-            di, 
-            xcoord + pos_b.x, 
-            ycoord + pos_b.y, 
-            color_create(255,32,32,255));
+            di,
+            xcoord + pos_b.x,
+            ycoord + pos_b.y,
+            color_create(32,255,32,255));
+        image_rect(di, pos_b.x, pos_b.y, size_b.x, size_b.y, color_create(255,32,32,255));
+        image_rect(di, pos_a.x, pos_a.y, size_a.x, size_a.y, color_create(32,32,255,255));
 #endif
 
         // Make sure that the hitpixel is within the area of the target sprite
         if(xcoord < 0 || xcoord >= size_b.x) continue;
         if(ycoord < 0 || ycoord >= size_b.y) continue;
 
+#ifdef DEBUGMODE
+        image_set_pixel(
+            di,
+            xcoord + pos_b.x,
+            ycoord + pos_b.y,
+            color_create(255,32,32,255));
+#endif
+
         // Get hitpixel
         sd_vga_image *vga = (sd_vga_image*)target->cur_sprite->raw_sprite;
-        if(vga->stencil[ycoord * size_b.x + xcoord] == 1) {
+        int hitpoint = (ycoord * vga->w) + xcoord;
+        if (object_get_direction(target) == OBJECT_FACE_LEFT) {
+            hitpoint = (ycoord * vga->w) + (vga->w - xcoord);
+        }
+        if(vga->stencil[hitpoint] == 1) {
             hcoords[found++] = vec2i_create(xcoord, ycoord);
             if(found >= level) {
                 vec2f sum = vec2f_create(0,0);
@@ -100,5 +119,6 @@ int intersect_sprite_hitpoint(object *obj, object *target, int level, vec2i *poi
             }
         }
     }
+
     return 0;
 }
