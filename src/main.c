6e93c1b4f401c76eb4b4c80c4882c2fc8075939a
Andrew Thompson <andrew@hijacked.us>
2013-12-13 11:11:05 -0500

    Close joysticks enumerated at boot time

--- src/main.c
+++ src/main.c
@@ -109,7 +109,7 @@ int main(int argc, char *argv[]) {
         }
 
         if (SDL_JoystickGetAttached(joy)) {
-            /*SDL_JoystickClose(joy);*/
+            SDL_JoystickClose(joy);
         }
     }
 
