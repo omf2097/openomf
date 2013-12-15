d179f06812346fb6c210b14a7c499d57ed3bfdfc
Tuomas Virtanen <katajakasa@gmail.com>
2013-12-15 14:09:45 +0200

    Overall travis cleanups

--- travis/deps.sh
+++ travis/deps.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo add-apt-repository ppa:openmw/deps -y
-sudo apt-get update -qq
-
-if test $CC = gcc; then sudo apt-get install -qq gcc-4.8; fi
-if test $CC = gcc; then sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20; fi
-if test $CC = gcc; then sudo update-alternatives --config gcc; fi
-
 sudo apt-get --force-yes install libopenal-dev libglew-dev libsdl2-static-dev libconfuse-dev libenet-dev libogg-dev libvorbis-dev
 
 cd travis/
