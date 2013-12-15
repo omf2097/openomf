da5257227ba92abdd41ab064a8dda64be4837d5f
Tuomas Virtanen <katajakasa@gmail.com>
2013-12-15 13:53:42 +0200

    More travis fixes.

--- travis/deps.sh
+++ travis/deps.sh
@@ -4,8 +4,8 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo add-apt-repository ppa:openmw/deps -y
 sudo apt-get update -qq
 
-if [ "$CXX" = "gcc" ]; then sudo apt-get install -qq g++-4.8; fi
-if [ "$CXX" = "gcc" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+if [ "$CC" = "gcc" ]; then sudo apt-get install -qq gcc-4.8; fi
+if [ "$CC" = "gcc" ]; then export CC="gcc-4.8"; fi
 
 sudo apt-get --force-yes install libopenal-dev libglew-dev libsdl2-static-dev libconfuse-dev libenet-dev libogg-dev libvorbis-dev
 
