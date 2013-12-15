a9a60b785864882a156002226ff7771a5c1170c2
Tuomas Virtanen <katajakasa@gmail.com>
2013-12-15 13:42:50 +0200

    Travis updates

--- travis/deps.sh
+++ travis/deps.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:openmw/deps -y
+sudo apt-get update -qq
+
+if [ "$CXX" = "gcc" ]; then sudo apt-get install -qq g++-4.8; fi
+if [ "$CXX" = "gcc" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+
+sudo apt-get --force-yes install libopenal-dev libglew-dev libsdl2-static-dev libconfuse-dev libenet-dev libogg-dev libvorbis-dev
+
 cd travis/
 git clone https://github.com/omf2097/libShadowDive.git
 git clone https://bitbucket.org/kode54/dumb.git
