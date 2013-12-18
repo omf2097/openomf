04c59eef96e7dbb68bc56e31d08d31f40c8ff429
Tuomas Virtanen <katajakasa@gmail.com>
2013-12-18 06:59:50 +0200

    Add missing file. Meh.

---
+++ include/game/game_state_type.h
@@ -0,0 +1,18 @@
+#ifndef _GAME_STATE_TYPE_H
+#define _GAME_STATE_TYPE_H
+
+#include "utils/vector.h"
+
+typedef struct scene_t scene;
+typedef struct game_player_t game_player;
+
+typedef struct game_state_t {
+    unsigned int run;
+    unsigned int this_id, next_id;
+    unsigned int tick;
+    scene *sc;
+    vector objects;
+    game_player *players[2];
+} game_state;
+
+#endif // _GAME_STATE_TYPE_H
