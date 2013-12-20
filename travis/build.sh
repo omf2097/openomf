b6ff91ccc140c319e733b9dac49f76fd7d008b89
hunter <animehunter@gmail.com>
2013-12-20 13:01:19 +1100

    now it should work

--- travis/build.sh
+++ travis/build.sh
@@ -4,7 +4,7 @@ mkdir build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_OGGVORBIS=On -DTRAVIS=On ..
-make && find testing/ -type f -perm +111 -print0 | xargs -0 source && make clean
+make && testing/test_main && make clean
 
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_OGGVORBIS=On -DTRAVIS=On ..
-make && find testing/ -type f -perm +111 -print0 | xargs -0 source
+make && testing/test_main
