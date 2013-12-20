fbb97871e0c49d5e3b2cc4aee5606739e7cf6c95
hunter <animehunter@gmail.com>
2013-12-20 12:30:11 +1100

    enable cunit tests

--- travis/build.sh
+++ travis/build.sh
@@ -3,4 +3,6 @@
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_OGGVORBIS=On -DTRAVIS=On ..
-make
+make && find testing/ -type f -perm +111 -print0 | xargs -0 source && make clean
+cmake -DCMAKE_BUILD_TYPE=Release -DUSE_OGGVORBIS=On -DTRAVIS=On ..
+make && find testing/ -type f -perm +111 -print0 | xargs -0 source
