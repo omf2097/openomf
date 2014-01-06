ff778b91732ee347318ee84158dfad62c28b0227
hunter <animehunter@gmail.com>
2014-01-06 11:12:26 +1100

    fix sdl on travis

--- travis/deps.sh
+++ travis/deps.sh
@@ -2,6 +2,10 @@
 
 sudo apt-get --force-yes install libopenal-dev libglew-dev libsdl2-static-dev libconfuse-dev libenet-dev libogg-dev libvorbis-dev
 
+# remove the 2 lines below if travis updates sdl to 2.0.1+
+wget http://libsdl.org/release/SDL2-2.0.1.tar.gz -O - | tar xz
+(cd SDL2-2.0.1 && ./configure --prefix=$PREFIX && make -j5 && make install)
+  
 cd travis/
 git clone --depth=1 https://github.com/omf2097/libShadowDive.git
 git clone --depth=1 https://bitbucket.org/kode54/dumb.git
