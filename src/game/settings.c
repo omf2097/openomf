c6ad16623dfbec7c80e257f78ac6447a227042e1
hunter <animehunter@gmail.com>
2014-01-19 08:05:03 +1100

    enable vsync by default

--- src/game/settings.c
+++ src/game/settings.c
@@ -47,7 +47,7 @@ typedef struct struct_to_field_t {
 const field f_video[] = {
     F_INT(settings_video,  screen_w,      640),
     F_INT(settings_video,  screen_h,      400),
-    F_BOOL(settings_video, vsync,           0),
+    F_BOOL(settings_video, vsync,           1),
     F_BOOL(settings_video, fullscreen,      0),
     F_INT(settings_video,  scaling,         0),
     F_BOOL(settings_video, instant_console, 0)
