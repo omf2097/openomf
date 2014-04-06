c2c8e9eb7523913200ddffa994d47797f0a4161c
Tuomas Virtanen <katajakasa@gmail.com>
2014-04-06 17:13:02 +0300

    Update travis to use SDL 2.0.3

--- travis/deps.sh
+++ travis/deps.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get --force-yes install libopenal-dev libconfuse-dev libpng-dev libenet-dev libogg-dev libvorbis-dev libasound2-dev libxss-dev libpulse-dev
 
-# remove the 2 lines below and add libsdl2-static-dev to apt-get if travis updates sdl to 2.0.1+
-wget http://libsdl.org/release/SDL2-2.0.1.tar.gz -O - | tar xz
+# remove the 2 lines below and add libsdl2-dev to apt-get if travis updates sdl to 2.0.1+
+wget http://libsdl.org/release/SDL2-2.0.3.tar.gz -O - | tar xz
 cd SDL2-2.0.1 && ./configure --prefix=$PREFIX && make -j5 && sudo make install
   
 cd travis/
