fa8aa71b11ef7851e89cffa6091482a7883217fd
Tuomas Virtanen <katajakasa@gmail.com>
2014-04-27 19:16:31 +0300

    Don't use submodules for Travis build

--- travis/build.sh
+++ travis/build.sh
@@ -3,8 +3,8 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_OGGVORBIS=On -DUSE_PNG=On -DTRAVIS=On ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_SUBMODULES=Off -DUSE_OGGVORBIS=On -DUSE_PNG=On -DTRAVIS=On ..
 make && make test && make clean
 
-cmake -DCMAKE_BUILD_TYPE=Release -DUSE_OGGVORBIS=On -DUSE_PNG=On -DTRAVIS=On ..
+cmake -DCMAKE_BUILD_TYPE=Release -DUSE_SUBMODULES=Off -DUSE_OGGVORBIS=On -DUSE_PNG=On -DTRAVIS=On ..
 make && make test
