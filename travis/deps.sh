c15f0e4ae7bbcaf535255d1f71b5f1aac52c2dd0
Tuomas Virtanen <katajakasa@gmail.com>
2014-04-29 18:55:04 +0300

    Some Travis tweaks to use SDL2 from repo instead of having to compile it.

--- travis/deps.sh
+++ travis/deps.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-sudo apt-get --force-yes install libopenal-dev libconfuse-dev libpng-dev libenet-dev libogg-dev libvorbis-dev libasound2-dev libxss-dev libpulse-dev
-
 # remove the 2 lines below and add libsdl2-dev to apt-get if travis updates sdl to 2.0.1+
-wget http://libsdl.org/release/SDL2-2.0.3.tar.gz -O - | tar xz
-cd SDL2-2.0.3 && ./configure --prefix=$PREFIX && make -j5 && sudo make install
+#wget http://libsdl.org/release/SDL2-2.0.3.tar.gz -O - | tar xz
+#cd SDL2-2.0.3 && ./configure --prefix=$PREFIX && make -j5 && sudo make install
   
 cd travis/
 git clone --depth=1 https://github.com/omf2097/libShadowDive.git
