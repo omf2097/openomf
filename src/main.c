1cacd1e181608223ea5709ddd1ca86f0fb00e722
animehunter <animehunter@gmail.com>
2014-05-02 09:16:16 +1000

    Bump up version

--- src/main.c
+++ src/main.c
@@ -94,7 +94,7 @@ int main(int argc, char *argv[]) {
     // Check arguments
     if(argc >= 2) {
         if(strcmp(argv[1], "-v") == 0) {
-            printf("OpenOMF v0.6\n");
+            printf("OpenOMF v0.6.5\n");
             printf("Source available at https://github.com/omf2097/ (MIT License)\n");
             printf("(C) 2097 Tuomas Virtanen, Andrew Thompson, Hunter and others\n");
             goto exit_0;
