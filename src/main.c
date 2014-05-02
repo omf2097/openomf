09693dadae25a7ee2e9f1ba17ec1e759661f611f
Tuomas Virtanen <katajakasa@gmail.com>
2014-05-02 03:36:32 +0300

    Move messagebox handling to a separate file

--- src/main.c
+++ src/main.c
@@ -14,35 +14,13 @@
 #include "shadowdive/stringparser.h"
 #include "utils/log.h"
 #include "utils/random.h"
+#include "utils/msgbox.h"
 #include "game/game_state.h"
 #include "game/utils/settings.h"
 #include "resources/global_paths.h"
 #include "resources/ids.h"
 #include "plugins/plugins.h"
 #include "controller/gamecontrollerdb.h"
-
-#ifdef STANDALONE_SERVER
-void err_msgbox(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    fprintf(stderr, "... Exiting\n");
-    va_end(args);
-}
-#else
-char err_msgbox_buffer[1024];
-void err_msgbox(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    vsnprintf(err_msgbox_buffer, sizeof(err_msgbox_buffer), fmt, args);
-    if(SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", err_msgbox_buffer, NULL) != 0) {
-        // if the message box failed, fallback to fprintf
-        vfprintf(stderr, fmt, args);
-        fprintf(stderr, "... Exiting\n");
-    }
-    va_end(args);
-}
-#endif
 
 int main(int argc, char *argv[]) {
     // Get path
