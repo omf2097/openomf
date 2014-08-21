3cd6e18ca4440d0bafaf791d58b90a05a6d3cef5
Tuomas Virtanen <katajakasa@gmail.com>
2014-08-21 19:06:45 +0300

    Soundtool: general updates
    - Add support for importing AU files
    - Fix support for exporting AU files
    - Add support for saving the soundsfile
    - Lots of sanity and file type checks

--- src/soundtool/main.c
+++ src/soundtool/main.c
@@ -40,10 +40,14 @@ int main(int argc, char *argv[]) {
     struct arg_lit *help = arg_lit0("h", "help", "print this help and exit");
     struct arg_lit *vers = arg_lit0("v", "version", "print version information and exit");
     struct arg_file *file = arg_file1("f", "file", "<file>", "SOUNDS.DAT file");
-    struct arg_int *sid = arg_int0("s", "sid", "<int>", "SampleID to play");
-    struct arg_int *sampleprint = arg_int0("p", "print", "<int>", "Samples to print");
+    struct arg_file *output = arg_file0("o", "output", "<file>", "Output sounds file");
+    struct arg_int *sid = arg_int0("s", "sound", "<int>", "Sound ID");
+    struct arg_int *sampleprint = arg_int0(NULL, "print", "<int>", "Print first n bytes from selected sound");
+    struct arg_lit *play = arg_lit0("p", "play", "Play selected sound");
+    struct arg_file *export = arg_file0("e", "export", "<file>", "Export selected sound to AU file");
+    struct arg_file *import = arg_file0("i", "import", "<file>", "Import selected sound from AU file");
     struct arg_end *end = arg_end(20);
-    void* argtable[] = {help,vers,file,sid,sampleprint,end};
+    void* argtable[] = {help,vers,file,output,sid,sampleprint,play,export,import,end};
     const char* progname = "soundtool";
     
     // Make sure everything got allocated
@@ -79,7 +83,7 @@ int main(int argc, char *argv[]) {
         printf("Try '%s --help' for more information.\n", progname);
         goto exit_0;
     }
-    
+
     // Open sounds.dat
     sd_sound_file sf;
     sd_sounds_create(&sf);
@@ -89,61 +93,89 @@ int main(int argc, char *argv[]) {
         goto exit_1;
     }
     
-    int id = sid->ival[0]-1;
-    if(sid->count > 0 && sampleprint->count > 0) {
-        int scount = sampleprint->ival[0];
-        printf("Sample size = %d\n", sf.sounds[id]->len);
-        printf("Attempting to print #%d first samples.\n", scount);
-        for(int i = 0; i < scount; i++) {
-            unsigned int s = sf.sounds[id]->data[i] & 0xFF;
-            printf("%2x ", s);
-        }
-    } else if(sid->count > 0) {
-        printf("Attempting to play sample #%d.\n", id+1);
-    
-        // Make sure there is data at requested ID position
-        if(sf.sounds[id] == 0) {
-            printf("Sample does not contain data.\n");
+    if(sid->count > 0) {
+        // Sound ID to handle
+        int sound_id = sid->ival[0];
+        sd_sound *sound = sd_sounds_get(&sf, sound_id-1);
+        if(sound == NULL) {
+            printf("Invalid sound ID");
             goto exit_1;
         }
-    
-        // Streamer
-        streamer.size = sf.sounds[id]->len;
-        streamer.pos = 0;
-        streamer.data = sf.sounds[id]->data;
-    
-        // Initialize required audio
-        SDL_zero(want);
-        want.freq = 8000;
-        want.format = AUDIO_U8;
-        want.channels = 1;
-        want.samples = 4096;
-        want.callback = stream;
-        want.userdata = &streamer;
-            
-        // Open device, play file
-        dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0);
-        if(dev == 0) {
-            printf("Failed to open audio dev: %s\n", SDL_GetError());
-            goto exit_0;
-        } else {
-            if(have.format != want.format) {
-                printf("Could not get correct playback format.\n");
-            } else {
-                printf("Starting playback ...\n");
-                SDL_PauseAudioDevice(dev, 0);
-                while(streamer.pos < streamer.size) {
-                    SDL_Delay(100);
-                }
-                printf("All done.\n");
+
+        if(sampleprint->count > 0) {
+            int count = (sampleprint->ival[0] > sound->len) ? sound->len : sampleprint->ival[0];
+            printf("Sample size = %d\n", sound->len);
+            printf("Attempting to print %d first bytes.\n", count);
+            for(int i = 0; i < count; i++) {
+                unsigned int s = sound->data[i] & 0xFF;
+                printf("%2x ", s);
             }
-            SDL_CloseAudioDevice(dev);
+        } else if(play->count > 0) {
+            printf("Attempting to play sample #%d.\n", sound_id);
+        
+            // Make sure there is data at requested ID position
+            if(sound->len <= 2) {
+                printf("Sample does not contain data.\n");
+                goto exit_1;
+            }
+        
+            // Streamer
+            streamer.size = sound->len;
+            streamer.pos = 0;
+            streamer.data = sound->data;
+        
+            // Initialize required audio
+            SDL_zero(want);
+            want.freq = 8000;
+            want.format = AUDIO_U8;
+            want.channels = 1;
+            want.samples = 4096;
+            want.callback = stream;
+            want.userdata = &streamer;
+                
+            // Open device, play file
+            dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, 0);
+            if(dev == 0) {
+                printf("Failed to open audio dev: %s\n", SDL_GetError());
+                goto exit_0;
+            } else {
+                if(have.format != want.format) {
+                    printf("Could not get correct playback format.\n");
+                } else {
+                    printf("Starting playback ...\n");
+                    SDL_PauseAudioDevice(dev, 0);
+                    while(streamer.pos < streamer.size) {
+                        SDL_Delay(100);
+                    }
+                    printf("All done.\n");
+                }
+                SDL_CloseAudioDevice(dev);
+            }
+        } else if(import->count > 0) {
+            if(sound->data != NULL) {
+                free(sound->data);
+            }
+            if(sd_sound_from_au(sound, import->filename[0]) != SD_SUCCESS) {
+                printf("Importing sample %d from file %s failed.\n", sound_id, import->filename[0]);
+            } else {
+                printf("Importing sample %d from file %s succeeded.\n", sound_id, import->filename[0]);
+            }
+        } else if(export->count > 0) {
+            if(sd_sound_to_au(sound, export->filename[0]) != SD_SUCCESS) {
+                printf("Exporting sample %d to file %s failed.\n", sound_id, export->filename[0]);
+            } else {
+                printf("Exporting sample %d to file %s succeeded.\n", sound_id, export->filename[0]);
+            }
+        } else {
+            printf("Selected sample %d\n", sound_id);
+            printf("Length = %d\n", sound->len);
         }
     } else {
         printf("Valid sample ID's:\n");
         int k = 0;
-        for(int i = 0; i < sf.sound_count; i++) {
-            if(sf.sounds[i] != NULL) {
+        for(int i = 0; i < SD_SOUNDS_MAX; i++) {
+            sd_sound *sound = sd_sounds_get(&sf, i);
+            if(sound != NULL && sound->len > 2) {
                 printf("%d", i+1);
                 if((k+1)%6==0) {
                     printf("\n");
@@ -156,6 +188,11 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
     
+    if(output->count > 0) {
+        if(sd_sounds_save(&sf, output->filename[0]) != SD_SUCCESS) {
+            printf("Saving soundfile to %s failed.\n", output->filename[0]);
+        }
+    }
 
 exit_1:
     sd_sounds_free(&sf);
