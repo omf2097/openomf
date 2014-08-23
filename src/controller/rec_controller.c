00ec14b81d2dd554ec137ae62551fbe8a6bda5b1
Andrew Thompson <andrew@hijacked.us>
2014-08-23 01:22:14 -0400

    Sort of working REC player

--- src/controller/rec_controller.c
+++ src/controller/rec_controller.c
@@ -3,7 +3,7 @@
 
 typedef struct wtf_t {
     int id;
-    unsigned int ticks;
+    int last_tick;
     int last_action;
     hashmap tick_lookup;
 } wtf;
@@ -12,18 +12,49 @@ int rec_controller_tick(controller *ctrl, int ticks, ctrl_event **ev) {
     wtf *data = ctrl->data;
     sd_rec_move *move;
     unsigned int len;
-    if (hashmap_iget(&data->tick_lookup, data->ticks, (void**)(&move), &len) == 0) {
-        DEBUG("tick %d has action %s for player %d", data->ticks, move->raw_action, move->player_id);
+    if (data->last_tick != ticks) {
+        if (hashmap_iget(&data->tick_lookup, ticks, (void**)(&move), &len) == 0) {
+            DEBUG("tick %d has action %d for player %d", ticks, move->raw_action, move->player_id);
+            if (move->action == SD_REC_NONE) {
+                controller_cmd(ctrl, ACT_STOP, ev);
+            } else {
+                if (move->action & SD_REC_PUNCH) {
+                    controller_cmd(ctrl, ACT_PUNCH, ev);
+                } else if (move->action & SD_REC_KICK) {
+                    controller_cmd(ctrl, ACT_KICK, ev);
+                }
+
+                int action = 0;
+                if (move->action & SD_REC_UP) {
+                    action |= ACT_UP;
+                }
+
+                if (move->action & SD_REC_DOWN) {
+                    action |= ACT_DOWN;
+                }
+
+                if (move->action & SD_REC_LEFT) {
+                    action |= ACT_LEFT;
+                }
+
+                if (move->action & SD_REC_RIGHT) {
+                    action |= ACT_RIGHT;
+                }
+                if (action != 0) {
+                    controller_cmd(ctrl, action, ev);
+                }
+            }
+        }
     }
-    data->ticks++;
+    data->last_tick = ticks;
     return 0;
 }
 
 void rec_controller_create(controller *ctrl, int player, sd_rec_file *rec) {
     wtf *data = malloc(sizeof(wtf));
     data->last_action = ACT_STOP;
-    data->ticks = 0;
-    hashmap_create(&data->tick_lookup, rec->move_count);
+    data->last_tick = 0;
+    hashmap_create(&data->tick_lookup, 9);
     DEBUG("recording has %d moves", rec->move_count);
     for(unsigned int i = 0; i < rec->move_count; i++) {
         if (rec->moves[i].player_id == player && rec->moves[i].extra == 2) {
