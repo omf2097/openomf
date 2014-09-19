ef070c0e3105d0d585301738eef4992a3237c65a
Tuomas Virtanen <katajakasa@gmail.com>
2014-09-19 15:16:20 +0300

    Use new action enums for rec actions

--- src/rectool/main.c
+++ src/rectool/main.c
@@ -109,22 +109,22 @@ void rec_entry_set_key(sd_rec_file *rec, int entry_id, const char *key, const ch
             rec->moves[entry_id].player_id = atoi(value);
             break;
         case 3:
-            rec->moves[entry_id].action = SD_REC_NONE;
-            if(action & SD_REC_PUNCH) {
-                rec->moves[entry_id].action |= SD_REC_PUNCH;
+            rec->moves[entry_id].action = SD_ACT_NONE;
+            if(action & SD_ACT_PUNCH) {
+                rec->moves[entry_id].action |= SD_ACT_PUNCH;
             }
-            if(action & SD_REC_KICK) {
-                rec->moves[entry_id].action |= SD_REC_KICK;
+            if(action & SD_ACT_KICK) {
+                rec->moves[entry_id].action |= SD_ACT_KICK;
             }
             switch(action & 0xF0) {
-                case 16: rec->moves[entry_id].action |= SD_REC_UP; break;
-                case 32: rec->moves[entry_id].action |= (SD_REC_UP|SD_REC_RIGHT); break;
-                case 48: rec->moves[entry_id].action |= SD_REC_RIGHT; break;
-                case 64: rec->moves[entry_id].action |= (SD_REC_DOWN|SD_REC_RIGHT); break;
-                case 80: rec->moves[entry_id].action |= SD_REC_DOWN; break;
-                case 96: rec->moves[entry_id].action |= (SD_REC_DOWN|SD_REC_LEFT); break;
-                case 112: rec->moves[entry_id].action |= SD_REC_LEFT; break;
-                case 128: rec->moves[entry_id].action |= (SD_REC_UP|SD_REC_LEFT); break;
+                case 16: rec->moves[entry_id].action |= SD_ACT_UP; break;
+                case 32: rec->moves[entry_id].action |= (SD_ACT_UP|SD_ACT_RIGHT); break;
+                case 48: rec->moves[entry_id].action |= SD_ACT_RIGHT; break;
+                case 64: rec->moves[entry_id].action |= (SD_ACT_DOWN|SD_ACT_RIGHT); break;
+                case 80: rec->moves[entry_id].action |= SD_ACT_DOWN; break;
+                case 96: rec->moves[entry_id].action |= (SD_ACT_DOWN|SD_ACT_LEFT); break;
+                case 112: rec->moves[entry_id].action |= SD_ACT_LEFT; break;
+                case 128: rec->moves[entry_id].action |= (SD_ACT_UP|SD_ACT_LEFT); break;
             }
             break;
         default:
