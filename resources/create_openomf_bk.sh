4f977b6769bec8dc992746452abf122657c79ff9
Tuomas Virtanen <katajakasa@gmail.com>
2014-11-11 21:31:24 +0200

    Add new WIP splash

--- resources/create_openomf_bk.sh
+++ resources/create_openomf_bk.sh
@@ -3,6 +3,7 @@
 rm -f openomf.bk
 bktool -n -o openomf.bk
 bktool -f openomf.bk -o openomf.bk -k palette --push
-bktool -f openomf.bk -o openomf.bk -k palette -k 0 --import openomf.gpl
 bktool -f openomf.bk -o openomf.bk -k background --import openomf.png
+bktool -f openomf.bk -o openomf.bk -k palette -k 0 --import openomf.gpl
+
 
