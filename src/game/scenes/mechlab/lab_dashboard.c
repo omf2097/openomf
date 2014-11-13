f37c162edc9dff33296ce9409649a12319f5bb4c
Tuomas Virtanen <katajakasa@gmail.com>
2014-11-13 16:56:49 +0200

    Show the rest of the HAR stats in tournament dashboard.

--- src/game/scenes/mechlab/lab_dashboard.c
+++ src/game/scenes/mechlab/lab_dashboard.c
@@ -37,16 +37,38 @@ component* lab_dashboard_create(scene *s, dashboard_widgets *dw) {
     xysizer_attach(xy, dw->money, 12, 82, 200, 6);
     xysizer_attach(xy, dw->tournament, 12, 88, 200, 6);
 
-    // Bars and texts
-    xysizer_attach(xy, label_create(&tconf_dark, "Power"), 12, 95, -1, -1);
+    // Bars and texts (bottom left side)
+    xysizer_attach(xy, label_create(&tconf_dark, "POWER"), 12, 95, -1, -1);
     dw->power = gauge_create(GAUGE_SMALL, 25, 3);
     xysizer_attach(xy, dw->power, 12, 102, -1, -1);
-    xysizer_attach(xy, label_create(&tconf_dark, "Agility"), 12, 106, -1, -1);
+    xysizer_attach(xy, label_create(&tconf_dark, "AGILITY"), 12, 106, -1, -1);
     dw->agility = gauge_create(GAUGE_SMALL, 25, 3);
     xysizer_attach(xy, dw->agility, 12, 113, -1, -1);
-    xysizer_attach(xy, label_create(&tconf_dark, "Endurance"), 12, 117, -1, -1);
+    xysizer_attach(xy, label_create(&tconf_dark, "ENDURANCE"), 12, 117, -1, -1);
     dw->endurance = gauge_create(GAUGE_SMALL, 25, 3);
     xysizer_attach(xy, dw->endurance, 12, 124, -1, -1);
+
+    // Bars and texts (bottom middle)
+    xysizer_attach(xy, label_create(&tconf_dark, "ARM POWER"), 125, 95, 200, 6);
+    dw->arm_power = gauge_create(GAUGE_BIG, 8, 3);
+    xysizer_attach(xy, dw->arm_power, 125, 102, -1, -1);
+    xysizer_attach(xy, label_create(&tconf_dark, "LEG POWER"), 125, 106, 200, 6);
+    dw->arm_speed = gauge_create(GAUGE_BIG, 8, 3);
+    xysizer_attach(xy, dw->arm_speed, 125, 113, -1, -1);
+    xysizer_attach(xy, label_create(&tconf_dark, "ARMOR"), 125, 117, 200, 6);
+    dw->armor = gauge_create(GAUGE_BIG, 8, 3);
+    xysizer_attach(xy, dw->armor, 125, 124, -1, -1);
+
+    // Bars and texts (bottom right side)
+    xysizer_attach(xy, label_create(&tconf_dark, "ARM SPEED"), 228, 95, 200, 6);
+    dw->arm_power = gauge_create(GAUGE_BIG, 8, 3);
+    xysizer_attach(xy, dw->arm_power, 228, 102, -1, -1);
+    xysizer_attach(xy, label_create(&tconf_dark, "LEG SPEED"), 228, 106, 200, 6);
+    dw->arm_speed = gauge_create(GAUGE_BIG, 7, 3);
+    xysizer_attach(xy, dw->arm_speed, 228, 113, -1, -1);
+    xysizer_attach(xy, label_create(&tconf_dark, "STUN RES"), 228, 117, 200, 6);
+    dw->armor = gauge_create(GAUGE_BIG, 7, 3);
+    xysizer_attach(xy, dw->armor, 228, 124, -1, -1);
 
     return xy;
 }
