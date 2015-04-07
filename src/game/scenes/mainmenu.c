9283aab27003786aecf09cad09519feb74ecf278
Ing. Radomír Polách <polacrad@fit.cvut.cz>
2015-04-08 01:05:44 +0200

    fix: nasty multiple sound played bug

--- src/game/scenes/mainmenu.c
+++ src/game/scenes/mainmenu.c
@@ -12,7 +12,7 @@
 
 typedef struct mainmenu_local_t {
     guiframe *frame;
-    int prev_key;
+    int prev_key[2];
 } mainmenu_local;
 
 void mainmenu_free(scene *scene) {
@@ -27,33 +27,30 @@ void mainmenu_tick(scene *scene, int paused) {
     guiframe_tick(local->frame);
 }
 
-void mainmenu_process_controller_events(mainmenu_local *local, ctrl_event *p) {
-    if(p) {
-        do {
-            if(p->type == EVENT_TYPE_ACTION) {
-                // Skip repeated keys
-                if(local->prev_key == p->event_data.action) {
-                    continue;
-                }
-                local->prev_key = p->event_data.action;
-
-                // Pass on the event
-                guiframe_action(local->frame, p->event_data.action);
-            }
-        } while((p = p->next));
-    }
-}
-
 void mainmenu_input_tick(scene *scene) {
     mainmenu_local *local = scene_get_userdata(scene);
 
-    for(int i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++) {
         game_player *player = game_state_get_player(scene->gs, i);
         
         // Poll the controller
         ctrl_event *p = NULL;
         controller_poll(player->ctrl, &p);
-        mainmenu_process_controller_events(local, p);
+        if (p) {
+            do {
+                if (p->type == EVENT_TYPE_ACTION) {
+                    // Skip repeated keys
+                    if (local->prev_key[i] == p->event_data.action) {
+                        continue;
+                    }
+                    
+                    local->prev_key[i] = p->event_data.action;
+
+                    // Pass on the event
+                    guiframe_action(local->frame, p->event_data.action);
+                }
+            } while((p = p->next));
+        }
         controller_free_chain(p);
     }
 }
@@ -136,7 +133,7 @@ int mainmenu_create(scene *scene) {
     scene->gs->net_mode = NET_MODE_NONE;
 
     // prev_key is used to prevent multiple clicks while key is down
-    local->prev_key = ACT_PUNCH;
+    local->prev_key[0] = local->prev_key[1] = ACT_PUNCH;
 
     // Music and renderer
     music_play(PSM_MENU);
