2625fc4eb03ac24a13d6d897e71208991572f47c
Tuomas Virtanen <katajakasa@gmail.com>
2015-06-24 14:17:42 +0300

    Add selection handling to frequency and resampler selections

--- src/game/scenes/mainmenu/menu_audio.c
+++ src/game/scenes/mainmenu/menu_audio.c
@@ -30,12 +30,24 @@ void menu_audio_mono_toggle(component *c, void *userdata, int pos) {
     music_reload();
 }
 
-void menu_audio_done(component *c, void *u) {
+void menu_audio_done(component *c, void *userdata) {
+    audio_menu_data *local = userdata;
+
+    // Set menu as finished
     menu *m = sizer_get_obj(c->parent);
     m->finished = 1;
+
+    // Reload music if changes made
+    settings_sound *s = &settings_get()->sound;
+    if(s->music_library != local->old_audio_settings.music_library
+       || s->music_frequency != local->old_audio_settings.music_frequency
+       || s->music_resampler != local->old_audio_settings.music_resampler)
+    {
+        music_reload();
+    }
 }
 
-void menu_audio_reset_freqs(audio_menu_data *local) {
+void menu_audio_reset_freqs(audio_menu_data *local, int use_settings) {
     module_source *sources = music_get_module_sources();
     int id = sources[textselector_get_pos(local->lib_selector)].id;
     audio_source_freq *freqs = music_module_get_freqs(id);
@@ -43,13 +55,16 @@ void menu_audio_reset_freqs(audio_menu_data *local) {
     textselector_clear_options(local->freq_selector);
     for(int i = 0; freqs[i].name != 0; i++) {
         textselector_add_option(local->freq_selector, freqs[i].name);
-        if(freqs[i].is_default) {
+        int use = (use_settings)
+            ? (settings_get()->sound.music_frequency == freqs[i].freq)
+            : freqs[i].is_default;
+        if(use) {
             textselector_set_pos(local->freq_selector, i);
         }
     }
 }
 
-void menu_audio_reset_resamplers(audio_menu_data *local) {
+void menu_audio_reset_resamplers(audio_menu_data *local, int use_settings) {
     module_source *sources = music_get_module_sources();
     int id = sources[textselector_get_pos(local->lib_selector)].id;
     audio_source_resampler *resamplers = music_module_get_resamplers(id);
@@ -57,7 +72,10 @@ void menu_audio_reset_resamplers(audio_menu_data *local) {
     textselector_clear_options(local->resampler_selector);
     for(int i = 0; resamplers[i].name != 0; i++) {
         textselector_add_option(local->resampler_selector, resamplers[i].name);
-        if(resamplers[i].is_default) {
+        int use = (use_settings)
+            ? (settings_get()->sound.music_resampler == resamplers[i].internal_id)
+            : resamplers[i].is_default;
+        if(use) {
             textselector_set_pos(local->resampler_selector, i);
         }
     }
@@ -67,19 +85,24 @@ void menu_audio_library_toggled(component *c, void *userdata, int pos) {
     audio_menu_data *local = userdata;
     module_source *sources = music_get_module_sources();
     settings_get()->sound.music_library = sources[pos].id;
-    menu_audio_reset_freqs(local);
-    menu_audio_reset_resamplers(local);
-    music_reload();
+    menu_audio_reset_freqs(local, 0);
+    menu_audio_reset_resamplers(local, 0);
 }
 
 void menu_audio_freq_toggled(component *c, void *userdata, int pos) {
     audio_menu_data *local = userdata;
-    (void)(local);
+    module_source *sources = music_get_module_sources();
+    int id = sources[textselector_get_pos(local->lib_selector)].id;
+    audio_source_freq *freqs = music_module_get_freqs(id);
+    settings_get()->sound.music_frequency = freqs[pos].freq;
 }
 
 void menu_audio_resampler_toggled(component *c, void *userdata, int pos) {
     audio_menu_data *local = userdata;
-    (void)(local);
+    module_source *sources = music_get_module_sources();
+    int id = sources[textselector_get_pos(local->lib_selector)].id;
+    audio_source_resampler *resamplers = music_module_get_resamplers(id);
+    settings_get()->sound.music_resampler = resamplers[pos].internal_id;
 }
 
 void menu_audio_free(component *c) {
@@ -119,14 +142,14 @@ component* menu_audio_create(scene *s) {
     menu_attach(menu, local->lib_selector);
 
     local->freq_selector = textselector_create(&tconf, "FREQUENCY:", menu_audio_freq_toggled, local);
-    menu_audio_reset_freqs(local);
+    menu_audio_reset_freqs(local, 1);
     menu_attach(menu, local->freq_selector);
 
     local->resampler_selector = textselector_create(&tconf, "RESAMPLE:", menu_audio_resampler_toggled, local);
-    menu_audio_reset_resamplers(local);
+    menu_audio_reset_resamplers(local, 1);
     menu_attach(menu, local->resampler_selector);
 
-    menu_attach(menu, textbutton_create(&tconf, "DONE", COM_ENABLED, menu_audio_done, s));
+    menu_attach(menu, textbutton_create(&tconf, "DONE", COM_ENABLED, menu_audio_done, local));
 
     // Userdata & free function for it
     menu_set_userdata(menu, local);
