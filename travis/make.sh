0e4e5c4840252516396360ec508eef0d6ed88904
Tuomas Virtanen <katajakasa@gmail.com>
2017-10-28 19:19:27 +0300

    Add Sonarcloud files

--- travis/make.sh
+++ travis/make.sh
@@ -1,6 +1,7 @@
-if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then 
-    cd $TRAVIS_BUILD_DIR
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/gcc-7 -DUSE_TESTS=On . 
-    make
-    make test
-fi
+cd $TRAVIS_BUILD_DIR
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/bin/gcc-7 -DUSE_TESTS=On . 
+make
+make test
+make clean
+build-wrapper-linux-x86-64 --out-dir bw-output make
+sonar-scanner
