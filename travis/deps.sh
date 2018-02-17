296cd0ff07851f4db78dd44b65e7fe958eca2414
Tuomas Virtanen <katajakasa@gmail.com>
2018-02-18 00:52:26 +0200

    Update travis dep packages

--- travis/deps.sh
+++ travis/deps.sh
@@ -13,13 +13,13 @@ sudo apt-get -y install \
     libsdl2-dev
 
 sudo apt-get remove libpng12-dev libz-dev -y
-wget "http://mirrors.kernel.org/ubuntu/pool/main/z/zlib/zlib1g-dev_1.2.11.dfsg-0ubuntu1_amd64.deb"
-wget "http://mirrors.kernel.org/ubuntu/pool/main/z/zlib/zlib1g_1.2.11.dfsg-0ubuntu1_amd64.deb"
+wget "http://mirrors.kernel.org/ubuntu/pool/main/z/zlib/zlib1g-dev_1.2.11.dfsg-0ubuntu2_amd64.deb"
+wget "http://mirrors.kernel.org/ubuntu/pool/main/z/zlib/zlib1g_1.2.11.dfsg-0ubuntu2_amd64.deb"
 wget "http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng1.6/libpng16-16_1.6.34-1_amd64.deb"
 wget "http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng1.6/libpng-tools_1.6.34-1_amd64.deb"
 wget "http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng1.6/libpng-dev_1.6.34-1_amd64.deb"
-sudo dpkg -i zlib1g_1.2.11.dfsg-0ubuntu1_amd64.deb
-sudo dpkg -i zlib1g-dev_1.2.11.dfsg-0ubuntu1_amd64.deb
+sudo dpkg -i zlib1g_1.2.11.dfsg-0ubuntu2_amd64.deb
+sudo dpkg -i zlib1g-dev_1.2.11.dfsg-0ubuntu2_amd64.deb
 sudo dpkg -i libpng16-16_1.6.34-1_amd64.deb
 sudo dpkg -i libpng-tools_1.6.34-1_amd64.deb
 sudo dpkg -i libpng-dev_1.6.34-1_amd64.deb
