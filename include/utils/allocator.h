020c4485245a742ba0e5c5245036d84a496d3833
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2021-10-21 20:26:24 +0300

    utils: Add omf_realloc() to move realloc() error handling to one place

--- include/utils/allocator.h
+++ include/utils/allocator.h
@@ -12,4 +12,8 @@ typedef struct allocator_t {
     omf_calloc_real((nmemb), (size), __FILE__, __LINE__)
 void *omf_calloc_real(size_t nmemb, size_t size, const char *file, int line);
 
+#define omf_realloc(ptr, size) \
+    omf_realloc_real((ptr), (size), __FILE__, __LINE__)
+void *omf_realloc_real(void *ptr, size_t size, const char *file, int line);
+
 #endif // _ALLOCATOR_H
--- src/utils/allocator.c
+++ src/utils/allocator.c
@@ -10,3 +10,12 @@ void *omf_calloc_real(size_t nmemb, size_t size, const char *file, int line) {
             nmemb, size, file, line);
     abort();
 }
+
+void *omf_realloc_real(void *ptr, size_t size, const char *file, int line) {
+    void *ret = realloc(ptr, size);
+    if (ret != NULL)
+        return ret;
+    fprintf(stderr, "realloc(%p, %zu) failed on %s:%d\n",
+            ptr, size, file, line);
+    abort();
+}
