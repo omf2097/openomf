84b094a192820793a86cba5420c436daecd52c9e
Tuomas Virtanen <katajakasa@gmail.com>
2023-04-25 22:25:36 +0300

    Improve game-controller loader logging

--- src/controller/game_controller_db.c
+++ src/controller/game_controller_db.c
@@ -6,11 +6,12 @@
 #include "utils/str.h"
 
 void joystick_load_external_mappings(void) {
+    int loaded;
     str controller_db_path;
     const char *resource_path = pm_get_local_path(RESOURCE_PATH);
     str_from_format(&controller_db_path, "%sgamecontrollerdb.txt", resource_path);
-    if(SDL_GameControllerAddMappingsFromFile(str_c(&controller_db_path)) > 0) {
-        INFO("Loaded external game controller mappings from %s", str_c(&controller_db_path));
+    if((loaded = SDL_GameControllerAddMappingsFromFile(str_c(&controller_db_path))) > 0) {
+        INFO("Loaded %d external game controller mappings from %s", loaded, str_c(&controller_db_path));
         INFO("We have %d known game controller mappings", SDL_GameControllerNumMappings());
     } else {
         INFO("No external game controller mappings file found from %s; skipping ...", str_c(&controller_db_path));
