43ee91756c03bee8360dce897c3bce2b183696d7
Tuomas Virtanen <katajakasa@gmail.com>
2023-05-01 02:14:45 +0300

    Move opengl buffer state management to the application.

--- src/video/opengl/buffers.h
+++ src/video/opengl/buffers.h
@@ -4,16 +4,19 @@
 #include <epoxy/gl.h>
 
 GLuint ubo_create(GLsizeiptr size);
+void ubo_update(GLuint id, GLsizeiptr size, const void *data);
 void ubo_free(GLuint id);
 
 GLuint vbo_create(GLsizeiptr size);
+void *vbo_map(GLuint id, GLsizei size);
+void vbo_unmap(GLuint id);
 void vbo_free(GLuint id);
 
 GLuint vao_create();
 void vao_free(GLuint id);
 
 GLuint texture_create(GLsizei w, GLsizei h, GLint internal_format, GLenum format);
-void texture_update(int x, int y, int w, int h, const char *bytes);
+void texture_update(GLuint id, int x, int y, int w, int h, const char *bytes);
 void texture_free(GLuint id);
 
 #endif // BUFFERS_H
