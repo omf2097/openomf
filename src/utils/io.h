62686e3c6b9f004025f9d2484104288e221c50da
Tuomas Virtanen <katajakasa@gmail.com>
2023-05-03 17:22:54 +0300

    Split file handling to a separate library with some added safety checks

---
+++ src/utils/io.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_IO_H
+#define UTILS_IO_H
+
+#include <stdio.h>
+
+FILE *file_open(const char *file_name, const char *mode);
+void file_seek(FILE *handle, long offset, int origin);
+long file_size(FILE *handle);
+void file_read(FILE *handle, char *buffer, long size);
+void file_close(FILE *handle);
+
+#endif // UTILS_IO_H
