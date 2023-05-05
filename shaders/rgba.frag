c259c3d0e64ecd95799c3d3afc20ab866916af7b
Tuomas Virtanen <katajakasa@gmail.com>
2023-05-05 23:25:33 +0300

    Initial implementation of multiple rendering passes for palette remapping

--- shaders/rgba.frag
+++ shaders/rgba.frag
@@ -5,13 +5,13 @@ in vec2 tex_coord;
 layout (std140) uniform palette {
     vec4 colors[256];
 };
-uniform sampler2D image;
+uniform sampler2D framebuffer;
 
 // Out
 layout (location = 0) out vec4 color;
 
 void main() {
-    vec4 texel = texture(image, tex_coord);
+    vec4 texel = texture(framebuffer, tex_coord);
     int index = int(texel.r * 255);
     color = colors[index].rgba;
 }
--- src/video/opengl/shaders.h
+++ src/video/opengl/shaders.h
@@ -7,7 +7,7 @@ bool create_program(GLuint *program_id, const char *const vertex_shader, const c
 void delete_program(GLuint program_id);
 void activate_program(GLuint program_id);
 void bind_uniform_4fv(GLuint program_id, const char *name, GLfloat *data);
-void bind_uniform_li(GLuint program_id, const char *name, GLuint value);
+void bind_uniform_1i(GLuint program_id, const char *name, GLuint value);
 void bind_uniform_block(GLuint program_id, const char *name, GLuint binding_id, GLuint buffer);
 
 #endif // SHADERS_H
