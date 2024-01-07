8894e944c82575e0c6d0e3b860c3c361a1982b77
Tuomas Virtanen <katajakasa@gmail.com>
2024-01-07 02:38:37 +0200

    Drop unnecessary audio code

--- src/audio/sound.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOUND_H
-#define SOUND_H
-
-void sound_play(int id, float volume, float panning, float pitch);
-int sound_playing(unsigned int sound_id);
-void sound_set_volume(float volume);
-
-#endif // SOUND_H
--- src/audio/sources/raw_source.h
+++
@@ -1,8 +0,0 @@
-#ifndef RAW_SOURCE_H
-#define RAW_SOURCE_H
-
-#include "audio/source.h"
-
-int raw_source_init(audio_source *src, char *buffer, int len);
-
-#endif // RAW_SOURCE_H
