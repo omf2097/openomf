b1a2da7a4e3a175ed427088353a84b2e41d0fec6
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2024-07-26 15:47:05 +0300

    game: Support signed score in score_format()

--- src/game/utils/formatting.h
+++ src/game/utils/formatting.h
@@ -1,6 +1,6 @@
 #ifndef FORMATTING_H
 #define FORMATTING_H
 
-void score_format(unsigned int score, char *buf, int maxlen);
+void score_format(int signed_score, char *buf, int maxlen);
 
 #endif // FORMATTING_H
