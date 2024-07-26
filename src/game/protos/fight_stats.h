efbe315cc560c98349a120c155040f537741e946
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2024-07-26 05:44:51 +0300

    game: Expand the tournament financial report

---
+++ src/game/protos/fight_stats.h
@@ -0,0 +1,11 @@
+#ifndef FIGHT_STATS_H
+#define FIGHT_STATS_H
+
+typedef struct fight_stats_t {
+    int winnings;
+    int bonuses;
+    int repair_cost;
+    int profit;
+} fight_stats;
+
+#endif // FIGHT_STATS_H
