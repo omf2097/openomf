b8b6f1936f7441644b1e7ed70e4d3446802af773
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2024-07-26 15:47:05 +0300

    game: Support signed score in score_format()

--- src/game/utils/formatting.c
+++ src/game/utils/formatting.c
@@ -1,15 +1,21 @@
 #include "game/utils/formatting.h"
 #include <stdio.h>
 
-void score_format(unsigned int score, char *buf, int maxlen) {
+void score_format(int signed_score, char *buf, int maxlen) {
     unsigned int n = 0;
     unsigned int scale = 1;
+    unsigned int score = (unsigned int)(signed_score);
+    int len = 0;
+    if(signed_score < 0) {
+        len = snprintf(buf, maxlen, "-");
+        score = (unsigned int)(-1 * signed_score);
+    }
     while(score >= 1000) {
         n = n + scale * (score % 1000);
         score /= 1000;
         scale *= 1000;
     }
-    int len = snprintf(buf, maxlen, "%u", score);
+    len += snprintf(buf + len, maxlen, "%u", score);
     while(scale != 1 && len < maxlen) {
         scale /= 1000;
         score = n / scale;
