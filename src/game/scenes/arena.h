9e5a978adbceeb7e45ff5274aac9867ea217c5e2
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2024-09-16 23:28:09 +0300

    game: Add arena_state_hash()

--- src/game/scenes/arena.h
+++ src/game/scenes/arena.h
@@ -16,5 +16,6 @@ void arena_set_state(scene *scene, int state);
 palette *arena_get_player_palette(scene *scene, int player);
 void arena_toggle_rein(scene *scene);
 void maybe_install_har_hooks(scene *scene);
+uint32_t arena_state_hash(game_state *gs);
 
 #endif // ARENA_H
