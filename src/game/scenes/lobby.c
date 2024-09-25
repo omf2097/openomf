6acd285251e8dbdf5686cc00688f7a12142f0f65
Andrew Thompson <andrew@hijacked.us>
2024-09-25 13:00:03 -0700

    More UI

--- src/game/scenes/lobby.c
+++ src/game/scenes/lobby.c
@@ -2,14 +2,28 @@
 #include "utils/allocator.h"
 #include "video/video.h"
 
+#define TEXT_BRIGHT color_create(240, 250, 250, 255)
 #define TEXT_COLOR color_create(186, 250, 250, 255)
 #define BLUE_TEXT_COLOR color_create(60, 182, 255, 255)
 
 #define TEXT_BLACK color_create(0, 0, 0, 255)
 
+enum
+{
+    LOBBY_CHALLENGE,
+    LOBBY_WHISPER,
+    LOBBY_YELL,
+    LOBBY_REFRESH,
+    LOBBY_EXIT,
+    LOBBY_ACTION_COUNT
+};
+
 typedef struct lobby_local_t {
     char name[40];
+    char msg[128];
+    list log;
     bool named;
+    uint8_t mode;
 } lobby_local;
 
 static int lobby_event(scene *scene, SDL_Event *e) {
@@ -34,14 +48,39 @@ static int lobby_event(scene *scene, SDL_Event *e) {
                 if(SDL_HasClipboardText()) {
                     strncat(local->name, SDL_GetClipboardText(), sizeof(local->name) - strlen(local->name));
                 }
-            } else if(state[SDL_SCANCODE_RETURN]) {
+            } else if(state[SDL_SCANCODE_RETURN] && strlen(local->name) > 0) {
                 local->named = true;
             } else if(state[SDL_SCANCODE_ESCAPE]) {
                 game_state_set_next(scene->gs, SCENE_MENU);
             }
             return 0;
         }
+    } else {
+        if(e->type == SDL_TEXTINPUT && (local->mode == LOBBY_WHISPER || local->mode == LOBBY_YELL)) {
+            strncat(local->msg, e->text.text, sizeof(local->msg) - strlen(local->msg));
+            return 0;
+        } else if(e->type == SDL_KEYDOWN) {
+            const unsigned char *state = SDL_GetKeyboardState(NULL);
+            if((state[SDL_SCANCODE_BACKSPACE] || state[SDL_SCANCODE_DELETE]) &&
+               (local->mode == LOBBY_WHISPER || local->mode == LOBBY_YELL)) {
+                size_t len = strlen(local->msg);
+                if(len > 0) {
+                    local->msg[len - 1] = '\0';
+                }
+            } else if(state[SDL_SCANCODE_RIGHT]) {
+                local->mode++;
+                if(local->mode >= LOBBY_ACTION_COUNT) {
+                    local->mode = LOBBY_CHALLENGE;
+                }
+            } else if(state[SDL_SCANCODE_LEFT]) {
+                local->mode--;
+                if(local->mode >= LOBBY_ACTION_COUNT) {
+                    local->mode = LOBBY_EXIT;
+                }
+            }
+        }
     }
+
     return 1;
 }
 
@@ -89,6 +128,23 @@ void lobby_render_overlay(scene *scene) {
                                      TEXT_BLACK);
         snprintf(buf, sizeof(buf), "%s%c", local->name, 127);
         font_render_shadowed(&font_net1, buf, 130, 140, TEXT_COLOR, TEXT_SHADOW_RIGHT | TEXT_SHADOW_BOTTOM);
+    } else {
+        color c = (local->mode == LOBBY_CHALLENGE && scene->gs->int_tick % 2 == 0) ? TEXT_BRIGHT : BLUE_TEXT_COLOR;
+        snprintf(buf, sizeof(buf), "Challenge");
+        font_render_shadowed_colored(&font_net1, buf, 9, 140, c, TEXT_SHADOW_RIGHT | TEXT_SHADOW_BOTTOM, TEXT_BLACK);
+        c = (local->mode == LOBBY_WHISPER && scene->gs->int_tick % 2 == 0) ? TEXT_BRIGHT : BLUE_TEXT_COLOR;
+        snprintf(buf, sizeof(buf), "Whisper");
+        font_render_shadowed_colored(&font_net1, buf, 85, 140, c, TEXT_SHADOW_RIGHT | TEXT_SHADOW_BOTTOM, TEXT_BLACK);
+        c = (local->mode == LOBBY_YELL && scene->gs->int_tick % 2 == 0) ? TEXT_BRIGHT : BLUE_TEXT_COLOR;
+        snprintf(buf, sizeof(buf), "Yell");
+        font_render_shadowed_colored(&font_net1, buf, 143, 140, c, TEXT_SHADOW_RIGHT | TEXT_SHADOW_BOTTOM, TEXT_BLACK);
+        c = (local->mode == LOBBY_REFRESH && scene->gs->int_tick % 2 == 0) ? TEXT_BRIGHT : BLUE_TEXT_COLOR;
+        snprintf(buf, sizeof(buf), "Refresh");
+        font_render_shadowed_colored(&font_net1, buf, 178, 140, c, TEXT_SHADOW_RIGHT | TEXT_SHADOW_BOTTOM, TEXT_BLACK);
+
+        c = (local->mode == LOBBY_EXIT && scene->gs->int_tick % 2 == 0) ? TEXT_BRIGHT : BLUE_TEXT_COLOR;
+        snprintf(buf, sizeof(buf), "Exit");
+        font_render_shadowed_colored(&font_net1, buf, 240, 140, c, TEXT_SHADOW_RIGHT | TEXT_SHADOW_BOTTOM, TEXT_BLACK);
     }
 }
 
@@ -107,7 +163,9 @@ int lobby_create(scene *scene) {
     scene_set_userdata(scene, local);
 
     local->name[0] = 0;
+    local->msg[0] = 0;
     local->named = false;
+    local->mode = LOBBY_CHALLENGE;
 
     // scene_set_input_poll_cb(scene, lobby_input_tick);
 
