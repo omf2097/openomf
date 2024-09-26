1f00f2134e24c95fe5bf31aee8b7535fa9e1c604
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2024-09-26 09:12:43 +0300

    game: Remove commented out code from mechlab_input_tick()

--- src/game/scenes/mechlab.c
+++ src/game/scenes/mechlab.c
@@ -413,7 +413,6 @@ void mechlab_input_tick(scene *scene) {
                         mechlab_select_dashboard(scene, DASHBOARD_STATS);
                     } else if(i->event_data.action == ACT_KICK || i->event_data.action == ACT_PUNCH) {
                         strncpy(player1->pilot->name, textinput_value(local->nw.input), 17);
-                        // mechlab_select_dashboard(scene, DASHBOARD_SELECT_NEW_PIC);
                         trnmenu_finish(
                             guiframe_get_root(local->frame)); // This will trigger exception case in mechlab_tick
                     } else {
@@ -435,13 +434,6 @@ void mechlab_input_tick(scene *scene) {
                     mechlab_select_dashboard(scene, DASHBOARD_STATS);
                     guiframe_set_root(local->frame, lab_menu_main_create(scene, found));
                     guiframe_layout(local->frame);
-
-                    // } else if (local->dashtype == DASHBOARD_SELECT_NEW_PIC && (i->event_data.action == ACT_KICK ||
-                    // i->event_data.action == ACT_PUNCH)) {
-                    //         mechlab_select_dashboard(scene, DASHBOARD_SELECT_DIFFICULTY);
-                    //         trnmenu_finish(
-                    //             guiframe_get_root(local->frame)); // This will trigger exception case in mechlab_tick
-                    // If view is any other, just pass input to the bottom menu
                 } else {
                     guiframe_action(local->frame, i->event_data.action);
                 }
