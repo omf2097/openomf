42dd73d3fd7f359fe5c691ce17b0f00745daf41c
Tuomas Virtanen <katajakasa@gmail.com>
2024-09-28 20:59:33 +0300

    hashmap naming and documentation cleanups

--- src/utils/hashmap.h
+++ src/utils/hashmap.h
@@ -9,8 +9,10 @@ typedef struct hashmap_t hashmap;
 typedef void (*hashmap_free_cb)(void *);
 
 struct hashmap_pair_t {
-    unsigned int keylen, vallen;
-    void *key, *val;
+    unsigned int key_len;
+    unsigned int value_len;
+    void *key;
+    void *value;
 };
 
 struct hashmap_node_t {
@@ -30,13 +32,13 @@ void hashmap_create_cb(hashmap *hm, hashmap_free_cb free_cb);
 void hashmap_free(hashmap *hashmap);
 unsigned int hashmap_size(const hashmap *hashmap);
 unsigned int hashmap_reserved(const hashmap *hashmap);
-void *hashmap_put(hashmap *hm, const void *key, unsigned int keylen, const void *val, unsigned int vallen);
+void *hashmap_put(hashmap *hm, const void *key, unsigned int key_len, const void *val, unsigned int value_len);
 void hashmap_sput(hashmap *hashmap, const char *key, void *value, unsigned int value_len);
 void hashmap_iput(hashmap *hashmap, unsigned int key, void *value, unsigned int value_len);
-int hashmap_get(hashmap *hm, const void *key, unsigned int keylen, void **val, unsigned int *vallen);
+int hashmap_get(hashmap *hm, const void *key, unsigned int key_len, void **value, unsigned int *value_len);
 int hashmap_sget(hashmap *hashmap, const char *key, void **value, unsigned int *value_len);
 int hashmap_iget(hashmap *hashmap, unsigned int key, void **value, unsigned int *value_len);
-int hashmap_del(hashmap *hm, const void *key, unsigned int keylen);
+int hashmap_del(hashmap *hm, const void *key, unsigned int key_len);
 void hashmap_sdel(hashmap *hashmap, const char *key);
 void hashmap_idel(hashmap *hashmap, unsigned int key);
 void hashmap_iter_begin(const hashmap *hashmap, iterator *iter);
