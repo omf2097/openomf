6447003b44b08228353584304a7f295158aea255
Tuomas Virtanen <katajakasa@gmail.com>
2024-09-30 00:37:12 +0300

    fix: Game controller database improvements

--- src/controller/game_controller_db.c
+++ src/controller/game_controller_db.c
@@ -1,19 +1,20 @@
 #include <SDL_gamecontroller.h>
-#include <string.h>
 
 #include "resources/pathmanager.h"
 #include "utils/log.h"
 #include "utils/str.h"
 
 void joystick_load_external_mappings(void) {
+    int loaded;
     str controller_db_path;
     const char *resource_path = pm_get_local_path(RESOURCE_PATH);
     str_from_format(&controller_db_path, "%sgamecontrollerdb.txt", resource_path);
-    if(SDL_GameControllerAddMappingsFromFile(str_c(&controller_db_path)) > 0) {
-        INFO("Loaded external game controller mappings from %s", str_c(&controller_db_path));
+    if((loaded = SDL_GameControllerAddMappingsFromFile(str_c(&controller_db_path))) > 0) {
+        INFO("Loaded %d external game controller mappings from %s", loaded, str_c(&controller_db_path));
         INFO("We have %d known game controller mappings", SDL_GameControllerNumMappings());
     } else {
-        INFO("No external game controller mappings file found from %s; skipping ...", str_c(&controller_db_path));
+        INFO("No external game controller mappings file found from '%s' (%s); skipping ...", str_c(&controller_db_path),
+             SDL_GetError());
     }
     str_free(&controller_db_path);
 }
