68c03534a074aa55a042e2898bc23f0851063fa7
Andrew Thompson <andrew@hijacked.us>
2024-10-02 11:34:27 -0700

    More lobby UI

--- src/game/gui/textinput.h
+++ src/game/gui/textinput.h
@@ -8,6 +8,7 @@ typedef void (*textinput_done_cb)(component *c, void *userdata);
 
 component *textinput_create(const text_settings *tconf, const char *text, const char *help, const char *initialoption);
 char *textinput_value(const component *c);
+void textinput_clear(component *c);
 void textinput_enable_background(component *c, int enabled);
 void textinput_set_max_chars(component *c, int max_chars);
 void textinput_set_done_cb(component *c, textinput_done_cb done_cb, void *userdata);
