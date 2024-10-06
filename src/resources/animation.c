64e8277ff9cdb2b8e4ce6a5fee210dc708072306
Andrew Thompson <andrew@hijacked.us>
2024-10-06 12:18:05 -0700

    Better sprite deduping

--- src/resources/animation.c
+++ src/resources/animation.c
@@ -3,6 +3,11 @@
 #include "utils/allocator.h"
 #include <stdlib.h>
 
+typedef struct sprite_reference_t {
+    sprite *sprite;
+    bool owned;
+} sprite_reference;
+
 void animation_create(animation *ani, array *sprites, void *src, int id) {
     sd_animation *sdani = (sd_animation *)src;
 
@@ -32,17 +37,29 @@ void animation_create(animation *ani, array *sprites, void *src, int id) {
     }
 
     // Handle sprites
-    vector_create_with_size(&ani->sprites, sizeof(sprite *), sdani->sprite_count);
+    vector_create_with_size(&ani->sprites, sizeof(sprite_reference), sdani->sprite_count);
     sprite *tmp_sprite;
     for(int i = 0; i < sdani->sprite_count; i++) {
         if(sdani->sprites[i]->missing) {
+            // read the right index from the sprite table
             tmp_sprite = array_get(sprites, sdani->sprites[i]->index);
-            vector_append(&ani->sprites, &tmp_sprite);
+            sprite_reference spr;
+            spr.sprite = tmp_sprite;
+            spr.owned = false;
+            vector_append(&ani->sprites, &spr);
         } else {
             tmp_sprite = omf_calloc(1, sizeof(sprite));
             sprite_create(tmp_sprite, (void *)sdani->sprites[i], i);
-            array_set(sprites, sdani->sprites[i]->index, tmp_sprite);
-            vector_append(&ani->sprites, &tmp_sprite);
+            sprite_reference spr;
+            spr.sprite = tmp_sprite;
+            spr.owned = false;
+            if(sdani->sprites[i]->index) {
+                // insert into the global sprite table
+                array_set(sprites, sdani->sprites[i]->index, tmp_sprite);
+            } else {
+                spr.owned = true;
+            }
+            vector_append(&ani->sprites, &spr);
         }
     }
 }
@@ -54,8 +71,11 @@ animation *create_animation_from_single(sprite *sp, vec2i pos) {
     str_from_c(&a->animation_string, "A9999999999");
     vector_create_with_size(&a->collision_coords, sizeof(collision_coord), 0);
     vector_create_with_size(&a->extra_strings, sizeof(str), 0);
-    vector_create_with_size(&a->sprites, sizeof(sprite *), 1);
-    vector_append(&a->sprites, sp);
+    vector_create_with_size(&a->sprites, sizeof(sprite_reference), 1);
+    sprite_reference spr;
+    spr.sprite = sp;
+    spr.owned = false;
+    vector_append(&a->sprites, &spr);
     return a;
 }
 
@@ -78,22 +98,22 @@ int animation_clone(animation *src, animation *dst) {
         str_from(&new_str, tmp_str);
         vector_append(&dst->extra_strings, &new_str);
     }
-    vector_create_with_size(&dst->sprites, sizeof(sprite), vector_size(&src->sprites));
+    vector_create_with_size(&dst->sprites, sizeof(sprite_reference), vector_size(&src->sprites));
     vector_iter_begin(&src->sprites, &it);
-    sprite **tmp_sprite = NULL;
-    while((tmp_sprite = iter_next(&it)) != NULL) {
-        vector_append(&dst->sprites, *tmp_sprite);
+    sprite_reference *spr = NULL;
+    while((spr = iter_next(&it)) != NULL) {
+        vector_append(&dst->sprites, spr);
     }
 
     return 0;
 }
 
 sprite *animation_get_sprite(animation *ani, int sprite_id) {
-    sprite **s = (sprite **)vector_get(&ani->sprites, sprite_id);
+    sprite_reference *s = (sprite_reference *)vector_get(&ani->sprites, sprite_id);
     if(s == NULL) {
         return NULL;
     }
-    return *s;
+    return s->sprite;
 }
 
 int animation_get_sprite_count(animation *ani) {
@@ -116,6 +136,15 @@ void animation_free(animation *ani) {
         str_free(tmp_str);
     }
     vector_free(&ani->extra_strings);
+
+    vector_iter_begin(&ani->sprites, &it);
+    sprite_reference *spr;
+    while((spr = iter_next(&it)) != NULL) {
+        if(spr->owned) {
+            sprite_free(spr->sprite);
+            omf_free(spr->sprite);
+        }
+    }
 
     vector_free(&ani->sprites);
 }
