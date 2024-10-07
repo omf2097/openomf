524d2a783ee463002cc061dd1e36ea88266df5d9
Tuomas Virtanen <katajakasa@gmail.com>
2024-10-07 19:32:18 +0300

    Add omf_malloc() variant

--- src/utils/allocator.c
+++ src/utils/allocator.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void *omf_malloc_real(size_t size, const char *file, int line) {
+    void *ret = malloc(size);
+    if(ret != NULL)
+        return ret;
+    fprintf(stderr, "malloc(%zu) failed on %s:%d\n", size, file, line);
+    abort();
+}
+
 void *omf_calloc_real(size_t nmemb, size_t size, const char *file, int line) {
     void *ret = calloc(nmemb, size);
     if(ret != NULL)
