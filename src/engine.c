ca54405c4e4d8c0ed7989f857dcbe2b206a59946
Tuomas Virtanen <katajakasa@gmail.com>
2024-10-08 21:05:40 +0300

    More warp!

--- src/engine.c
+++ src/engine.c
@@ -237,20 +237,24 @@ void engine_run(engine_init_flags *init_flags) {
             debugger_proceed = 0;
         }
 
-        // Tick static features. This is a fixed with-rate tick, and is meant for running things
-        // that are not dependent on game speed (such as menus).
-        if(static_wait > STATIC_TICKS) {
-            game_state_static_tick(gs, false);
-            console_tick();
-            static_wait -= STATIC_TICKS;
-        }
+        // In warp mode, allow more ticks to happen per vsync period.
+        int tick_limit = gs->warp_speed ? 10 : 1;
+        while(tick_limit--) {
+            // Tick static features. This is a fixed with-rate tick, and is meant for running things
+            // that are not dependent on game speed (such as menus).
+            if(static_wait > STATIC_TICKS) {
+                game_state_static_tick(gs, false);
+                console_tick();
+                static_wait -= STATIC_TICKS;
+            }
 
-        // Tick dynamic features. This is a dynamically changing tick, and it depends on things such as
-        // hit-pause, hit slowdown and game-speed slider. It is meant for ticking everything that has to do
-        // with the actual gameplay stuff.
-        if(dynamic_wait > game_state_ms_per_dyntick(gs)) {
-            game_state_dynamic_tick(gs, false);
-            dynamic_wait -= game_state_ms_per_dyntick(gs);
+            // Tick dynamic features. This is a dynamically changing tick, and it depends on things such as
+            // hit-pause, hit slowdown and game-speed slider. It is meant for ticking everything that has to do
+            // with the actual gameplay stuff.
+            if(dynamic_wait > game_state_ms_per_dyntick(gs)) {
+                game_state_dynamic_tick(gs, false);
+                dynamic_wait -= game_state_ms_per_dyntick(gs);
+            }
         }
 
         // Do the actual video rendering jobs
