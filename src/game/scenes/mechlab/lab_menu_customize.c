9218d0bf4552148f1a793aaf1b7e33c993918fb0
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2024-10-20 15:54:12 +0300

    game: Rename mechlab customize labels

--- src/game/scenes/mechlab/lab_menu_customize.c
+++ src/game/scenes/mechlab/lab_menu_customize.c
@@ -21,9 +21,8 @@ static uint8_t max_leg_speed[11] = {8, 9, 5, 6, 8, 8, 7, 6, 7, 5, 6};
 static uint8_t max_arm_power[11] = {5, 5, 9, 8, 4, 6, 6, 5, 5, 6, 7};
 static uint8_t max_leg_power[11] = {6, 6, 8, 4, 5, 7, 5, 7, 6, 7, 7};
 
-// I don't care anymore, sorry
-static component *label1;
-static component *label2;
+static component *header_label;
+static component *details_label;
 
 static int32_t har_prices[11] = {20000, 36000, 26000, 28000, 29000, 32000, 25000, 30000, 24000, 22000, 75000};
 
@@ -454,8 +453,8 @@ void lab_menu_focus_blue(component *c, bool focused, void *userdata) {
         } else {
             mechlab_set_hint(s, lang_get(548));
         }
-        label_set_text(label1, "");
-        label_set_text(label2, "");
+        label_set_text(header_label, "");
+        label_set_text(details_label, "");
     }
 }
 
@@ -467,8 +466,8 @@ void lab_menu_focus_yellow(component *c, bool focused, void *userdata) {
         } else {
             mechlab_set_hint(s, lang_get(552));
         }
-        label_set_text(label1, "");
-        label_set_text(label2, "");
+        label_set_text(header_label, "");
+        label_set_text(details_label, "");
     }
 }
 
@@ -480,8 +479,8 @@ void lab_menu_focus_red(component *c, bool focused, void *userdata) {
         } else {
             mechlab_set_hint(s, lang_get(550));
         }
-        label_set_text(label1, "");
-        label_set_text(label2, "");
+        label_set_text(header_label, "");
+        label_set_text(details_label, "");
     }
 }
 
@@ -493,26 +492,26 @@ static void lab_menu_focus_arm_power(component *c, bool focused, void *userdata)
         game_player *p1 = game_state_get_player(s->gs, 0);
         sd_pilot *pilot = game_player_get_pilot(p1);
         if(mechlab_get_selling(s)) {
-            label_set_text(label1, "ARM POWER:\n\nSALES PRICE:");
+            label_set_text(header_label, "ARM POWER:\n\nSALES PRICE:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->arm_power];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format((int)(price * 0.85), price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->arm_power, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(553)); // TODO sprintf arm/leg
         } else {
-            label_set_text(label1, "ARM POWER:\n\nUPGRADE COST:");
+            label_set_text(header_label, "ARM POWER:\n\nUPGRADE COST:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->arm_power + 1];
             bool max_level = pilot->arm_power >= max_arm_power[pilot->har_id];
             if(price < 1 || max_level) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format(price, price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->arm_power + 1, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(554)); // TODO sprintf arm/leg
         }
@@ -527,26 +526,26 @@ static void lab_menu_focus_leg_power(component *c, bool focused, void *userdata)
         game_player *p1 = game_state_get_player(s->gs, 0);
         sd_pilot *pilot = game_player_get_pilot(p1);
         if(mechlab_get_selling(s)) {
-            label_set_text(label1, "LEG POWER:\n\nSALES PRICE:");
+            label_set_text(header_label, "LEG POWER:\n\nSALES PRICE:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->leg_power];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format((int)(price * 0.85), price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->leg_power, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(555)); // TODO sprintf arm/leg
         } else {
-            label_set_text(label1, "LEG POWER:\n\nUPGRADE COST:");
+            label_set_text(header_label, "LEG POWER:\n\nUPGRADE COST:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->leg_power + 1];
             bool max_level = pilot->leg_power >= max_leg_power[pilot->har_id];
             if(price < 1 || max_level) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format(price, price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->leg_power + 1, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(556)); // TODO sprintf arm/leg
         }
@@ -561,26 +560,26 @@ static void lab_menu_focus_arm_speed(component *c, bool focused, void *userdata)
         game_player *p1 = game_state_get_player(s->gs, 0);
         sd_pilot *pilot = game_player_get_pilot(p1);
         if(mechlab_get_selling(s)) {
-            label_set_text(label1, "ARM SPEED:\n\nSALES PRICE:");
+            label_set_text(header_label, "ARM SPEED:\n\nSALES PRICE:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->arm_speed];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format((int)(price * 0.85), price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->arm_speed, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(557)); // TODO sprintf arm/leg
         } else {
-            label_set_text(label1, "ARM SPEED:\n\nUPGRADE COST:");
+            label_set_text(header_label, "ARM SPEED:\n\nUPGRADE COST:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->arm_speed + 1];
             bool max_level = pilot->arm_speed >= max_arm_speed[pilot->har_id];
             if(price < 1 || max_level) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format(price, price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->arm_speed + 1, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(558)); // TODO sprintf arm/leg
         }
@@ -595,26 +594,26 @@ static void lab_menu_focus_leg_speed(component *c, bool focused, void *userdata)
         game_player *p1 = game_state_get_player(s->gs, 0);
         sd_pilot *pilot = game_player_get_pilot(p1);
         if(mechlab_get_selling(s)) {
-            label_set_text(label1, "LEG SPEED:\n\nSALES PRICE:");
+            label_set_text(header_label, "LEG SPEED:\n\nSALES PRICE:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->leg_speed];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format((int)(price * 0.85), price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->leg_speed, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(559)); // TODO sprintf arm/leg
         } else {
-            label_set_text(label1, "LEG SPEED:\n\nUPGRADE COST:");
+            label_set_text(header_label, "LEG SPEED:\n\nUPGRADE COST:");
             int32_t price = arm_leg_prices[pilot->har_id][pilot->leg_speed + 1];
             bool max_level = pilot->leg_speed >= max_leg_speed[pilot->har_id];
             if(price < 1 || max_level) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format(price, price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->leg_speed + 1, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(560)); // TODO sprintf arm/leg
         }
@@ -629,25 +628,25 @@ static void lab_menu_focus_armor(component *c, bool focused, void *userdata) {
         game_player *p1 = game_state_get_player(s->gs, 0);
         sd_pilot *pilot = game_player_get_pilot(p1);
         if(mechlab_get_selling(s)) {
-            label_set_text(label1, "ARMOR PLATE:\n\nSALES PRICE:");
+            label_set_text(header_label, "ARMOR PLATE:\n\nSALES PRICE:");
             int32_t price = armor_prices[pilot->har_id][pilot->armor];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format((int)(price * 0.85), price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->armor, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(561));
         } else {
-            label_set_text(label1, "ARMOR PLATE:\n\nUPGRADE COST:");
+            label_set_text(header_label, "ARMOR PLATE:\n\nUPGRADE COST:");
             int32_t price = armor_prices[pilot->har_id][pilot->armor + 1];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format(price, price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->armor + 1, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(562));
         }
@@ -662,25 +661,25 @@ static void lab_menu_focus_stun_resistance(component *c, bool focused, void *use
         game_player *p1 = game_state_get_player(s->gs, 0);
         sd_pilot *pilot = game_player_get_pilot(p1);
         if(mechlab_get_selling(s)) {
-            label_set_text(label1, "STUN RES.:\n\nSALES PRICE:");
+            label_set_text(header_label, "STUN RES.:\n\nSALES PRICE:");
             int32_t price = stun_resistance_prices[pilot->har_id][pilot->stun_resistance];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format((int)(price * 0.85), price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->stun_resistance, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(563));
         } else {
-            label_set_text(label1, "STUN RES.:\n\nUPGRADE COST:");
+            label_set_text(header_label, "STUN RES.:\n\nUPGRADE COST:");
             int32_t price = stun_resistance_prices[pilot->har_id][pilot->stun_resistance + 1];
             if(price < 1) {
-                label_set_text(label2, "Unavailable\n\nUnavailable");
+                label_set_text(details_label, "Unavailable\n\nUnavailable");
             } else {
                 score_format(price, price_str, sizeof(price_str));
                 snprintf(tmp, sizeof(tmp), "Level %d\n\n$ %sK", pilot->stun_resistance + 1, price_str);
-                label_set_text(label2, tmp);
+                label_set_text(details_label, tmp);
             }
             mechlab_set_hint(s, lang_get(564));
         }
@@ -709,10 +708,10 @@ void lab_menu_focus_trade(component *c, bool focused, void *userdata) {
         DEBUG("got %d trades from the bitmask %d", tradecount, p1->pilot->har_trades);
         // check if there's anything for trade
         if(tradecount == 0) {
-            label_set_text(label1, lang_get(488));
-            label_set_text(label2, "");
+            label_set_text(header_label, lang_get(488));
+            label_set_text(details_label, "");
         } else {
-            label_set_text(label1, lang_get(461));
+            label_set_text(header_label, lang_get(461));
             char tmp[200] = "";
             // pick 5 of however many we got
             // naturally, I unrolled this loop for performance
@@ -730,7 +729,7 @@ void lab_menu_focus_trade(component *c, bool focused, void *userdata) {
                 snprintf(tmp, 200, "%s %s %s %s %s", lang_get(31 + trades[0]), lang_get(31 + trades[1]),
                          lang_get(31 + trades[2]), lang_get(31 + trades[3]), lang_get(31 + trades[4]));
             }
-            label_set_text(label2, tmp);
+            label_set_text(details_label, tmp);
         }
     }
 }
@@ -743,8 +742,8 @@ void lab_menu_focus_done(component *c, bool focused, void *userdata) {
         } else {
             mechlab_set_hint(s, lang_get(568));
         }
-        label_set_text(label1, "");
-        label_set_text(label2, "");
+        label_set_text(header_label, "");
+        label_set_text(details_label, "");
     }
 }
 
@@ -811,16 +810,16 @@ component *lab_menu_customize_create(scene *s) {
     tconf.valign = TEXT_TOP;
     tconf.lspacing = 2;
     tconf.cforeground = 0xA5;
-    label1 = label_create(&tconf, "");
-    component_set_size_hints(label1, 90, 80);
-    component_set_pos_hints(label1, 210, 150);
-    trnmenu_attach(menu, label1);
+    header_label = label_create(&tconf, "");
+    component_set_size_hints(header_label, 90, 80);
+    component_set_pos_hints(header_label, 210, 150);
+    trnmenu_attach(menu, header_label);
 
     tconf.cforeground = 0xA7;
-    label2 = label_create(&tconf, "");
-    component_set_size_hints(label2, 90, 80);
-    component_set_pos_hints(label2, 210, 158);
-    trnmenu_attach(menu, label2);
+    details_label = label_create(&tconf, "");
+    component_set_size_hints(details_label, 90, 80);
+    component_set_pos_hints(details_label, 210, 158);
+    trnmenu_attach(menu, details_label);
 
     // Bind hand animation
     trnmenu_bind_hand(menu, hand_of_doom, s->gs);
