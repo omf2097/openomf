f33c08817f38b99348f472aa925e1a661e1b1292
Magnus Larsen <golgothasTerror101@gmail.com>
2024-10-23 20:28:29 -0700

    Support vcpkg for dependencies

--- cmake-scripts/Findconfuse.cmake
+++ cmake-scripts/Findconfuse.cmake
@@ -1,4 +1,15 @@
 
+if(VCPKG_TOOLCHAIN)
+    find_package(unofficial-libconfuse CONFIG)
+    if(unofficial-libconfuse_FOUND)
+        get_target_property(CONFUSE_INCLUDE_DIR unofficial::libconfuse::libconfuse INTERFACE_INCLUDE_DIRECTORIES)
+        set(CONFUSE_LIBRARY unofficial::libconfuse::libconfuse)
+
+        mark_as_advanced(CONFUSE_INCLUDE_DIR CONFUSE_LIBRARY)
+        return()
+    endif(unofficial-libconfuse_FOUND)
+endif(VCPKG_TOOLCHAIN)
+
 SET(CONFUSE_SEARCH_PATHS
     /usr/local/
     /usr
