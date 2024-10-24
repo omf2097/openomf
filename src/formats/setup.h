1d67373c75b8582d59f4d8712e846e2e770a9d9a
Magnus Larsen <golgothasTerror101@gmail.com>
2024-10-23 21:33:27 -0700

    Use static_assert to check packing instead of __attribute__((packed))
    Note that the alignof() these structures will no longer be 1, but I
    think this is correct.

--- src/formats/setup.h
+++ src/formats/setup.h
@@ -10,6 +10,7 @@
 #ifndef SD_SETUP_H
 #define SD_SETUP_H
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -26,7 +27,8 @@ typedef struct {
     uint16_t unk3 : 2;
     uint16_t power_2 : 3;
     uint16_t unk4 : 3;
-} __attribute__((packed)) gflags0;
+} gflags0;
+static_assert(2 == sizeof(gflags0), "gflags0 should pack into 2 bytes");
 
 typedef struct {
     uint8_t knockdown : 2;
@@ -35,20 +37,23 @@ typedef struct {
     uint8_t hyper_mode : 1;
     uint8_t screen_shakes : 1;
     uint8_t animations : 1;
-} __attribute__((packed)) gflags1;
+} gflags1;
+static_assert(1 == sizeof(gflags1), "gflags1 should pack into 1 byte");
 
 typedef struct {
     uint8_t palette_animation : 1;
     uint8_t unk2 : 2;
     uint8_t snow_checking : 1;
     uint8_t unk3 : 4;
-} __attribute__((packed)) gflags2;
+} gflags2;
+static_assert(1 == sizeof(gflags2), "gflags2 should pack into 1 byte");
 
 typedef struct {
     uint32_t stereo_reversed : 1;
     uint32_t match_count : 2;
     uint32_t unk : 29;
-} __attribute__((packed)) gflags3;
+} gflags3;
+static_assert(4 == sizeof(gflags3), "gflags3 should pack into 4 bytes");
 
 typedef struct {
     uint8_t game_speed;
@@ -71,7 +76,8 @@ typedef struct {
     uint8_t sound_volume;
     uint8_t music_volume;
     char unknown_r[38];
-} __attribute__((packed)) sd_setup_file;
+} sd_setup_file;
+static_assert(296 == sizeof(sd_setup_file), "sd_setup_file should pack into 296 bytes");
 
 int sd_setup_create(sd_setup_file *setup);
 void sd_setup_free(sd_setup_file *setup);
