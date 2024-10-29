4967e356f72b1b24788dc9d33b8e1dedf891440a
Magnus Larsen <golgothasTerror101@gmail.com>
2024-10-28 21:51:50 -0700

    Fix & simplify Windows scandir

--- src/utils/scandir.c
+++ src/utils/scandir.c
@@ -1,4 +1,6 @@
 #include "utils/scandir.h"
+#include "utils/str.h"
+#include <assert.h>
 #include <string.h>
 
 #if defined(_WIN32) || defined(WIN32)
@@ -7,18 +9,36 @@
 #include <dirent.h>
 #endif
 
+static void check_valid_directory(char const *dir) {
+#if !NDEBUG
+    assert(dir != NULL);
+    assert(dir[0] != '\0');
+    size_t end = strlen(dir) - 1;
+#if _WIN32
+    assert(dir[end] == '\\');
+#else
+    assert(dir[end] == '/');
+#endif
+#endif
+}
+
 int scan_directory(list *dir_list, const char *dir) {
+    check_valid_directory(dir);
 #if defined(_WIN32) || defined(WIN32)
 
+    str glob;
+    str_from_format(&glob, "%s*", dir);
     WIN32_FIND_DATAA entry;
     HANDLE hFind;
-    if((hFind = FindFirstFileA(dir, &entry)) == INVALID_HANDLE_VALUE) {
+    if((hFind = FindFirstFileA(str_c(&glob), &entry)) == INVALID_HANDLE_VALUE) {
+        str_free(&glob);
         return 1;
     }
     while(FindNextFileA(hFind, &entry) != FALSE) {
         list_append(dir_list, entry.cFileName, strlen(entry.cFileName) + 1);
     }
     FindClose(hFind);
+    str_free(&glob);
     return 0;
 
 #else
@@ -38,21 +58,22 @@ int scan_directory(list *dir_list, const char *dir) {
 }
 
 int scan_directory_prefix(list *dir_list, const char *dir, const char *prefix) {
+    check_valid_directory(dir);
 #if defined(_WIN32) || defined(WIN32)
 
+    str glob;
+    str_from_format(&glob, "%s%s*", prefix, dir);
     WIN32_FIND_DATAA entry;
     HANDLE hFind;
-    if((hFind = FindFirstFileA(dir, &entry)) == INVALID_HANDLE_VALUE) {
+    if((hFind = FindFirstFileA(str_c(&glob), &entry)) == INVALID_HANDLE_VALUE) {
+        str_free(&glob);
         return 1;
     }
-    size_t prefix_len = strlen(prefix);
     while(FindNextFileA(hFind, &entry) != FALSE) {
-        size_t filename_len = strlen(entry.cFileName);
-        if(filename_len >= prefix_len && memcmp(entry.cFileName, prefix, prefix_len) == 0) {
-            list_append(dir_list, entry.cFileName, filename_len + 1);
-        }
+        list_append(dir_list, entry.cFileName, strlen(entry.cFileName) + 1);
     }
     FindClose(hFind);
+    str_free(&glob);
     return 0;
 
 #else
@@ -76,21 +97,22 @@ int scan_directory_prefix(list *dir_list, const char *dir, const char *prefix) {
 }
 
 int scan_directory_suffix(list *dir_list, const char *dir, const char *suffix) {
+    check_valid_directory(dir);
 #if defined(_WIN32) || defined(WIN32)
 
+    str glob;
+    str_from_format(&glob, "%s*%s", dir, suffix);
     WIN32_FIND_DATAA entry;
     HANDLE hFind;
-    if((hFind = FindFirstFileA(dir, &entry)) == INVALID_HANDLE_VALUE) {
+    if((hFind = FindFirstFileA(str_c(&glob), &entry)) == INVALID_HANDLE_VALUE) {
+        str_free(&glob);
         return 1;
     }
-    size_t suffix_len = strlen(suffix);
     while(FindNextFileA(hFind, &entry) != FALSE) {
-        size_t filename_len = strlen(entry.cFileName);
-        if(filename_len >= suffix_len && memcmp(entry.cFileName + filename_len - suffix_len, suffix, suffix_len) == 0) {
-            list_append(dir_list, entry.cFileName, filename_len + 1);
-        }
+        list_append(dir_list, entry.cFileName, strlen(entry.cFileName) + 1);
     }
     FindClose(hFind);
+    str_free(&glob);
     return 0;
 
 #else
