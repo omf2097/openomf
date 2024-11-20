d98876a0784bbf007b222c2aa9c95bd990c245d1
Tuomas Virtanen <katajakasa@gmail.com>
2024-11-20 20:44:41 +0200

    Select correct renderer in video settings

--- src/game/scenes/mainmenu/menu_video.c
+++ src/game/scenes/mainmenu/menu_video.c
@@ -141,11 +141,14 @@ component *menu_video_create(scene *s) {
         textselector_create(&tconf, "API:", "Choose the video renderer API to use", renderer_toggled, local);
     menu_attach(menu, renderer_selector);
 
-    // Add available renderers
+    // Add available renderers and make sure correct one is selected by default.
     const char *r_name;
     for(int r = 0; r < video_get_renderer_count(); r++) {
         video_get_renderer_info(r, &r_name, NULL);
         textselector_add_option(renderer_selector, r_name);
+        if (strcmp(r_name, setting->video.renderer) == 0) {
+            textselector_set_pos(renderer_selector, r);
+        }
     }
 
     // Resolution selector
