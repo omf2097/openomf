016ef463b1194231178e67b13ee9354489f8cfdf
Magnus Larsen <golgothasTerror101@gmail.com>
2024-11-22 07:44:01 -0800

    Remove compat's strdup

--- src/utils/compat.h
+++ src/utils/compat.h
@@ -1,7 +1,6 @@
 #ifndef COMPAT_H
 #define COMPAT_H
 
-#include "platform.h"
 #include <string.h>
 
 #if __APPLE__
@@ -11,10 +10,6 @@ typedef uint_least16_t char16_t;
 typedef uint_least32_t char32_t;
 #else
 #include <uchar.h>
-#endif
-
-#ifndef HAVE_STD_STRDUP
-char *strdup(const char *s1);
 #endif
 
 #endif // COMPAT_H
