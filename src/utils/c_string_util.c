921878aa18fece796ab9e210d4e78a85eb6c9d6e
Magnus Larsen <golgothasTerror101@gmail.com>
2024-11-25 11:05:23 -0800

    Add omf_strnlen_s, a strlen for fixed-size strings

--- src/utils/c_string_util.c
+++ src/utils/c_string_util.c
@@ -1,5 +1,6 @@
 #include "utils/c_string_util.h"
 #include "utils/allocator.h"
+#include <assert.h>
 #include <string.h>
 
 char *strncpy_or_truncate(char *dest, const char *src, size_t n) {
@@ -15,4 +16,13 @@ char *omf_strdup_real(char const *s, char const *file, int line) {
     char *new_s = omf_malloc_real(valid_range, file, line);
     memcpy(new_s, s, valid_range);
     return new_s;
+}
+
+size_t omf_strnlen_s(char const *str, size_t strsz) {
+    assert(!(!str && strsz));
+    if(!str) {
+        return 0;
+    }
+    char const *found = memchr(str, '\0', strsz);
+    return found ? (size_t)(found - str) : strsz;
 }
--- src/utils/c_string_util.h
+++ src/utils/c_string_util.h
@@ -10,4 +10,8 @@ char *strncpy_or_truncate(char *dest, const char *src, size_t n);
 char *omf_strdup_real(char const *s, char const *file, int line);
 #define omf_strdup(s) omf_strdup_real((s), __FILE__, __LINE__)
 
+// Reads up to strsz bytes of str, returning the position of the
+// first null character or strsz if it was not found.
+size_t omf_strnlen_s(char const *str, size_t strsz);
+
 #endif // C_STRING_UTIL_H
