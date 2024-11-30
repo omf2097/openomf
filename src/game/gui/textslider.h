3221ef5f9647891a8360252e4c01ce5cd8092afe
Magnus Larsen <golgothasTerror101@gmail.com>
2024-11-30 02:17:39 -0800

    Disable panning on volume slider sfx

--- src/game/gui/textslider.h
+++ src/game/gui/textslider.h
@@ -11,5 +11,7 @@ component *textslider_create(const text_settings *tconf, const char *text, const
 component *textslider_create_bind(const text_settings *tconf, const char *text, const char *help,
                                   unsigned int positions, int has_off, textslider_slide_cb cb, void *userdata,
                                   int *bind);
+// by default, the "next" and "previous" item selection noises are panned; call this to disable that effect.
+void textslider_disable_panning(component *c);
 
 #endif // TEXTSLIDER_H
