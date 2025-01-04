e40a77f370e62d7ef4e16af30fbafebc4d1d0855
Magnus Larsen <golgothasTerror101@gmail.com>
2025-01-03 21:08:57 -0800

    Remove old LANG and LANG2 enums

--- src/resources/languages.h
+++ src/resources/languages.h
@@ -13,33 +13,6 @@
 bool lang_init(void);
 void lang_close(void);
 
-/*! \brief OMF 2097 String ID
- *
- * These string IDs match OMFv2.1 (Epic Challenge Arena)
- */
-enum
-{
-    // there are 10 HARs
-    LANG_STR_HAR = 31,
-    LANG_STR_NEWSROOM_NEWCHAMPION = 79,
-    // there are 3*2 pronouns
-    LANG_STR_PRONOUN = 81,
-    // there are 24*2 newsroom texts
-    LANG_STR_NEWSROOM_TEXT = 87,
-
-    LANG_STR_COUNT = 1013,
-};
-
-/*! \brief OpenOMF String ID
- *
- * These string IDs should match BuildLanguages.cmake and the various ${LANG}2.TXT files
- */
-enum
-{
-    LANG2_STR_LANGUAGE,
-    LANG2_STR_COUNT
-};
-
 // Gets a localization string
 const char *lang_get(int id);
 
