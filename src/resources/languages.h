004557270d0d7c58de2d73273d4210c480372254
Magnus Larsen <golgothasTerror101@gmail.com>
2025-01-03 21:17:49 -0800

    Add doc comments to languages.h

--- src/resources/languages.h
+++ src/resources/languages.h
@@ -13,10 +13,21 @@
 bool lang_init(void);
 void lang_close(void);
 
-// Gets a localization string
+/**
+ * @brief Gets a C string from a language string index.
+ * @details Cheap to call, as it's a simple array lookup.
+ *  Currently, returned strings are encoded in CP437; this may
+ *  not be true in the future, there's been talk of switching to UTF-8.
+ * @param id Lang id to translate, from the enum. Example: LangChooseYourPilot
+ */
 const char *lang_get(int id);
 
-// Gets an openomf localization string (from one of the tables)
+/**
+ * @brief Translates a language string range and offset to a C String.
+ * @details Cheap to call, as it's a single array lookup.
+ * @param LangStr Lang id to translate, from the enum. Example: LangTrainingHelp
+ * @param offset into the range, 0 returns the first of the range.
+ */
 #define lang_get_offset(LangStr, offset) lang_get_offset_impl(LangStr, LangStr##_Last, (offset))
 const char *lang_get_offset_impl(int id, int last, int offset);
 
