76575aa717fbe4b853ae9eb5fc27a9bc8898f3ed
Tuomas Virtanen <katajakasa@gmail.com>
2025-02-08 23:00:30 +0200

    Improve array tests

--- testing/test_array.c
+++ testing/test_array.c
@@ -5,44 +5,53 @@
 #include <utils/array.h>
 #include <utils/iterator.h>
 
-array test_array;
-void *test_ptr;
-
-#define TEST_STR_SIZE 8
+static const char test_data[5] = {'T', 'E', 'S', 'T', 0};
 
 void test_array_create(void) {
+    array test_array;
     array_create(&test_array);
     CU_ASSERT(test_array.filled == 0);
     CU_ASSERT(test_array.allocated_size != 0);
     CU_ASSERT(test_array.data != NULL);
-}
-
-void test_array_free(void) {
     array_free(&test_array);
     CU_ASSERT(test_array.filled == 0);
 }
 
 void test_array_set(void) {
-    void *test_data = omf_calloc(TEST_STR_SIZE, 1);
-    memset(test_data, 1, TEST_STR_SIZE);
-    test_ptr = test_data;
+    array test_array;
+    array_create(&test_array);
     array_set(&test_array, 0, test_data);
     CU_ASSERT(test_array.filled == 1);
+    CU_ASSERT(test_array.allocated_size >= 4);
+    array_free(&test_array);
 }
 
 void test_array_get(void) {
-    CU_ASSERT_FATAL(array_get(&test_array, 0) != NULL)
-    CU_ASSERT(array_get(&test_array, 1) == NULL);
-    CU_ASSERT_FATAL(array_get(&test_array, 0) == test_ptr);
+    array test_array;
+    array_create(&test_array);
+    array_set(&test_array, 0, test_data);
 
-    char *value = array_get(&test_array, 0);
-    for(int i = 0; i < TEST_STR_SIZE; i++) {
-        CU_ASSERT(value[i] == 1);
-    }
+    CU_ASSERT_FATAL(array_get(&test_array, 0) == test_data);
+    CU_ASSERT_STRING_EQUAL(array_get(&test_array, 0), test_data);
+    CU_ASSERT(array_get(&test_array, 1) == NULL);
+
+    array_free(&test_array);
+}
+
+void test_array_iter_next(void) {
+    array test_array;
+    array_create(&test_array);
+    array_set(&test_array, 0, test_data);
+
+    iterator it;
+    array_iter_begin(&test_array, &it);
+    CU_ASSERT_STRING_EQUAL(iter_next(&it), test_data);
+    CU_ASSERT(iter_next(&it) == NULL);
+
+    array_free(&test_array);
 }
 
 void array_test_suite(CU_pSuite suite) {
-    // Add tests
     if(CU_add_test(suite, "Test for array create", test_array_create) == NULL) {
         return;
     }
@@ -52,7 +61,7 @@ void array_test_suite(CU_pSuite suite) {
     if(CU_add_test(suite, "Test for array get", test_array_get) == NULL) {
         return;
     }
-    if(CU_add_test(suite, "Test for array free", test_array_free) == NULL) {
+    if(CU_add_test(suite, "Test for array iter_next", test_array_iter_next) == NULL) {
         return;
     }
 }
