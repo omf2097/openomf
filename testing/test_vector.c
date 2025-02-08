60ec1a3bb6f4aca337f7c01c2760f79df0aa2d7e
Tuomas Virtanen <katajakasa@gmail.com>
2025-02-09 00:59:53 +0200

    Add missing vector function tests

--- testing/test_vector.c
+++ testing/test_vector.c
@@ -53,7 +53,7 @@ void test_vector_get(void) {
     vector_free(&test_vector);
 }
 
-void test_vector_iterator(void) {
+void test_vector_iter_next(void) {
     int values[2] = {1, 2};
     vector test_vector;
     vector_create(&test_vector, sizeof(int));
@@ -65,6 +65,22 @@ void test_vector_iterator(void) {
     CU_ASSERT(*(int *)iter_next(&it) == values[0]);
     CU_ASSERT(*(int *)iter_next(&it) == values[1]);
     CU_ASSERT(iter_next(&it) == NULL);
+
+    vector_free(&test_vector);
+}
+
+void test_vector_iter_prev(void) {
+    int values[2] = {1, 2};
+    vector test_vector;
+    vector_create(&test_vector, sizeof(int));
+    vector_append(&test_vector, &values[0]);
+    vector_append(&test_vector, &values[1]);
+
+    iterator it;
+    vector_iter_end(&test_vector, &it);
+    CU_ASSERT(*(int *)iter_prev(&it) == values[1]);
+    CU_ASSERT(*(int *)iter_prev(&it) == values[0]);
+    CU_ASSERT(iter_prev(&it) == NULL);
 
     vector_free(&test_vector);
 }
@@ -89,6 +105,55 @@ void test_vector_delete(void) {
     // Make sure the iterator returns nothing
     vector_iter_begin(&test_vector, &it);
     CU_ASSERT_PTR_NULL(iter_next(&it));
+
+    vector_free(&test_vector);
+}
+
+void test_vector_swap_delete_at(void) {
+    int values[2] = {1, 2};
+    vector test_vector;
+    vector_create(&test_vector, sizeof(int));
+    vector_append(&test_vector, &values[0]);
+    vector_append(&test_vector, &values[1]);
+
+    vector_swapdelete_at(&test_vector, 0);
+
+    CU_ASSERT(vector_size(&test_vector) == 1);
+    iterator it;
+    vector_iter_begin(&test_vector, &it);
+    CU_ASSERT(*(int *)iter_next(&it) == values[1]);
+    CU_ASSERT_PTR_NULL(iter_next(&it));
+
+    vector_free(&test_vector);
+}
+
+void test_vector_pop(void) {
+    int values[2] = {1, 2};
+    vector test_vector;
+    vector_create(&test_vector, sizeof(int));
+    vector_append(&test_vector, &values[0]);
+    vector_append(&test_vector, &values[1]);
+
+    vector_pop(&test_vector);
+
+    CU_ASSERT(vector_size(&test_vector) == 1);
+    iterator it;
+    vector_iter_begin(&test_vector, &it);
+    CU_ASSERT(*(int *)iter_next(&it) == values[0]);
+    CU_ASSERT_PTR_NULL(iter_next(&it));
+
+    vector_free(&test_vector);
+}
+
+void test_vector_back(void) {
+    int values[2] = {1, 2};
+    vector test_vector;
+    vector_create(&test_vector, sizeof(int));
+    vector_append(&test_vector, &values[0]);
+    vector_append(&test_vector, &values[1]);
+
+    const void *ptr = vector_back(&test_vector);
+    CU_ASSERT(*(int *)ptr == values[1]);
 
     vector_free(&test_vector);
 }
@@ -127,13 +192,25 @@ void vector_test_suite(CU_pSuite suite) {
     if(CU_add_test(suite, "Test for vector get", test_vector_get) == NULL) {
         return;
     }
-    if(CU_add_test(suite, "Test for vector iterator", test_vector_iterator) == NULL) {
+    if(CU_add_test(suite, "Test for vector iter_next", test_vector_iter_next) == NULL) {
+        return;
+    }
+    if(CU_add_test(suite, "Test for vector iter_prev", test_vector_iter_prev) == NULL) {
         return;
     }
     if(CU_add_test(suite, "Test for vector delete", test_vector_delete) == NULL) {
         return;
     }
+    if(CU_add_test(suite, "Test for vector swap_delete_at", test_vector_swap_delete_at) == NULL) {
+        return;
+    }
     if(CU_add_test(suite, "Test for zero size vector operation", test_vector_zero_size) == NULL) {
+        return;
+    }
+    if(CU_add_test(suite, "Test for vector pop", test_vector_pop) == NULL) {
+        return;
+    }
+    if(CU_add_test(suite, "Test for vector back", test_vector_back) == NULL) {
         return;
     }
 }
