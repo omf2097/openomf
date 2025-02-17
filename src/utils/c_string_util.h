35fb6b63433fbe36dd91018096e47243f217a941
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-02-17 12:51:07 +0200

    utils: Add omf_strndup()

--- src/utils/c_string_util.h
+++ src/utils/c_string_util.h
@@ -10,6 +10,10 @@ char *strncpy_or_truncate(char *dest, const char *src, size_t n);
 char *omf_strdup_real(char const *s, char const *file, int line);
 #define omf_strdup(s) omf_strdup_real((s), __FILE__, __LINE__)
 
+// strndup() that uses our allocator
+char *omf_strndup_real(char const *s, size_t n, char const *file, int line);
+#define omf_strndup(s, n) omf_strndup_real((s), (n), __FILE__, __LINE__)
+
 // Reads up to strsz bytes of str, returning the position of the
 // first null character or strsz if it was not found.
 size_t omf_strnlen_s(char const *str, size_t strsz);
