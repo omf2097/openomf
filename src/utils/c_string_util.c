ddb0cc7feeef26714905f0c5633116332b901e36
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-02-17 12:51:07 +0200

    utils: Add omf_strndup()

--- src/utils/c_string_util.c
+++ src/utils/c_string_util.c
@@ -1,5 +1,6 @@
 #include "utils/c_string_util.h"
 #include "utils/allocator.h"
+#include "utils/miscmath.h"
 #include <assert.h>
 #include <string.h>
 
@@ -14,6 +15,14 @@ char *omf_strdup_real(char const *s, char const *file, int line) {
     assert(s != NULL);
     size_t valid_range = strlen(s) + 1;
     char *new_s = omf_malloc_real(valid_range, file, line);
+    memcpy(new_s, s, valid_range);
+    return new_s;
+}
+
+char *omf_strndup_real(char const *s, size_t n, char const *file, int line) {
+    assert(s != NULL);
+    size_t valid_range = smin2(strlen(s), n);
+    char *new_s = omf_calloc_real(valid_range + 1, 1, file, line);
     memcpy(new_s, s, valid_range);
     return new_s;
 }
