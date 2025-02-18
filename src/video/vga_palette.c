863429eae6187157798ecda00917eaf529d757ca
Tuomas Virtanen <katajakasa@gmail.com>
2025-02-18 21:53:15 +0200

    CR: Move comments to header file

--- src/video/vga_palette.c
+++ src/video/vga_palette.c
@@ -8,15 +8,6 @@ void vga_palette_init(vga_palette *palette) {
     memset(palette, 0, sizeof(vga_palette));
 }
 
-/**
- * Tint each palette color with a reference color.
- *
- * @param pal Palette to operate on
- * @param ref_index Reference colour index for the mixed color
- * @param start Palette start index
- * @param end Palette end index
- * @param step Strength of the effect (0 - 255).
- */
 void vga_palette_tint_range(vga_palette *pal, vga_index ref_index, vga_index start, vga_index end, uint8_t step) {
     vga_color ref = pal->colors[ref_index];
     uint32_t m, u;
@@ -29,15 +20,6 @@ void vga_palette_tint_range(vga_palette *pal, vga_index ref_index, vga_index sta
     }
 }
 
-/**
- * Mix each palette color together with a reference color.
- *
- * @param pal Palette to operate on
- * @param ref_index Reference colour index for the mixed color
- * @param start Palette start index
- * @param end Palette end index
- * @param step How much is mixed (0 - 255).
- */
 void vga_palette_mix_range(vga_palette *pal, vga_index ref_index, vga_index start, vga_index end, uint8_t step) {
     vga_color ref = pal->colors[ref_index];
     uint32_t inv = 255 - step;
--- src/video/vga_palette.h
+++ src/video/vga_palette.h
@@ -21,7 +21,26 @@ static_assert(768 == sizeof(vga_palette), "vga_palette should pack into 768 byte
 
 void vga_palette_init(vga_palette *palette);
 
+/**
+ * Tint each palette color with a reference color.
+ *
+ * @param pal Palette to operate on
+ * @param ref_index Reference colour index for the mixed color
+ * @param start Palette start index
+ * @param end Palette end index
+ * @param step Strength of the effect (0 - 255).
+ */
 void vga_palette_tint_range(vga_palette *pal, vga_index ref_index, vga_index start, vga_index end, uint8_t step);
+
+/**
+ * Mix each palette color together with a reference color.
+ *
+ * @param pal Palette to operate on
+ * @param ref_index Reference colour index for the mixed color
+ * @param start Palette start index
+ * @param end Palette end index
+ * @param step How much is mixed (0 - 255).
+ */
 void vga_palette_mix_range(vga_palette *pal, vga_index ref_index, vga_index start, vga_index end, uint8_t step);
 
 #endif // VGA_PALETTE_H
