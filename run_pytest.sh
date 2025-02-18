647f96271c399fc76c783ae4b11fe8c01023478e
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-02-18 14:01:30 +0200

    run_pytest.sh: Find openomf executable from build directory

--- run_pytest.sh
+++ run_pytest.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-if [ -z "$2" ]; then
-  echo "Usage: $0 <build-dir> <openomf-executable>" >&2
+if [ -z "$1" ]; then
+  echo "Usage: $0 <build-dir>" >&2
   exit 1
 fi
 
 export BUILD_DIR="$1"
-export OPENOMF_BIN="$2"
+OPENOMF_BIN=$(find "$BUILD_DIR" -name openomf -type f -executable -print -quit)
+if [ -z "$OPENOMF_BIN" ]; then
+  echo "Could not find openomf executable from $BUILD_DIR" >&2
+  exit 1
+fi
+export OPENOMF_BIN="./${OPENOMF_BIN#$BUILD_DIR}"
 
 exec poetry run pytest -vrP pytest
