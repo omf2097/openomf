a6859d77078b32ed4cb6c3e88640f12cb4209fdf
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2025-03-09 16:35:17 -0700

    Added const to multiplier constants

--- src/game/scenes/mechlab/lab_menu_customize.c
+++ src/game/scenes/mechlab/lab_menu_customize.c
@@ -31,9 +31,9 @@ static int32_t har_prices[11] = {20000, 36000, 26000, 28000, 29000, 32000, 25000
 static int32_t har_upgrade_price[11] = { 380, 400, 350, 400, 500, 330, 420, 370, 450, 360, 700 };
 static int32_t upgrade_level_mutiplier[10] = { 0, 1, 3, 7, 12, 18, 30, 50, 75, 120 };
 
-static int32_t arm_leg_multiplier = 2;
-static int32_t stun_res_multiplier = 3;
-static int32_t armor_multiplier = 5;
+static const int32_t arm_leg_multiplier = 2;
+static const int32_t stun_res_multiplier = 3;
+static const int32_t armor_multiplier = 5;
 
 static void lab_menu_focus_arm_power(component *c, bool focused, void *userdata);
 static void lab_menu_focus_arm_speed(component *c, bool focused, void *userdata);
