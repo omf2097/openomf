ca3d15d3de6916edc53c61b7214c1c20ea928b1e
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2025-03-11 08:44:09 -0700

    HAR Upgrade Formula (#980)
    * Upgrade price tables replaced with actual formula (expanding in the future will be easier...)
    
    * Created lab_har_constants.h, may migrate more values in the future

---
+++ src/game/scenes/mechlab/lab_har_constants.h
@@ -0,0 +1,11 @@
+#ifndef LAB_HAR_CONSTANTS_H
+#define LAB_HAR_CONSTANTS_H
+
+static const uint8_t max_arm_speed[11] = {6, 8, 4, 6, 9, 7, 8, 6, 9, 6, 7};
+static const uint8_t max_leg_speed[11] = {8, 9, 5, 6, 8, 8, 7, 6, 7, 5, 6};
+static const uint8_t max_arm_power[11] = {5, 5, 9, 8, 4, 6, 6, 5, 5, 6, 7};
+static const uint8_t max_leg_power[11] = {6, 6, 8, 4, 5, 7, 5, 7, 6, 7, 7};
+static const uint8_t max_stun_res[11] = {8, 6, 8, 6, 7, 6, 6, 7, 7, 7, 6};
+static const uint8_t max_armor[11] = {5, 7, 7, 8, 6, 8, 6, 9, 6, 6, 7};
+
+#endif // LAB_HAR_CONSTANTS_H
