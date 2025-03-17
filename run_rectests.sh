e7418075162a85c9ea6e1b68a79b960ea5f749e6
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2025-03-17 01:37:59 -0700

    Removed invalidated test

--- run_rectests.sh
+++ run_rectests.sh
@@ -19,7 +19,6 @@ tests=(
     "Electra should be able to kick while doing inputs for rolling thunder:65K6P.REC"
     "Jaguar uses same animation for K and 6P but Shadow does not:6P.REC"
     "Novas 1P can hit more than once with the Q tag and use AI for air launch:NOVA-1P.REC"
-    "Jaguar standing throw should not result in overlap with opponent in corner:JAG-CORNER-THROW.REC"
     "Jaguar doing a standing throw as the winning move should not cause a slide:JAG-NO-SLIDE-WIN-THROW.REC"
     "Flail charging punch should cancel when blocked:FLAIL_454P.REC"
     "Jaguar should keep walking right after doing a standing throw if the input is held:KEEP-WALKING-AFTER-JAG-STANDING-THROW.REC"
