80fc1ed72867c3a247b5a5dda606804ddb216d23
Andrew Thompson <andrew@hijacked.us>
2025-03-28 17:45:15 -0700

    Simplify object_distance function
    Co-authored-by: Magnus Larsen <golgothasTerror101@gmail.com>

--- src/game/protos/object.c
+++ src/game/protos/object.c
@@ -506,9 +506,7 @@ void object_move(object *obj) {
 }
 
 float object_distance(object *a, object *b) {
-    float x_dist = b->pos.x - a->pos.x;
-    float y_dist = b->pos.y - a->pos.y;
-    return sqrtf((x_dist * x_dist) + (y_dist * y_dist));
+    return vec2f_dist(a->pos, b->pos);
 }
 
 /** Frees the object and all resources attached to it (even the animation, if it is owned by the object)
