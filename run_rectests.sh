f878768b351c17c5d82b48a8475e3122cb7b90a7
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2025-03-30 22:59:06 -0700

    Added rectest

--- run_rectests.sh
+++ run_rectests.sh
@@ -46,6 +46,7 @@ tests=(
     "Electra's shards can be blocked:SHARDS-ARE-BLOCKABLE.REC"
     "Electra can throw Katana on wakeup without getting hit:WAKEUP_THROW.REC"
     "Katana can chain stomps and end with a jump kick:STOMP.REC"
+    "Gargoyle reaches the top of the stage:FLY.REC"
 )
 
 # Setup temp directory for outputs
