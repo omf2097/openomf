c442cf1241bc049d7a960208c61a438f98477ad4
Magnus Larsen <golgothasTerror101@gmail.com>
2025-04-02 21:52:26 -0700

    trnlist: store tournaments in a vector

--- src/resources/trnmanager.h
+++ src/resources/trnmanager.h
@@ -2,9 +2,10 @@
 #define TRNMANAGER_H
 
 #include "formats/tournament.h"
-#include "utils/list.h"
+#include "utils/vector.h"
 
-list *trnlist_init(void);
+void trnlist_init(vector *trnlist);
+void trnlist_free(vector *trnlist);
 int trn_load(sd_tournament_file *trn, const char *trnname);
 
 #endif // TRNMANAGER_H
