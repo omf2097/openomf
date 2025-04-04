909269e9a92aea172f0d662c1e9427dd225d8bab
Andrew Thompson <andrew@hijacked.us>
2025-04-04 05:28:37 -0700

    Fix to allow Eva/Jazzy chained unranked fights

--- src/game/scenes/newsroom.c
+++ src/game/scenes/newsroom.c
@@ -392,8 +392,7 @@ int newsroom_create(scene *scene) {
             local->challenger = fight_stats->challenger;
             p2->pilot = local->challenger;
             fight_stats->challenger = NULL;
-        } else if(p1->chr && p2->pilot && p2->pilot->rank != 0) {
-            // we didn't just beat an unranked challenger
+        } else if(p1->chr) {
             int health = game_player_get_score(p1)->health;
             // see if we have meet any unranked challenger criteria
             for(int k = p1->chr->pilot.enemies_ex_unranked - 1; k < p1->chr->pilot.enemies_inc_unranked; k++) {
@@ -414,8 +413,13 @@ int newsroom_create(scene *scene) {
                        (!p->req_fighter || p->req_fighter == p1->pilot->har_id) &&
                        (!p->req_scrap || (p->req_scrap && fight_stats->finish >= FINISH_SCRAP)) &&
                        (!p->req_destroy || (p->req_destroy && fight_stats->finish == FINISH_DESTRUCTION))) {
-                        fight_stats->challenger = p;
-                        log_debug("found challenger!");
+                        // check if we just beat an unranked challenger, and if this new unranked is directly tied to
+                        // that (eg. Jazz Jackrabbit and Eva Earlong) otherwise don't allow another unranked to chain
+                        if(p2->pilot && (p2->pilot->rank != 0 ||
+                                         (p->req_enemy && &p1->chr->enemies[p->req_enemy - 1]->pilot == p2->pilot))) {
+                            fight_stats->challenger = p;
+                            log_debug("found challenger!");
+                        }
                         // XXX we are going to assume the first match, from the lowest in the file, is the one to choose
                         break;
                     }
