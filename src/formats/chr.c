5aa0edde902f2c3a67d644a54a3f8bfe7d963ac2
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-04-05 00:58:15 +0300

    formats: Make sd_chr_load() case insensitive about file names

--- src/formats/chr.c
+++ src/formats/chr.c
@@ -71,18 +71,13 @@ int sd_chr_load(sd_chr_file *chr, const char *filename) {
     sd_pilot_load_from_mem(mr, &chr->pilot);
     memreader_close(mr);
 
-    char tmp[200];
-    str pic_file;
-    str trn_file;
+    char tmp[256];
     sd_tournament_file trn;
     sd_pic_file pic, players;
     bool trn_loaded = false;
 
     if(dirname) {
-        str_from_c(&pic_file, chr->pilot.trn_image);
-        str_toupper(&pic_file);
-        snprintf(tmp, 200, "%s%s", dirname, str_c(&pic_file));
-        str_free(&pic_file);
+        snprintf(tmp, sizeof(tmp), "%s%s", dirname, chr->pilot.trn_image);
         sd_pic_create(&pic);
         sd_pic_load(&pic, tmp);
 
@@ -102,10 +97,7 @@ int sd_chr_load(sd_chr_file *chr, const char *filename) {
         }
 
         if(*chr->pilot.trn_name != '\0') {
-            str_from_c(&trn_file, chr->pilot.trn_name);
-            str_toupper(&trn_file);
-            trn_loaded = trn_load(&trn, str_c(&trn_file)) == 0;
-            str_free(&trn_file);
+            trn_loaded = trn_load(&trn, chr->pilot.trn_name) == 0;
         }
     }
 
