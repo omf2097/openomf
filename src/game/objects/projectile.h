b7f33749e96827ca9794843dd435d0973efcf7de
Andrew Thompson <andrew@hijacked.us>
2025-04-17 08:46:09 -0700

    Implement uh tag and remove har child objects

--- src/game/objects/projectile.h
+++ src/game/objects/projectile.h
@@ -7,6 +7,7 @@
 typedef struct har_t har;
 
 int projectile_create(object *obj, object *parent);
+void projectile_finished(object *obj);
 const af *projectile_get_af_data(const object *obj);
 uint8_t projectile_get_owner(const object *obj);
 void projectile_set_wall_bounce(object *obj, int bounce);
