6b89a4f812188b23b9bd06c9d1c4780d032b27c3
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-04-18 23:12:24 +0300

    opengl3: Disable pointer-overflow sanitizer for setup_vao_layout()
    We are doing things the sanitizer doesn't approve here.

--- src/video/renderers/opengl3/helpers/object_array.c
+++ src/video/renderers/opengl3/helpers/object_array.c
@@ -54,7 +54,7 @@ typedef struct object_array {
     step += size * sizeof(GLint);                                                                                      \
     index++
 
-static void setup_vao_layout(void) {
+__attribute__((no_sanitize("pointer-overflow"))) static void setup_vao_layout(void) {
     int stride = 4 * sizeof(GLfloat) + 7 * sizeof(GLint);
     int index = 0;
     unsigned char *step = 0;
