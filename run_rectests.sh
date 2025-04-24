c13891c14cfa43d4864e22b215908768de8cb2a8
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2025-04-24 08:52:44 -0700

    Added rectests

--- run_rectests.sh
+++ run_rectests.sh
@@ -56,6 +56,8 @@ tests=(
     "Electra is stuck blocking until Thorn's ul tag wears off:UL_TAG_HOLD_UP.REC"
     "Pyros's destruct sequence works completely:PYROS_DESTRUCT.REC"
     "Katana's corkscrew blade has landing recovery:LANDING_RECOVERY.REC"
+    "Flail 66K is unblockable:UNBLOCKABLE_THROW.REC"
+    "Electra 66P dodges Earthquake Smash:DODGE_EARTHQUAKE.REC"
 )
 
 # Setup temp directory for outputs
