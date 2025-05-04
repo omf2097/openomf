7ba1422055f1cf41cde950cb1c93b312f47c2aa0
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-05-04 11:50:50 +0300

    tournament: Move economy numbers to har_economy.h

--- src/game/scenes/mechlab/har_economy.h
+++ src/game/scenes/mechlab/har_economy.h
@@ -8,4 +8,12 @@ static const uint8_t max_leg_power[11] = {6, 6, 8, 4, 5, 7, 5, 7, 6, 7, 7};
 static const uint8_t max_stun_res[11] = {8, 6, 8, 6, 7, 6, 6, 7, 7, 7, 6};
 static const uint8_t max_armor[11] = {5, 7, 7, 8, 6, 8, 6, 9, 6, 6, 7};
 
+static const int32_t har_prices[11] = {20000, 36000, 26000, 28000, 29000, 32000, 25000, 30000, 24000, 22000, 75000};
+static const int32_t har_upgrade_price[11] = {380, 400, 350, 400, 500, 330, 420, 370, 450, 360, 700};
+static const int32_t upgrade_level_multiplier[10] = {0, 1, 3, 7, 12, 18, 30, 50, 75, 120};
+
+static const int32_t arm_leg_multiplier = 2;
+static const int32_t stun_res_multiplier = 3;
+static const int32_t armor_multiplier = 5;
+
 #endif // HAR_ECONOMY_H
