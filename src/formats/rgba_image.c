bc5483ce54f1e9d7de263155308cbc7212b7174e
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-05-13 23:20:44 +0300

    formats: Fix sd_rgba_image_to_png() return value

--- src/formats/rgba_image.c
+++ src/formats/rgba_image.c
@@ -97,7 +97,8 @@ int sd_rgba_image_clear(sd_rgba_image *img, char r, char g, char b, char a) {
 }
 
 int sd_rgba_image_to_png(const sd_rgba_image *img, const char *filename) {
-    return write_rgb_png(filename, img->w, img->h, (const unsigned char *)img->data, true, false);
+    return write_rgb_png(filename, img->w, img->h, (const unsigned char *)img->data, true, false) ? SD_SUCCESS
+                                                                                                  : SD_FAILURE;
 }
 
 void sd_rgba_image_free(sd_rgba_image *img) {
