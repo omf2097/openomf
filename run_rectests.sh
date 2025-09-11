12659dd00aee49eeac8d598f20d6fd9907c42507
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2025-09-11 09:58:44 -0700

    Throw Range Bug (#1289)
    * Add rectest
    
    * Check next move's throw range as well as current move, fix existing rectest
    
    * fix typo
    
    * Change function signature to avoid redundant code
    
    * Add rectest for KO during throw

--- run_rectests.sh
+++ run_rectests.sh
@@ -49,6 +49,8 @@ tests=(
     "Katana can chain stomps and end with a jump kick:STOMP.REC"
     "Gargoyle's diving claw doesn't flip the opponent around:DIVING-CLAW.REC"
     "Flail cannot hit air 66K throw out of range:THROW-RANGE.REC"
+    "Flail cannot hit 66K if the followup animation is out of range:THROW_RANGE_BUG.REC"
+    "Flail's 66K attempt KO's the opponent and the game is fine with it:THROW_KO.REC"
     "Gargoyle reaches the top of the stage:FLY.REC"
     "Shadow grab can be interrupted by hitting the originating HAR:SHADOW-GRAB-INTERRUPT-HIT.REC"
     "Jaguar throw deals half stun due to rehit rules:DAMPENED_STUN.REC"
