d6a5ce0fa628905dd3c75582fa317b8cce0e60dd
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2025-09-11 08:10:31 -0700

    Add rectest for KO during throw

--- run_rectests.sh
+++ run_rectests.sh
@@ -50,6 +50,7 @@ tests=(
     "Gargoyle's diving claw doesn't flip the opponent around:DIVING-CLAW.REC"
     "Flail cannot hit air 66K throw out of range:THROW-RANGE.REC"
     "Flail cannot hit 66K if the followup animation is out of range:THROW_RANGE_BUG.REC"
+    "Flail's 66K attempt KO's the opponent and the game is fine with it:THROW_KO.REC"
     "Gargoyle reaches the top of the stage:FLY.REC"
     "Shadow grab can be interrupted by hitting the originating HAR:SHADOW-GRAB-INTERRUPT-HIT.REC"
     "Jaguar throw deals half stun due to rehit rules:DAMPENED_STUN.REC"
