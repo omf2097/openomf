f80290c0138db8e0fccb2bfa383700febc35ea61
Andrew Thompson <andrew@hijacked.us>
2025-09-18 19:27:30 -0700

    Enumerate zip files in mod directories and their contents

--- src/resources/modmanager.c
+++ src/resources/modmanager.c
@@ -7,28 +7,59 @@
 #include "utils/allocator.h"
 #include "utils/c_string_util.h"
 #include "utils/log.h"
-#include "utils/scandir.h"
+#include "vendored/zip/zip.h"
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
 
-int mod_count(void) {
-    list dir_list;
-    list_create(&dir_list);
-    const path scan = get_system_mod_directory();
-    if(!path_glob(&scan, &dir_list, "*.zip")) {
+int mod_find(list *mod_list) {
+    size_t size = 0;
+    path scan = get_system_mod_directory();
+    if(!path_glob(&scan, mod_list, "*.zip")) {
         log_warn("Failed to scan system mods directory!");
     } else {
-        log_info("Found %d system mods.", list_size(&dir_list));
+        log_info("Found %d system mods.", list_size(mod_list));
     }
+    size = list_size(mod_list);
     scan = get_user_mod_directory();
-    if(!path_glob(&scan, &dir_list, "*.zip")) {
+    if(!path_glob(&scan, mod_list, "*.zip")) {
         log_warn("Failed to scan user mods directory!");
     } else {
-        log_info("Found %d user mods.", list_size(&dir_list));
+        log_info("Found %d user mods.", list_size(mod_list) - size);
     }
-    size += list_size(&dir_list);
-    list_free(&dir_list);
+    size = list_size(mod_list);
 
     return size;
+}
+
+bool modmanager_init(void) {
+    list dir_list;
+    list_create(&dir_list);
+    mod_find(&dir_list);
+
+    iterator it;
+    list_iter_begin(&dir_list, &it);
+    path *p;
+    foreach(it, p) {
+        struct zip_t *zip = zip_open(path_c(p), 0, 'r');
+        ssize_t entries = zip_entries_total(zip);
+        if(entries > 0) {
+            log_info("mod %s has %d files", path_c(p), entries);
+            for(size_t i = 0; i < (size_t)entries; i++) {
+                if(zip_entry_openbyindex(zip, i) == 0 && zip_entry_isdir(zip) == 0) {
+                    log_info("mod contains %s", zip_entry_name(zip));
+                    unsigned long long entry_size = zip_entry_uncomp_size(zip);
+                    void *entry_buf = omf_calloc(entry_size, 1);
+                    if(zip_entry_noallocread(zip, entry_buf, entry_size) < 0) {
+                        log_warn("failed to load %s into memory", zip_entry_name(zip));
+                    }
+                }
+            }
+        } else {
+            log_warn("mod %s has no contents", path_c(p));
+        }
+    }
+
+    list_free(&dir_list);
+    return true;
 }
