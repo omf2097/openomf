7430eff0654b7d22ccb40d1cd26ca652e04d9c1d
Andrew Thompson <andrew@hijacked.us>
2025-10-14 17:12:33 -0700

    Support portrait width/height in ini files, load HAR palettes

--- src/utils/png_reader.h
+++ src/utils/png_reader.h
@@ -11,6 +11,7 @@
 #define PNG_READER_H
 
 #include "utils/path.h"
+#include "video/vga_palette.h"
 #include <stdbool.h>
 
 /**
@@ -23,6 +24,6 @@
  */
 bool read_paletted_png(const path *filename, unsigned char *dst);
 bool read_paletted_png_from_memory(const unsigned char *buf, size_t len, unsigned char *dst, int *w, int *h,
-                                   bool allow_transparency);
+                                   bool allow_transparency, vga_palette *pal);
 
 #endif // PNG_READER_H
