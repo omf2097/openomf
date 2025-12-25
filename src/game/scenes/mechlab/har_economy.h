dbff0a247e69cb6bbabaa42947c7e7ac69dcfc29
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-12-25 20:45:45 +0200

    mechlab: Rename lab_har_constants.h to har_economy.h
    This is so we can add functions there too, not just constants.

--- src/game/scenes/mechlab/har_economy.h
+++ src/game/scenes/mechlab/har_economy.h
@@ -1,5 +1,5 @@
-#ifndef LAB_HAR_CONSTANTS_H
-#define LAB_HAR_CONSTANTS_H
+#ifndef HAR_ECONOMY_H
+#define HAR_ECONOMY_H
 
 static const uint8_t max_arm_speed[11] = {6, 8, 4, 6, 9, 7, 8, 6, 9, 6, 7};
 static const uint8_t max_leg_speed[11] = {8, 9, 5, 6, 8, 8, 7, 6, 7, 5, 6};
@@ -8,4 +8,4 @@ static const uint8_t max_leg_power[11] = {6, 6, 8, 4, 5, 7, 5, 7, 6, 7, 7};
 static const uint8_t max_stun_res[11] = {8, 6, 8, 6, 7, 6, 6, 7, 7, 7, 6};
 static const uint8_t max_armor[11] = {5, 7, 7, 8, 6, 8, 6, 9, 6, 6, 7};
 
-#endif // LAB_HAR_CONSTANTS_H
+#endif // HAR_ECONOMY_H
