01bab72b57fec87cfac79342a95c1cdf8ddab3ca
Martti Rannanjärvi <martti.rannanjarvi@iki.fi>
2025-12-25 21:05:13 +0200

    tournament: Make pilots purchase random upgrades if they have money

--- src/game/scenes/mechlab/har_economy.c
+++ src/game/scenes/mechlab/har_economy.c
@@ -3,6 +3,25 @@
 #include "utils/log.h"
 #include "utils/random.h"
 
+bool har_can_upgrade(sd_pilot *pilot, enum HAR_UPGRADE upgrade) {
+    switch(upgrade) {
+        case ARM_POWER:
+            return pilot->arm_power < max_arm_power[pilot->har_id];
+        case ARM_SPEED:
+            return pilot->arm_speed < max_arm_speed[pilot->har_id];
+        case LEG_POWER:
+            return pilot->leg_power < max_leg_power[pilot->har_id];
+        case LEG_SPEED:
+            return pilot->leg_speed < max_leg_speed[pilot->har_id];
+        case ARMOR:
+            return pilot->armor < max_armor[pilot->har_id];
+        case STUN_RES:
+            return pilot->stun_resistance < max_stun_res[pilot->har_id];
+        default:
+            abort();
+    }
+}
+
 void purchase_random_har(sd_pilot *pilot) {
     int32_t possible_purchases[10];
     size_t n = 0;
@@ -16,4 +35,81 @@ void purchase_random_har(sd_pilot *pilot) {
     pilot->har_id = possible_purchases[rand_int(n)];
     pilot->money -= har_prices[pilot->har_id];
     log_debug("Pilot %s purchased HAR %u and has %d money left", pilot->name, pilot->har_id, pilot->money);
+}
+
+void purchase_random_har_upgrades(sd_pilot *pilot) {
+    enum HAR_UPGRADE possible_upgrades[N_UPGRADES];
+    size_t n;
+    do {
+        n = 0;
+        for(enum HAR_UPGRADE u = ARM_POWER; u < N_UPGRADES; ++u) {
+            if(har_can_upgrade(pilot, u) && pilot->money >= upgrade_price(pilot, u)) {
+                possible_upgrades[n] = u;
+                ++n;
+            }
+        }
+        if(n == 0) {
+            break;
+        }
+        enum HAR_UPGRADE chosen_upgrade = possible_upgrades[rand_int(n)];
+        upgrade_har(pilot, chosen_upgrade);
+        log_debug("Pilot %s bought upgrade %d and has %d money left", pilot->name, chosen_upgrade, pilot->money);
+    } while(n > 0);
+}
+
+int32_t upgrade_price(sd_pilot *pilot, enum HAR_UPGRADE upgrade) {
+    int32_t price = har_upgrade_price[pilot->har_id];
+    switch(upgrade) {
+        case ARM_POWER:
+            price *= upgrade_level_multiplier[pilot->arm_power + 1] * arm_leg_multiplier;
+            break;
+        case ARM_SPEED:
+            price *= upgrade_level_multiplier[pilot->arm_speed + 1] * arm_leg_multiplier;
+            break;
+        case LEG_POWER:
+            price *= upgrade_level_multiplier[pilot->leg_power + 1] * arm_leg_multiplier;
+            break;
+        case LEG_SPEED:
+            price *= upgrade_level_multiplier[pilot->leg_speed + 1] * arm_leg_multiplier;
+            break;
+        case ARMOR:
+            price *= upgrade_level_multiplier[pilot->armor + 1] * armor_multiplier;
+            break;
+        case STUN_RES:
+            price *= upgrade_level_multiplier[pilot->stun_resistance + 1] * stun_res_multiplier;
+            break;
+        default:
+            abort();
+    }
+    return price;
+}
+
+void upgrade_har(sd_pilot *pilot, enum HAR_UPGRADE upgrade) {
+    int32_t price = upgrade_price(pilot, upgrade);
+    assert(pilot->money >= price);
+    assert(har_can_upgrade(pilot, upgrade));
+
+    pilot->money -= price;
+    switch(upgrade) {
+        case ARM_POWER:
+            pilot->arm_power++;
+            break;
+        case ARM_SPEED:
+            pilot->arm_speed++;
+            break;
+        case LEG_POWER:
+            pilot->leg_power++;
+            break;
+        case LEG_SPEED:
+            pilot->leg_speed++;
+            break;
+        case ARMOR:
+            pilot->armor++;
+            break;
+        case STUN_RES:
+            pilot->stun_resistance++;
+            break;
+        default:
+            abort();
+    }
 }
--- src/game/scenes/mechlab/har_economy.h
+++ src/game/scenes/mechlab/har_economy.h
@@ -3,6 +3,17 @@
 
 #include "formats/pilot.h"
 
+enum HAR_UPGRADE
+{
+    ARM_POWER,
+    ARM_SPEED,
+    LEG_POWER,
+    LEG_SPEED,
+    ARMOR,
+    STUN_RES,
+    N_UPGRADES,
+};
+
 static const uint8_t max_arm_speed[11] = {6, 8, 4, 6, 9, 7, 8, 6, 9, 6, 7};
 static const uint8_t max_leg_speed[11] = {8, 9, 5, 6, 8, 8, 7, 6, 7, 5, 6};
 static const uint8_t max_arm_power[11] = {5, 5, 9, 8, 4, 6, 6, 5, 5, 6, 7};
@@ -18,6 +29,10 @@ static const int32_t arm_leg_multiplier = 2;
 static const int32_t stun_res_multiplier = 3;
 static const int32_t armor_multiplier = 5;
 
+bool har_can_upgrade(sd_pilot *pilot, enum HAR_UPGRADE upgrade);
 void purchase_random_har(sd_pilot *pilot);
+void purchase_random_har_upgrades(sd_pilot *pilot);
+void upgrade_har(sd_pilot *pilot, enum HAR_UPGRADE);
+int32_t upgrade_price(sd_pilot *pilot, enum HAR_UPGRADE);
 
 #endif // HAR_ECONOMY_H
