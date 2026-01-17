e061a1907cb078084dd62a885eaa686704bf37f2
Tuomas Virtanen <katajakasa@gmail.com>
2026-01-17 02:32:39 +0200

    ringbuffer: Split things into functions

--- src/utils/ringbuffer.c
+++ src/utils/ringbuffer.c
@@ -16,34 +16,54 @@ void rb_free(ring_buffer *rb) {
     omf_free(rb->data);
 }
 
-size_t rb_write(ring_buffer *rb, const char *data, size_t len) {
-    len = (len > (rb->size - rb->len)) ? (rb->size - rb->len) : len;
-    if(len == 0) {
-        return 0;
-    }
-    if(len + rb->w_pos > rb->size) {
-        const size_t k = (len + rb->w_pos) % rb->size;
-        memcpy((rb->data + rb->w_pos), data, len - k);
-        memcpy(rb->data, data + (len - k), k);
-    } else {
-        memcpy((rb->data + rb->w_pos), data, len);
-    }
+// Advance write position by len bytes, wrapping if necessary.
+static inline void rb_advance_write(ring_buffer *rb, const size_t len) {
     rb->len += len;
     rb->w_pos += len;
     if(rb->w_pos >= rb->size) {
-        rb->w_pos = rb->w_pos % rb->size;
+        rb->w_pos -= rb->size;
     }
-    return len;
 }
 
+// Advance read position by len bytes, wrapping if necessary.
+static inline void rb_advance_read(ring_buffer *rb, const size_t len) {
+    rb->len -= len;
+    rb->r_pos += len;
+    if(rb->r_pos >= rb->size) {
+        rb->r_pos -= rb->size;
+    }
+}
+
+// Copy len bytes from data into the buffer at write position, handling wraparound.
+static void rb_write_data(const ring_buffer *rb, const char *data, const size_t len) {
+    if(len + rb->w_pos > rb->size) {
+        const size_t k = (len + rb->w_pos) - rb->size;
+        memcpy(rb->data + rb->w_pos, data, len - k);
+        memcpy(rb->data, data + (len - k), k);
+    } else {
+        memcpy(rb->data + rb->w_pos, data, len);
+    }
+}
+
+// Copy len bytes from the buffer at read position into data, handling wraparound.
 static void rb_read_data(const ring_buffer *rb, char *data, const size_t len) {
     if(len + rb->r_pos > rb->size) {
-        const size_t k = (len + rb->r_pos) % rb->size;
+        const size_t k = (len + rb->r_pos) - rb->size;
         memcpy(data, rb->data + rb->r_pos, len - k);
         memcpy(data + (len - k), rb->data, k);
     } else {
         memcpy(data, rb->data + rb->r_pos, len);
     }
+}
+
+size_t rb_write(ring_buffer *rb, const char *data, size_t len) {
+    len = (len > (rb->size - rb->len)) ? (rb->size - rb->len) : len;
+    if(len == 0) {
+        return 0;
+    }
+    rb_write_data(rb, data, len);
+    rb_advance_write(rb, len);
+    return len;
 }
 
 size_t rb_read(ring_buffer *rb, char *data, size_t len) {
@@ -52,11 +72,7 @@ size_t rb_read(ring_buffer *rb, char *data, size_t len) {
         return 0;
     }
     rb_read_data(rb, data, len);
-    rb->len -= len;
-    rb->r_pos += len;
-    if(rb->r_pos >= rb->size) {
-        rb->r_pos = rb->r_pos % rb->size;
-    }
+    rb_advance_read(rb, len);
     return len;
 }
 
@@ -74,11 +90,7 @@ size_t rb_skip(ring_buffer *rb, size_t len) {
     if(len == 0) {
         return 0;
     }
-    rb->len -= len;
-    rb->r_pos += len;
-    if(rb->r_pos >= rb->size) {
-        rb->r_pos = rb->r_pos % rb->size;
-    }
+    rb_advance_read(rb, len);
     return len;
 }
 
