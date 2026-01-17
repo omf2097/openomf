a05403e09b6db61a9f0775163ecf8ecb07c1b7bc
Tuomas Virtanen <katajakasa@gmail.com>
2026-01-17 02:20:06 +0200

    ringbuffer: Add a quick bailout to write and read functions if nothing to do

--- src/utils/ringbuffer.c
+++ src/utils/ringbuffer.c
@@ -18,22 +18,22 @@ void rb_free(ring_buffer *rb) {
 
 size_t rb_write(ring_buffer *rb, const char *data, size_t len) {
     len = (len > (rb->size - rb->len)) ? (rb->size - rb->len) : len;
-    if(rb->len < rb->size) {
-        if(len + rb->w_pos > rb->size) {
-            const size_t k = (len + rb->w_pos) % rb->size;
-            memcpy((rb->data + rb->w_pos), data, len - k);
-            memcpy(rb->data, data + (len - k), k);
-        } else {
-            memcpy((rb->data + rb->w_pos), data, len);
-        }
-        rb->len += len;
-        rb->w_pos += len;
-        if(rb->w_pos >= rb->size) {
-            rb->w_pos = rb->w_pos % rb->size;
-        }
-        return len;
+    if(len == 0) {
+        return 0;
     }
-    return 0;
+    if(len + rb->w_pos > rb->size) {
+        const size_t k = (len + rb->w_pos) % rb->size;
+        memcpy((rb->data + rb->w_pos), data, len - k);
+        memcpy(rb->data, data + (len - k), k);
+    } else {
+        memcpy((rb->data + rb->w_pos), data, len);
+    }
+    rb->len += len;
+    rb->w_pos += len;
+    if(rb->w_pos >= rb->size) {
+        rb->w_pos = rb->w_pos % rb->size;
+    }
+    return len;
 }
 
 static void rb_read_data(const ring_buffer *rb, char *data, const size_t len) {
@@ -48,38 +48,38 @@ static void rb_read_data(const ring_buffer *rb, char *data, const size_t len) {
 
 size_t rb_read(ring_buffer *rb, char *data, size_t len) {
     len = (len > rb->len) ? rb->len : len;
-    if(rb->len > 0) {
-        rb_read_data(rb, data, len);
-        rb->len -= len;
-        rb->r_pos += len;
-        if(rb->r_pos >= rb->size) {
-            rb->r_pos = rb->r_pos % rb->size;
-        }
-        return len;
+    if(len == 0) {
+        return 0;
     }
-    return 0;
+    rb_read_data(rb, data, len);
+    rb->len -= len;
+    rb->r_pos += len;
+    if(rb->r_pos >= rb->size) {
+        rb->r_pos = rb->r_pos % rb->size;
+    }
+    return len;
 }
 
 size_t rb_peek(const ring_buffer *rb, char *data, size_t len) {
     len = (len > rb->len) ? rb->len : len;
-    if(rb->len > 0) {
-        rb_read_data(rb, data, len);
-        return len;
+    if(len == 0) {
+        return 0;
     }
-    return 0;
+    rb_read_data(rb, data, len);
+    return len;
 }
 
 size_t rb_skip(ring_buffer *rb, size_t len) {
     len = (len > rb->len) ? rb->len : len;
-    if(rb->len > 0) {
-        rb->len -= len;
-        rb->r_pos += len;
-        if(rb->r_pos >= rb->size) {
-            rb->r_pos = rb->r_pos % rb->size;
-        }
-        return len;
+    if(len == 0) {
+        return 0;
     }
-    return 0;
+    rb->len -= len;
+    rb->r_pos += len;
+    if(rb->r_pos >= rb->size) {
+        rb->r_pos = rb->r_pos % rb->size;
+    }
+    return len;
 }
 
 size_t rb_length(const ring_buffer *rb) {
