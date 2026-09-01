aec05c3fef94e7ff3c525f4e22b4d28f6b333e49
LegendaryBlueShirt <aokmaniac13@justnopoint.com>
2026-09-01 00:12:18 -0700

    Fix peculiar corner behavior (#1429)
    * Right side air position is now clamped to 299 instead of 300
    Corner crossup behavior is now accurate to source
    Wallslam is no longer disabled after hp reaches 0
    
    * Added test
    
    * Fix merge conflicts, add comment for future bugfix
    
    * Fix bad merge
    
    * Update recfile after merge

--- run_rectests.sh
+++ run_rectests.sh
@@ -77,6 +77,7 @@ tests=(
     "Katana enters winpose after landing from heel stomp:KATANA_WINPOSE.REC"
     "Round one starts on tick 92:ROUNDONE.REC"
     "Chronos can attack opponent when in stasis in the air and rehit mode is disabled:CHRONOS_AIR_STASIS.REC"
+    "Player 2 can crossup in the left corner and neither player can crossup in the right corner:CORNER_TEST.REC"
     "Player can hold back to block even after being crossed up:BLOCK_DIR.REC"
 )
 
